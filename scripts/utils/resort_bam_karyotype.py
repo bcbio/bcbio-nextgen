d1c6d70aa3e65895e515a48340f069435715bb0c
Brad Chapman <chapmanb@50mail.com>
2010-09-29 12:50:59 -0400

    Allow resorting of BAMs based on an input reference dictionary to match

--- scripts/utils/resort_bam_karyotype.py
+++ scripts/utils/resort_bam_karyotype.py
@@ -13,7 +13,12 @@ This takes a sorted BAM files with an alternative ordering of chromosomes
 and re-sorts it the karyotypic way.
 
 Usage:
-    resort_bam_karyotype.py [<in_files>]
+    resort_bam_karyotype.py <reference dict> [<one or more> <BAM files>]
+
+<reference dict> is a *.dict file produced by Picard that identifies the order
+of chromsomes to sort by:
+
+java -jar CreateSequenceDictionary.jar REFERENCE=your.fasta OUTPUT=your.dict
 
 Requires:
     pysam -- http://code.google.com/p/pysam/
@@ -23,11 +28,13 @@ import sys
 
 import pysam
 
-def main(in_bams):
+def main(ref_file, *in_bams):
+    ref = pysam.Samfile(ref_file, "r")
+    sorter = SortByHeader(ref.header)
     for bam in in_bams:
-        sort_bam(bam, sort_by_karyotype)
+        sort_bam(bam, sorter.header_cmp, sorter.to_include)
 
-def sort_bam(in_bam, sort_fn):
+def sort_bam(in_bam, sort_fn, to_include=None):
     out_file = "%s-ksort%s" % os.path.splitext(in_bam)
     index_file = "%s.bai" % in_bam
     if not os.path.exists(index_file):
@@ -36,8 +43,10 @@ def sort_bam(in_bam, sort_fn):
     orig = pysam.Samfile(in_bam, "rb")
     chroms = [(c["SN"], c) for c in orig.header["SQ"]]
     new_chroms = chroms[:]
+    if to_include:
+        new_chroms = [(c, x) for (c, x) in new_chroms if c in to_include]
     new_chroms.sort(sort_fn)
-    remapper = id_remapper(chroms, new_chroms)
+    remapper = _id_remapper(chroms, new_chroms)
     new_header = orig.header
     new_header["SQ"] = [h for (_, h) in new_chroms]
 
@@ -48,7 +57,7 @@ def sort_bam(in_bam, sort_fn):
             read.mrnm = remapper[read.mrnm]
             new.write(read)
 
-def id_remapper(orig, new):
+def _id_remapper(orig, new):
     """Provide a dictionary remapping original read indexes to new indexes.
 
     When re-ordering the header, the individual read identifiers need to be
@@ -59,9 +68,24 @@ def id_remapper(orig, new):
         new_chrom_to_index[chr_n] = i_n
     remap_indexes = {}
     for i_o, (chr_o, _) in enumerate(orig):
-        remap_indexes[i_o] = new_chrom_to_index[chr_o]
+        if chr_o in new_chrom_to_index.keys():
+            remap_indexes[i_o] = new_chrom_to_index[chr_o]
     remap_indexes[None] = None
     return remap_indexes
+
+class SortByHeader:
+    """Provide chromosome sorting to match an existing header.
+    """
+    def __init__(self, base_header):
+        self._chrom_indexes = {}
+        self.to_include = []
+        for i, item in enumerate(base_header["SQ"]):
+            self._chrom_indexes[item["SN"]] = i
+            self.to_include.append(item["SN"])
+
+    def header_cmp(self, one, two):
+        return cmp(self._chrom_indexes[one[0]],
+                   self._chrom_indexes[two[0]])
 
 def sort_by_karyotype(one, two):
     """Sort function to order reads by karyotype.
@@ -81,5 +105,5 @@ def _split_to_karyotype(name):
     return parts
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main(*sys.argv[1:])
 
