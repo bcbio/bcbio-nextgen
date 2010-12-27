9a943214218b6a6ba9f5dd069715ec2da64d9cba
chapmanb <chapmanb@50mail.com>
2010-12-27 07:12:07 -0500

    Provide utility functions for common Picard tasks

---
+++ bcbio/picard/run.py
@@ -0,0 +1,17 @@
+"""Convenience functions for running common Picard and GATK utilities.
+"""
+from bcbio.picard.utils import curdir_tmpdir
+
+def picard_sort(picard, align_bam):
+    """Sort a BAM file by coordinates.
+    """
+    base, ext = os.path.splitext(align_bam)
+    out_file = "%s-sort%s" % (base, ext)
+    if not os.path.exists(out_file):
+        with curdir_tmpdir() as tmp_dir:
+            opts = [("INPUT", align_bam),
+                    ("OUTPUT", out_file),
+                    ("TMP_DIR", tmp_dir),
+                    ("SORT_ORDER", "coordinate")]
+            picard.run("SortSam", opts)
+    return out_file
