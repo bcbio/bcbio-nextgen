1bbcac43b85e43098ddfd8e632ba190e84b924ca
chapmanb <chapmanb@50mail.com>
2010-12-27 17:08:38 -0500

    Provide general merge functionality using Picard

--- bcbio/picard/run.py
+++ bcbio/picard/run.py
@@ -15,3 +15,17 @@ def picard_sort(picard, align_bam):
                     ("SORT_ORDER", "coordinate")]
             picard.run("SortSam", opts)
     return out_file
+
+def picard_merge(picard, *in_files):
+    """Merge multiple BAM files together with Picard.
+    """
+    out_file = "%s-merge.bam" % os.path.commonprefix(in_files)
+    if not os.path.exists(out_file):
+        with curdir_tmpdir() as tmp_dir:
+            opts = [("OUTPUT", out_file),
+                    ("SORT_ORDER", "coordinate"),
+                    ("TMP_DIR", tmp_dir)]
+            for in_file in in_files:
+                opts.append(("INPUT", in_file))
+            picard.run("MergeSam", opts)
+    return out_file
