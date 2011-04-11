5fc2c3f2aa7c340f90f0ab1fb91cd50aa375c941
Roman Valls Guimera <roman.valls.guimera@scilifelab.se>
2011-04-11 14:30:18 +0200

    Relevant files to keep, mail notifications testing

--- bcbio/log/__init__.py
+++ bcbio/log/__init__.py
@@ -7,10 +7,17 @@ import logbook
 
 def create_log_handler(config, log_name):
     log_dir = config.get("log_dir", None)
+    email = config.get("email_notify", None)
+    
     if log_dir:
         if not os.path.exists(log_dir):
             os.makedirs(log_dir)
         handler = logbook.FileHandler(os.path.join(log_dir, "%s.log" % log_name))
     else:
         handler = logbook.StreamHandler(sys.stdout)
+        
+    if email:
+        handler = logbook.MailHandler("ngsmaster@example.com", [email],
+                                      format_string=logbook.handlers.MAIL_FORMAT_STRING,
+                                      level='INFO', bubble = True)
     return handler
