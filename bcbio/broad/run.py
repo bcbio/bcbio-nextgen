55e0e7dab2c4a8975b4975bea6c5da059bc137ee
chapmanb <chapmanb@50mail.com>
2011-05-20 15:27:52 -0400

    Reusable indel realignment wrappers

--- bcbio/broad/run.py
+++ bcbio/broad/run.py
@@ -39,3 +39,56 @@ def picard_index(picard, in_bam):
                 ("OUTPUT", index_file)]
         picard.run("BuildBamIndex", opts)
     return index_file
+
+def picard_index_ref(picard, ref_file):
+    """Provide a Picard style dict index file for a reference genome.
+    """
+    dict_file = "%s.dict" % os.path.splitext(ref_file)[0]
+    if not os.path.exists(dict_file):
+        opts = [("REFERENCE", ref_file),
+                ("OUTPUT", dict_file)]
+        picard.run("CreateSequenceDictionary", opts)
+    return dict_file
+
+def gatk_realigner_targets(runner, align_bam, ref_file, dbsnp=None):
+    """Generate a list of interval regions for realignment around indels.
+    """
+    out_file = "%s-realign.intervals" % os.path.splitext(align_bam)[0]
+    params = ["-T", "RealignerTargetCreator",
+              "-I", align_bam,
+              "-R", ref_file,
+              "-o", out_file,
+              "-l", "INFO",
+              ]
+    if dbsnp:
+        params += ["-B:dbsnp,VCF", dbsnp]
+    if not (os.path.exists(out_file) and os.path.getsize(out_file) > 0):
+        runner.run_gatk(params)
+    return out_file
+
+def gatk_indel_realignment(runner, align_bam, ref_file, intervals):
+    """Perform realignment of BAM file in specified regions
+    """
+    out_file = "%s-realign.bam" % os.path.splitext(align_bam)[0]
+    params = ["-T", "IndelRealigner",
+              "-I", align_bam,
+              "-R", ref_file,
+              "-targetIntervals", intervals,
+              "-o", out_file,
+              "-l", "INFO",
+              ]
+    if not (os.path.exists(out_file) and os.path.getsize(out_file) > 0):
+        with curdir_tmpdir() as tmp_dir:
+            runner.run_gatk(params, tmp_dir)
+    return out_file
+
+def gatk_realigner(runner, ref_file, align_bam, dbsnp=None):
+    """Realign a BAM file around indels using GATK.
+    """
+    picard_index_ref(runner, ref_file)
+    realign_target_file = gatk_realigner_targets(runner, align_bam,
+                                                 ref_file, dbsnp)
+    realign_bam = gatk_indel_realignment(runner, align_bam, ref_file,
+                                         realign_target_file)
+    return realign_bam
+
