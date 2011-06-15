e838d11d136c71a0f1ea5c137e4c7bee3f66c945
chapmanb <chapmanb@50mail.com>
2011-06-15 08:36:17 -0400

    Move flowcell merge functionality to separate pipeline module

---
+++ bcbio/pipeline/merge.py
@@ -0,0 +1,76 @@
+"""Handle multiple samples present on a single flowcell
+
+This provides functionality for cases where you the same sample in multiple lanes
+on a flowcell. There can be combined from barcoded subsets and are identified
+based on unique sample names.
+"""
+import os
+import shutil
+import collections
+
+from bcbio import utils
+from bcbio.broad import BroadRunner
+from bcbio.pipeline.fastq import get_fastq_files
+
+def combine_fastq_files(in_files, work_dir):
+    if len(in_files) == 1:
+        return in_files[0]
+    else:
+        cur1, cur2 = in_files[0]
+        out1 = os.path.join(work_dir, os.path.basename(cur1))
+        out2 = os.path.join(work_dir, os.path.basename(cur2)) if cur2 else None
+        if not os.path.exists(out1):
+            with open(out1, "a") as out_handle:
+                for (cur1, _) in in_files:
+                    with open(cur1) as in_handle:
+                        shutil.copyfileobj(in_handle, out_handle)
+        if out2 and not os.path.exists(out2):
+            with open(out2, "a") as out_handle:
+                for (_, cur2) in in_files:
+                    with open(cur2) as in_handle:
+                        shutil.copyfileobj(in_handle, out_handle)
+        return out1, out2
+
+def organize_samples(align_dir, fastq_dir, work_dir, fc_name, fc_date, run_items):
+    """Organize BAM output files by sample name, handling multiplexing.
+    """
+    bams_by_sample = collections.defaultdict(list)
+    sample_info = dict()
+    fastq_by_sample = collections.defaultdict(list)
+    for lane_info in run_items:
+        multiplex = lane_info.get("multiplex", None)
+        if multiplex:
+            mfastq_dir = os.path.join(work_dir, "%s_%s_%s_barcode" %
+                    (lane_info["lane"], fc_date, fc_name))
+            for multi in multiplex:
+                name = (lane_info.get("name", ""), lane_info["description"],
+                        multi["name"])
+                fname = os.path.join(align_dir, "%s_%s_%s_%s-sort.bam" %
+                    (lane_info["lane"], fc_date, fc_name, multi["barcode_id"]))
+                if os.path.exists(fname):
+                    bams_by_sample[name].append(fname)
+                    sample_info[name] = lane_info
+                    fastq_by_sample[name].append(get_fastq_files(mfastq_dir,
+                        lane_info["lane"], fc_name, multi["barcode_id"]))
+        else:
+            name = (lane_info.get("name", ""), lane_info["description"])
+            fname = os.path.join(align_dir, "%s_%s_%s-sort.bam" %
+                    (lane_info["lane"], fc_date, fc_name))
+            if os.path.exists(fname):
+                bams_by_sample[name].append(fname)
+                sample_info[name] = lane_info
+                fastq_by_sample[name].append(get_fastq_files(fastq_dir,
+                    lane_info["lane"], fc_name))
+    return sorted(bams_by_sample.items()), dict(fastq_by_sample), sample_info
+
+def merge_bam_files(bam_files, work_dir, config):
+    """Merge multiple BAM files from a sample into a single BAM for processing.
+    """
+    out_file = os.path.join(work_dir, os.path.basename(bam_files[0]))
+    picard = BroadRunner(config["program"]["picard"],
+                         max_memory=config["algorithm"].get("java_memory", ""))
+    picard.run_fn("picard_merge", bam_files, out_file)
+    for b in bam_files:
+        utils.save_diskspace(b, "BAM merged to %s" % out_file, config)
+    return out_file
+
--- scripts/automated_initial_analysis.py
+++ scripts/automated_initial_analysis.py
@@ -22,13 +22,10 @@ Workflow:
 import os
 import sys
 import json
-import contextlib
 import subprocess
 import glob
 import csv
 import copy
-import shutil
-import collections
 from optparse import OptionParser
 import xml.etree.ElementTree as ET
 import StringIO
@@ -45,6 +42,8 @@ from bcbio.log import create_log_handler
 from bcbio.pipeline.fastq import get_fastq_files
 from bcbio.pipeline.alignment import align_to_sort_bam, get_genome_ref
 from bcbio.pipeline.demultiplex import split_by_barcode, add_multiplex_across_lanes
+from bcbio.pipeline.merge import (combine_fastq_files, organize_samples,
+                                  merge_bam_files)
 
 LOG_NAME = os.path.splitext(os.path.basename(__file__))[0]
 log = logbook.Logger(LOG_NAME)
@@ -136,7 +135,7 @@ def process_sample(sample_name, fastq_files, info, bam_files, work_dir,
     (_, galaxy_dir) = _get_full_paths("", config, config_file)
     (_, sam_ref) = get_genome_ref(genome_build, config["algorithm"]["aligner"],
                                   galaxy_dir)
-    fastq1, fastq2 = _combine_fastq_files(fastq_files, work_dir)
+    fastq1, fastq2 = combine_fastq_files(fastq_files, work_dir)
     log.info("Combining and preparing wig file %s" % str(sample_name))
     sort_bam = merge_bam_files(bam_files, work_dir, config)
     bam_to_wig(sort_bam, config, config_file)
@@ -157,68 +156,6 @@ def process_sample(sample_name, fastq_files, info, bam_files, work_dir,
         print sample_name, "Generating summary files"
         generate_align_summary(sort_bam, fastq2 is not None, sam_ref,
                 config, sample_name, config_file)
-
-def _combine_fastq_files(in_files, work_dir):
-    if len(in_files) == 1:
-        return in_files[0]
-    else:
-        cur1, cur2 = in_files[0]
-        out1 = os.path.join(work_dir, os.path.basename(cur1))
-        out2 = os.path.join(work_dir, os.path.basename(cur2)) if cur2 else None
-        if not os.path.exists(out1):
-            with open(out1, "a") as out_handle:
-                for (cur1, _) in in_files:
-                    with open(cur1) as in_handle:
-                        shutil.copyfileobj(in_handle, out_handle)
-        if out2 and not os.path.exists(out2):
-            with open(out2, "a") as out_handle:
-                for (_, cur2) in in_files:
-                    with open(cur2) as in_handle:
-                        shutil.copyfileobj(in_handle, out_handle)
-        return out1, out2
-
-def organize_samples(align_dir, fastq_dir, work_dir, fc_name, fc_date, run_items):
-    """Organize BAM output files by sample name, handling multiplexing.
-    """
-    bams_by_sample = collections.defaultdict(list)
-    sample_info = dict()
-    fastq_by_sample = collections.defaultdict(list)
-    for lane_info in run_items:
-        multiplex = lane_info.get("multiplex", None)
-        if multiplex:
-            mfastq_dir = os.path.join(work_dir, "%s_%s_%s_barcode" %
-                    (lane_info["lane"], fc_date, fc_name))
-            for multi in multiplex:
-                name = (lane_info.get("name", ""), lane_info["description"],
-                        multi["name"])
-                fname = os.path.join(align_dir, "%s_%s_%s_%s-sort.bam" %
-                    (lane_info["lane"], fc_date, fc_name, multi["barcode_id"]))
-                if os.path.exists(fname):
-                    bams_by_sample[name].append(fname)
-                    sample_info[name] = lane_info
-                    fastq_by_sample[name].append(get_fastq_files(mfastq_dir,
-                        lane_info["lane"], fc_name, multi["barcode_id"]))
-        else:
-            name = (lane_info.get("name", ""), lane_info["description"])
-            fname = os.path.join(align_dir, "%s_%s_%s-sort.bam" %
-                    (lane_info["lane"], fc_date, fc_name))
-            if os.path.exists(fname):
-                bams_by_sample[name].append(fname)
-                sample_info[name] = lane_info
-                fastq_by_sample[name].append(get_fastq_files(fastq_dir,
-                    lane_info["lane"], fc_name))
-    return sorted(bams_by_sample.items()), dict(fastq_by_sample), sample_info
-
-def merge_bam_files(bam_files, work_dir, config):
-    """Merge multiple BAM files from a sample into a single BAM for processing.
-    """
-    out_file = os.path.join(work_dir, os.path.basename(bam_files[0]))
-    picard = BroadRunner(config["program"]["picard"],
-                         max_memory=config["algorithm"].get("java_memory", ""))
-    picard.run_fn("picard_merge", bam_files, out_file)
-    for b in bam_files:
-        utils.save_diskspace(b, "BAM merged to %s" % out_file, config)
-    return out_file
 
 def bam_to_wig(bam_file, config, config_file):
     """Provide a BigWig coverage file of the sorted alignments.
