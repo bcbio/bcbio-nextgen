bb99c014482fc489fd0cb6482c40ad436ce0ed9f
chapmanb <chapmanb@50mail.com>
2011-08-01 09:20:21 -0400

    Automatically get number of workers needed unless configured specifically

--- scripts/distributed_nextgen_pipeline.py
+++ scripts/distributed_nextgen_pipeline.py
@@ -14,20 +14,23 @@ Usage:
 """
 import sys
 import time
+import math
 import subprocess
 
 import yaml
 
+from bcbio.pipeline.run_info import get_run_info
 from bcbio.distributed import lsf, sge
 
 def main(config_file, fc_dir, run_info_yaml=None):
     with open(config_file) as in_handle:
         config = yaml.load(in_handle)
     assert config["algorithm"]["num_cores"] == "messaging", \
-           "Designed for use with 'messaging' parallelization"
+           "This script is used with configured 'messaging' parallelization"
     cluster = globals()[config["distributed"]["cluster_platform"]]
+    workers_needed = _needed_workers(get_run_info(fc_dir, config, run_info_yaml)[-1])
     print "Starting cluster workers"
-    jobids = start_workers(cluster, config, config_file)
+    jobids = start_workers(cluster, workers_needed, config, config_file)
     try:
         print "Running analysis"
         run_analysis(config_file, fc_dir, run_info_yaml, cluster, config)
@@ -35,27 +38,23 @@ def main(config_file, fc_dir, run_info_yaml=None):
         print "Cleaning up cluster workers"
         stop_workers(cluster, jobids)
 
-def start_workers(cluster, config, config_file):
-    args = config["distributed"]["platform_args"].split()
+def start_workers(cluster, workers_needed, config, config_file):
+    # we can manually specify workers or dynamically get as many as needed
+    num_workers = config["distributed"].get("num_workers", None)
+    if num_workers is None:
+        cores_per_host = config["distributed"].get("cores_per_host", 1)
+        if cores_per_host == 0:
+            raise ValueError("Need to set num_workers if cores_per_host is determined dynamically")
+        num_workers = int(math.ceil(float(workers_needed) / cores_per_host))
     program_cl = [config["analysis"]["worker_program"], config_file]
-    jobids = [cluster.submit_job(args, program_cl)
-              for _ in range(config["distributed"]["num_workers"])]
+    args = config["distributed"]["platform_args"].split()
+    jobids = [cluster.submit_job(args, program_cl) for _ in range(num_workers)]
     while not(cluster.are_running(jobids)):
         time.sleep(5)
     return jobids
 
 def run_analysis(config_file, fc_dir, run_info_yaml, cluster, config):
-    # get arguments, ignoring need for multiple cores in arguments
-    # since analysis is single threaded runner
-    args = []
-    ignore = False
-    for a in config["distributed"]["platform_args"].split():
-        if ignore:
-            ignore = False
-        elif a == "-n":
-            ignore = True
-        else:
-            args.append(a)
+    args = config["distributed"]["platform_args"].split()
     program_cl = [config["analysis"]["process_program"], config_file, fc_dir]
     if run_info_yaml:
         program_cl.append(run_info_yaml)
@@ -77,6 +76,15 @@ def _monitor_analysis(cluster, jobid):
 def stop_workers(cluster, jobids):
     for jobid in jobids:
         cluster.stop_job(jobid)
+
+def _needed_workers(run_info):
+    """Determine workers needed to run multiplex flowcells in parallel.
+    """
+    names = []
+    for lane in run_info["details"]:
+        for multiplex in lane.get("multiplex", [{"barcode_id": ""}]):
+            names.append((lane.get("name", ""), lane["description"], multiplex["barcode_id"]))
+    return len(set(names))
 
 if __name__ == "__main__":
     main(*sys.argv[1:])
