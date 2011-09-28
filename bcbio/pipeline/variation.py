bf03c4499cfc581bacc6d6078ca3a54ba8d6a81d
chapmanb <chapmanb@50mail.com>
2011-09-28 14:37:26 -0400

    Require recalibration plots to be turned on; avoid time consuming process for sake of a graph unless needed

--- bcbio/pipeline/variation.py
+++ bcbio/pipeline/variation.py
@@ -18,7 +18,8 @@ def recalibrate_quality(sort_bam_file, fastq1, fastq2, sam_ref,
     """
     dbsnp_file = _configured_ref_file("dbsnp", config, sam_ref)
     recal_file = gatk_recalibrate(sort_bam_file, sam_ref, config, dbsnp_file)
-    _analyze_recalibration(recal_file, fastq1, fastq2, dirs, config)
+    if config["algorithm"].get("recalibration_plots", False):
+        _analyze_recalibration(recal_file, fastq1, fastq2, dirs, config)
     return recal_file
 
 def _analyze_recalibration(recal_file, fastq1, fastq2, dirs, config):
