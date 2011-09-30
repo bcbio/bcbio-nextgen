bab80564faf8023597740b31bd2fff9180b1c847
chapmanb <chapmanb@50mail.com>
2011-09-30 10:12:24 -0400

    Generalize distributed management for use outside of pipeline scripts

--- scripts/nextgen_analysis_server.py
+++ scripts/nextgen_analysis_server.py
@@ -12,6 +12,8 @@ Usage:
                              on. No argument runs the default queue, which
                              handles processing alignments. 'toplevel' handles
                              managing the full work process.]
+   [--tasks=task.module.import: Specify the module of tasks to make available.
+                                Defaults to bcbio.distributed.tasks if not specified.]
 """
 import os
 import sys
@@ -23,8 +25,9 @@ import yaml
 from bcbio import utils
 from bcbio.distributed.messaging import create_celeryconfig
 
-def main(config_file, queues=None):
-    task_module = "bcbio.distributed.tasks"
+def main(config_file, queues=None, task_module=None):
+    if task_module is None:
+        task_module = "bcbio.distributed.tasks"
     with open(config_file) as in_handle:
         config = yaml.load(in_handle)
     with utils.curdir_tmpdir() as work_dir:
@@ -44,9 +47,11 @@ if __name__ == "__main__":
     parser = optparse.OptionParser()
     parser.add_option("-q", "--queues", dest="queues", action="store",
                       default=None)
+    parser.add_option("-t", "--tasks", dest="task_module", action="store",
+                      default=None)
     (options, args) = parser.parse_args()
     if len(args) != 1:
         print "Incorrect arguments"
         print __doc__
         sys.exit()
-    main(args[0], options.queues)
+    main(args[0], options.queues, options.task_module)
