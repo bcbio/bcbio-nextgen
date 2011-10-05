c01f5ca0feced097d98bed30589fb09f3ee7c784
chapmanb <chapmanb@50mail.com>
2011-10-05 11:43:13 -0400

    Allow configuration of number of workers on commandline

--- scripts/distributed_nextgen_pipeline.py
+++ scripts/distributed_nextgen_pipeline.py
@@ -3,25 +3,28 @@
 
 Usage:
   run_distributed_job.py <config_file> <fc_dir> [<run_info_yaml>]
+     -n <number of processes to start>
 """
 import sys
+from optparse import OptionParser
 
 import yaml
 
 from bcbio.pipeline.run_info import get_run_info
 from bcbio.distributed.manage import run_and_monitor
 
-def main(config_file, fc_dir, run_info_yaml=None):
+def main(config_file, fc_dir, run_info_yaml=None, num_workers=None):
     with open(config_file) as in_handle:
         config = yaml.load(in_handle)
     assert config["algorithm"]["num_cores"] == "messaging", \
            "Use this script only with configured 'messaging' parallelization"
-    workers_needed = _needed_workers(get_run_info(fc_dir, config, run_info_yaml)[-1])
+    if num_workers is None:
+        num_workers = _needed_workers(get_run_info(fc_dir, config, run_info_yaml)[-1])
     task_module = "bcbio.distributed.tasks"
     args = [config_file, fc_dir]
     if run_info_yaml:
         args.append(run_info_yaml)
-    run_and_monitor(config, config_file, args, workers_needed, task_module)
+    run_and_monitor(config, config_file, args, num_workers, task_module)
 
 def _needed_workers(run_info):
     """Determine workers needed to run multiplex flowcells in parallel.
@@ -33,8 +36,12 @@ def _needed_workers(run_info):
     return len(set(names))
 
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
+    parser = OptionParser()
+    parser.add_option("-n", "--numworkers", dest="num_workers", default=None)
+    (options, args) = parser.parse_args()
+    if len(args) < 2:
         print "Incorrect arguments"
         print __doc__
         sys.exit()
-    main(*sys.argv[1:])
+    kwargs = {"num_workers": options.num_workers}
+    main(*args, **kwargs)
