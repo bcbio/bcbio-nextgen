10c84cb0c48337c7280f20a17f2f68b75e950857
Valentine Svensson <valentine@nxn.se>
2011-10-12 11:31:07 +0200

    Generalizing config_loader

--- bcbio/pipeline/config_loader.py
+++ bcbio/pipeline/config_loader.py
@@ -10,7 +10,11 @@ program:
 algorithm:
 	setting1:
 	setting2:
+log_dir:
+store_dir:
+store_host:
 analysis:
+	config_file:
 	towig_script:
 distributed:
 	rabbitmq_vhost:
@@ -26,13 +30,32 @@ import os
 import yaml
 
 def load_config(config_file):
+	""" Goes through two levels of the given yaml  post processing config file, 
+	performs replacement of environment variables and returns a dictionary 
+	representing the yaml file.
+	"""
 	with open(config_file) as in_handle:
 		config = yaml.load(in_handle)
 	
 	for field, setting in config.items():
-		config[field] = os.path.expandvars(setting)
+		try:
+			config[field] = expand_path(setting)
+		except AttributeError:
+			pass
 
-		for sub_field, sub_setting in config[field].items():
-			config[field][sub_field] = os.path.expandvars(sub_setting)	
+		try:
+			for sub_field, sub_setting in config[field].items():
+				config[field][sub_field] = expand_path(sub_setting)
+		except AttributeError:
+			pass
 
 	return config
+
+def expand_path(path):
+	""" Combines os.path.expandvars with replacing ~ with $HOME.
+	"""
+	try:
+		new_path = os.path.expandvars(path.replace("~", "$HOME"))
+		return new_path
+	except AttributeError:
+		raise AttributeError("Not a path string")
