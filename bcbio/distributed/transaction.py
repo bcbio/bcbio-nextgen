fd2f63b25a5cacb15f2f2a8d63b74f8bcfc70f30
chapmanb <chapmanb@50mail.com>
2011-10-16 21:31:06 -0400

    Updated mechanism for transactions to handle non-clean shutdowns

---
+++ bcbio/distributed/transaction.py
@@ -0,0 +1,47 @@
+"""Handle file based transactions allowing safe restarts at any point.
+
+To handle interrupts,this defines output files written to temporary
+locations during processing and copied to the final location when finished.
+This ensures output files will be complete independent of method of
+interruption.
+"""
+import os
+import shutil
+
+import contextlib
+
+from bcbio import utils
+
+@contextlib.contextmanager
+def file_transaction(*rollback_files):
+    """Wrap file generation in a transaction, moving to output if finishes.
+    """
+    safe_names, orig_names = _flatten_plus_safe(rollback_files)
+    _remove_files(safe_names) # remove any half-finished transactions
+    try:
+        yield safe_names
+    except: # failure -- delete any temporary files
+        _remove_files(safe_names)
+    else: # worked -- move the temporary files to permanent location
+        for safe, orig in zip(safe_names, orig_names):
+            shutil.move(safe, orig)
+
+def _remove_files(fnames):
+    for x in fnames:
+        if x and os.path.exists(x) and os.path.isfile(x):
+            os.remove(x)
+
+def _flatten_plus_safe(rollback_files):
+    """Flatten names of files and create temporary file names.
+    """
+    tx_files, orig_files = [], []
+    for fnames in rollback_files:
+        if isinstance(fnames, str):
+            fnames = [fnames]
+        for fname in fnames:
+            tx_file = os.path.join(os.path.dirname(fname), "tx",
+                                   os.path.basename(fname))
+            utils.safe_makedir(os.path.dirname(tx_file))
+            tx_files.append(tx_file)
+            orig_files.append(fname)
+    return tx_files, orig_files
