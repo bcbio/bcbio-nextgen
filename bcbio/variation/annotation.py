e9c1c05b1ca8ab9d8bc938bbde2c25a5f9025e7e
chapmanb <chapmanb@50mail.com>
2011-11-18 07:52:04 -0500

    GATK support for snpEff versions is very strict; generalize to allow minor version changes without breaking

--- bcbio/variation/annotation.py
+++ bcbio/variation/annotation.py
@@ -15,6 +15,7 @@ def annotate_effects(orig_file, snpeff_file, genome_file, config):
     """
     broad_runner = broad.runner_from_config(config)
     out_file = "%s-annotated%s" % os.path.splitext(orig_file)
+    snpeff_file = _general_snpeff_version(snpeff_file)
     if not file_exists(out_file):
         with file_transaction(out_file) as tx_out_file:
             params = ["-T", "VariantAnnotator",
@@ -25,3 +26,33 @@ def annotate_effects(orig_file, snpeff_file, genome_file, config):
                       "--out", tx_out_file]
             broad_runner.run_gatk(params)
     return out_file
+
+def _fix_snpeff_version_line(line, supported_versions):
+    """Change snpEff versions to supported minor releases.
+
+    ##SnpEffVersion="2.0.3 (build 2011-10-08), by Pablo Cingolani"
+    """
+    start, rest = line.split('"', 1)
+    version, end = rest.split(" ", 1)
+    version_base = version.rsplit(".", 1)[0]
+    for sv in supported_versions:
+        sv_base = sv.rsplit(".", 1)[0]
+        if sv_base == version_base:
+            version = sv
+            break
+    return '%s"%s %s' % (start, version, end)
+
+def _general_snpeff_version(snpeff_file):
+    """GATK wants exact snpEff versions; allow related minor releases.
+    """
+    gatk_versions = ["2.0.2"]
+    safe_snpeff = "%s-safev%s" % os.path.splitext(snpeff_file)
+    if not file_exists(safe_snpeff):
+        with file_transaction(safe_snpeff) as tx_safe:
+            with open(snpeff_file) as in_handle:
+                with open(safe_snpeff, "w") as out_handle:
+                    for line in in_handle:
+                        if line.startswith("##SnpEffVersion"):
+                            line = _fix_snpeff_version_line(line, gatk_versions)
+                        out_handle.write(line)
+    return safe_snpeff
