1d705ac8f6bab17ca1c9cdab181466664687f28c
chapmanb <chapmanb@50mail.com>
2012-01-29 16:44:59 -0500

    Handle variable inputs to structural variation: non-paired reads, alternate sorting; remove outlier read pairs in calculation of expected insert size

--- bcbio/structural/hydra.py
+++ bcbio/structural/hydra.py
@@ -6,6 +6,7 @@ Pipeline: http://code.google.com/p/hydra-sv/wiki/TypicalWorkflow
 """
 import os
 import copy
+import collections
 import subprocess
 from contextlib import nested, closing
 
@@ -24,24 +25,46 @@ def select_unaligned_read_pairs(in_bam, extra, out_dir, config):
     """
     runner = broad.runner_from_config(config)
     base, ext = os.path.splitext(os.path.basename(in_bam))
-    sort_bam = os.path.join(out_dir, "{}-{}{}".format(base, extra, ext))
-    runner.run_fn("picard_sort", in_bam, sort_order="queryname", out_file=sort_bam)
-    out_fq1, out_fq2 = ["{}-{}.fq".format(os.path.splitext(sort_bam)[0], i) for i in [1, 2]]
-    if not utils.file_exists(out_fq1):
-        with nested(closing(pysam.Samfile(in_bam, "rb")),
-                    open(out_fq1, "wb"), open(out_fq2, "wb")) as (in_pysam, out1, out2):
+    nomap_bam = os.path.join(out_dir, "{}-{}{}".format(base, extra, ext))
+    sort_bam = runner.run_fn("picard_sort", in_bam, "queryname")
+    if not utils.file_exists(nomap_bam):
+        with file_transaction(nomap_bam) as tx_out:
+            runner.run("FilterSamReads", [("INPUT", sort_bam),
+                                          ("OUTPUT", tx_out),
+                                          ("EXCLUDE_ALIGNED", "true"),
+                                          ("WRITE_READS_FILES", "false"),
+                                          ("SORT_ORDER", "queryname")])
+    has_reads = False
+    with closing(pysam.Samfile(nomap_bam, "rb")) as in_pysam:
+        for read in in_pysam:
+            if read.is_paired:
+                has_reads = True
+                break
+    if has_reads:
+        out_fq1, out_fq2 = ["{}-{}.fq".format(os.path.splitext(nomap_bam)[0], i) for i in [1, 2]]
+        runner.run_fn("picard_bam_to_fastq", nomap_bam, out_fq1, out_fq2)
+        return out_fq1, out_fq2
+    else:
+        return None, None
+
+def remove_nopairs(in_bam, out_dir):
+    """Remove any reads without both pairs present in the file.
+    """
+    out_bam = os.path.join(out_dir, apply("{}-safepair{}".format,
+                                          os.path.splitext(os.path.basename(in_bam))))
+    if not utils.file_exists(out_bam):
+        read_counts = collections.defaultdict(int)
+        with closing(pysam.Samfile(in_bam, "rb")) as in_pysam:
             for read in in_pysam:
-                if read.is_paired and not read.is_proper_pair:
-                    cur_out = out1 if read.is_read1 else out2
-                    if read.is_reverse:
-                        seq = str(Seq(read.seq).reverse_complement())
-                        qual = "".join(reversed(read.qual))
-                    else:
-                        seq = read.seq
-                        qual = read.qual
-                    cur_out.write("@{name}\n{seq}\n+\n{qual}\n".format(
-                        name=read.qname, seq=seq, qual=qual))
-    return out_fq1, out_fq2
+                if read.is_paired:
+                    read_counts[read.qname] += 1
+        with closing(pysam.Samfile(in_bam, "rb")) as in_pysam:
+            with file_transaction(out_bam) as tx_out_bam:
+                with closing(pysam.Samfile(tx_out_bam, "wb", template=in_pysam)) as out_pysam:
+                    for read in in_pysam:
+                        if read_counts[read.qname] == 2:
+                            out_pysam.write(read)
+    return out_bam
 
 def calc_paired_insert_stats(in_bam):
     """Retrieve statistics for paired end read insert distances.
@@ -50,9 +73,12 @@ def calc_paired_insert_stats(in_bam):
     with closing(pysam.Samfile(in_bam, "rb")) as in_pysam:
         for read in in_pysam:
             if read.is_proper_pair and read.is_read1:
-                dists.append(read.isize)
-    return {"mean": numpy.mean(dists), "std": numpy.std(dists),
-            "median": numpy.median(dists)}
+                dists.append(abs(read.isize))
+    # remove outliers
+    med = numpy.median(dists)
+    filter_dists = filter(lambda x: x < med + 10 * med, dists)
+    return {"mean": numpy.mean(filter_dists), "std": numpy.std(filter_dists),
+            "median": numpy.median(filter_dists)}
 
 def tiered_alignment(in_bam, tier_num, multi_mappers, extra_args,
                      genome_build, pair_stats,
@@ -61,17 +87,20 @@ def tiered_alignment(in_bam, tier_num, multi_mappers, extra_args,
     """
     nomap_fq1, nomap_fq2 = select_unaligned_read_pairs(in_bam, "tier{}".format(tier_num),
                                                        work_dir, config)
-    base_name = "{}-tier{}out".format(os.path.splitext(os.path.basename(in_bam))[0],
-                                      tier_num)
-    config = copy.deepcopy(config)
-    dirs = copy.deepcopy(dirs)
-    config["algorithm"]["multiple_mappers"] = multi_mappers
-    config["algorithm"]["extra_align_args"] = ["-i", int(pair_stats["mean"]),
+    if nomap_fq1 is not None:
+        base_name = "{}-tier{}out".format(os.path.splitext(os.path.basename(in_bam))[0],
+                                          tier_num)
+        config = copy.deepcopy(config)
+        dirs = copy.deepcopy(dirs)
+        config["algorithm"]["multiple_mappers"] = multi_mappers
+        config["algorithm"]["extra_align_args"] = ["-i", int(pair_stats["mean"]),
                                                int(pair_stats["std"])] + extra_args
-    dirs["align"] = os.path.split(nomap_fq1)[0]
-    return align_to_sort_bam(nomap_fq1, nomap_fq2, genome_build, "novoalign",
-                             base_name, base_name,
-                             dirs, config)
+        dirs["align"] = os.path.split(nomap_fq1)[0]
+        return align_to_sort_bam(nomap_fq1, nomap_fq2, genome_build, "novoalign",
+                                 base_name, base_name,
+                                 dirs, config)
+    else:
+        return None
 
 ## Run hydra to identify structural variation breakpoints
 
@@ -129,10 +158,13 @@ def detect_sv(align_bam, genome_build, dirs, config):
     """
     work_dir = utils.safe_makedir(os.path.join(dirs["work"], "structural"))
     pair_stats = calc_paired_insert_stats(align_bam)
-    tier2_align = tiered_alignment(align_bam, "2", True, [],
+    fix_bam = remove_nopairs(align_bam, work_dir)
+    tier2_align = tiered_alignment(fix_bam, "2", True, [],
                                    genome_build, pair_stats,
                                    work_dir, dirs, config)
-    tier3_align = tiered_alignment(tier2_align, "3", "Ex 1100", ["-t", "300"],
-                                   genome_build, pair_stats,
-                                   work_dir, dirs, config)
-    hydra_bps = hydra_breakpoints(tier3_align, pair_stats)
+    if tier2_align:
+        tier3_align = tiered_alignment(tier2_align, "3", "Ex 1100", ["-t", "300"],
+                                       genome_build, pair_stats,
+                                       work_dir, dirs, config)
+        if tier3_align:
+            hydra_bps = hydra_breakpoints(tier3_align, pair_stats)
