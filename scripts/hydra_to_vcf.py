ab5ba4cd1b73027370c98425168742f7cd6b24c2
chapmanb <chapmanb@50mail.com>
2012-02-22 08:11:43 -0500

    Write VCF output for hydra defined structural variants

--- scripts/hydra_to_vcf.py
+++ scripts/hydra_to_vcf.py
@@ -9,7 +9,6 @@ vcf-variant-call-format-version-41
 
 Requirements:
 
-PyVCF: https://github.com/jamescasbon/PyVCF
 bx-python: https://bitbucket.org/james_taylor/bx-python/wiki/Home
 
 Usage:
@@ -22,11 +21,14 @@ import unittest
 from collections import namedtuple
 
 from bx.seq import twobit
-import vcf
 
 def main(hydra_file, genome_file):
     out_file = "{0}.vcf".format(os.path.splitext(hydra_file)[0])
     genome_2bit = twobit.TwoBitFile(open(genome_file))
+    with open(out_file, "w") as out_handle:
+        hydra_to_vcf_writer(hydra_file, genome_2bit, out_handle)
+
+# ## Build VCF representation from Hydra BedPe format
 
 def _vcf_info(start, end, mate_id):
     """Return breakend information line with mate and imprecise location.
@@ -35,13 +37,15 @@ def _vcf_info(start, end, mate_id):
         mate=mate_id, size=end-start)
 
 def _vcf_alt(base, other_chr, other_pos, other_isrc, is_first):
+    """Create ALT allele line in VCF 4.1 format associating with other paired end.
+    """
     if is_first:
         pipe = "]" if other_isrc else "["
         out_str = "{base}{pipe}{chr}:{pos}{pipe}"
     else:
         pipe = "[" if other_isrc else "]"
         out_str = "{pipe}{chr}:{pos}{pipe}{base}"
-    return out_str.format(pipe=pipe, chr=other_chr, pos=other_pos,
+    return out_str.format(pipe=pipe, chr=other_chr, pos=other_pos + 1,
                           base=base)
 
 def build_vcf_parts(feature, genome_2bit):
@@ -66,6 +70,8 @@ def build_vcf_parts(feature, genome_2bit):
                                  feature.strand1 == "-", False),
                         _vcf_info(feature.start2, feature.end2, id1)))
 
+# ## Parse Hydra output into BedPe tuple representation
+
 def hydra_parser(in_file):
     """Parse hydra input file into namedtuple of values.
     """
@@ -79,12 +85,43 @@ def hydra_parser(in_file):
                         line[3], int(line[4]), int(line[5]),
                         line[6], line[8], line[9])
 
+# ## Write VCF output
+
+def _write_vcf_header(out_handle):
+    """Write VCF header information for Hydra structural variant.
+    """
+    def w(line):
+        out_handle.write("{0}\n".format(line))
+    w('##fileformat=VCFv4.1')
+    w('##INFO=<ID=CIPOS,Number=2,Type=Integer,'
+      'Description="Confidence interval around POS for imprecise variants">')
+    w('##INFO=<ID=IMPRECISE,Number=0,Type=Flag,Description="Imprecise structural variation">')
+    w('##INFO=<ID=SVTYPE,Number=1,Type=String,Description="Type of structural variant">')
+    w('##INFO=<ID=MATEID,Number=.,Type=String,Description="ID of mate breakends">')
+    w('##source=hydra')
+    w("#" + "\t".join(["CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO"]))
+
+def _write_vcf_breakend(brend, out_handle):
+    """Write out a single VCF line with breakpoint information.
+    """
+    out_handle.write("{0}\n".format("\t".join(str(x) for x in
+        [brend.chrom, brend.pos + 1, brend.id, brend.ref, brend.alt,
+         ".", "PASS", brend.info])))
+
+def hydra_to_vcf_writer(hydra_file, genome_2bit, out_handle):
+    """Write hydra output as non-sorted VCF file.
+    """
+    _write_vcf_header(out_handle)
+    for feature in hydra_parser(hydra_file):
+        for brend in build_vcf_parts(feature, genome_2bit):
+            _write_vcf_breakend(brend, out_handle)
+
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         print "Incorrect arguments"
         print __doc__
         sys.exist()
-    main(sys.argv[1:])
+    main(*sys.argv[1:])
 
 # ## Test code
 
@@ -114,5 +151,5 @@ class HydraConvertTest(unittest.TestCase):
         genome_2bit = twobit.TwoBitFile(open(self.genome_file))
         breakend = hydra_parser(self.in_file).next()
         brend1, brend2 = build_vcf_parts(breakend, genome_2bit)
-        assert brend1.alt == "G]chr22:10111]"
+        assert brend1.alt == "G]chr22:10112]"
         assert brend2.info == "SVTYPE=BND;MATEID=hydra2a;IMPRECISE;CIPOS=0,102"
