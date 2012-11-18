b47994891ed03912ed5fc0c64b02942e81cedc13
chapmanb <chapmanb@50mail.com>
2012-11-18 06:42:41 -0500

    Extract out and generalize main functionality, allowing it to be called directly from Python code in addition to run via the commandline

--- scripts/automated_initial_analysis.py
+++ scripts/automated_initial_analysis.py
@@ -6,7 +6,8 @@ run from the Galaxy description, and uses this to perform an initial alignment
 and analysis.
 
 Usage:
-    automated_initial_analysis.py <YAML config file> <flow cell dir>
+    automated_initial_analysis.py <YAML config file>
+                                  [<flow cell dir>]
                                   [<YAML run information>]
 
 The optional <YAML run information> file specifies details about the
@@ -21,79 +22,19 @@ Workflow:
 """
 import os
 import sys
-from optparse import OptionParser
 
-import yaml
-
-from bcbio.solexa.flowcell import get_fastq_dir
-from bcbio import utils
-from bcbio.log import logger, setup_logging
-from bcbio.distributed.messaging import parallel_runner
-from bcbio.pipeline.run_info import get_run_info
-from bcbio.pipeline.demultiplex import add_multiplex_across_lanes
-from bcbio.pipeline.merge import organize_samples
-from bcbio.pipeline.qcsummary import write_metrics, write_project_summary
-from bcbio.variation.realign import parallel_realign_sample
-from bcbio.variation.genotype import parallel_variantcall, combine_multiple_callers
 from bcbio.pipeline.config_loader import load_config
+from bcbio.pipeline.main import run_main, parse_cl_args
 
-def main(config_file, fc_dir, run_info_yaml=None, num_cores=None):
+def main(config_file, fc_dir=None, run_info_yaml=None, num_cores=None):
     config = load_config(config_file)
     work_dir = os.getcwd()
     if config.get("log_dir", None) is None:
         config["log_dir"] = os.path.join(work_dir, "log")
     if num_cores:
         config["algorithm"]["num_cores"] = int(num_cores)
-    setup_logging(config)
-    run_main(config, config_file, fc_dir, work_dir, run_info_yaml)
-
-def run_main(config, config_file, fc_dir, work_dir, run_info_yaml):
-    align_dir = os.path.join(work_dir, "alignments")
-    run_module = "bcbio.distributed"
-    fc_name, fc_date, run_info = get_run_info(fc_dir, config, run_info_yaml)
-    fastq_dir, galaxy_dir, config_dir = _get_full_paths(get_fastq_dir(fc_dir),
-                                                        config, config_file)
-    config_file = os.path.join(config_dir, os.path.basename(config_file))
-    dirs = {"fastq": fastq_dir, "galaxy": galaxy_dir, "align": align_dir,
-            "work": work_dir, "flowcell": fc_dir, "config": config_dir}
-    run_parallel = parallel_runner(run_module, dirs, config, config_file)
-
-    # process each flowcell lane
-    run_items = add_multiplex_across_lanes(run_info["details"], dirs["fastq"], fc_name)
-    lanes = ((info, fc_name, fc_date, dirs, config) for info in run_items)
-    lane_items = run_parallel("process_lane", lanes)
-    align_items = run_parallel("process_alignment", lane_items)
-    # process samples, potentially multiplexed across multiple lanes
-    samples = organize_samples(align_items, dirs, config_file)
-    samples = run_parallel("merge_sample", samples)
-    samples = run_parallel("recalibrate_sample", samples)
-    samples = parallel_realign_sample(samples, run_parallel)
-    samples = parallel_variantcall(samples, run_parallel)
-    samples = run_parallel("postprocess_variants", samples)
-    samples = combine_multiple_callers(samples)
-    samples = run_parallel("detect_sv", samples)
-    run_parallel("process_sample", samples)
-    run_parallel("generate_bigwig", samples, {"programs": ["ucsc_bigwig"]})
-    write_project_summary(samples)
-    write_metrics(run_info, fc_name, fc_date, dirs)
-
-# ## Utility functions
-
-def _get_full_paths(fastq_dir, config, config_file):
-    """Retrieve full paths for directories in the case of relative locations.
-    """
-    fastq_dir = utils.add_full_path(fastq_dir)
-    config_dir = utils.add_full_path(os.path.dirname(config_file))
-    galaxy_config_file = utils.add_full_path(config["galaxy_config"], config_dir)
-    return fastq_dir, os.path.dirname(galaxy_config_file), config_dir
+    run_main(config, config_file, work_dir, fc_dir, run_info_yaml)
 
 if __name__ == "__main__":
-    parser = OptionParser()
-    parser.add_option("-n", "--numcores", dest="num_cores", default=None)
-    (options, args) = parser.parse_args()
-    if len(args) < 2:
-        print "Incorrect arguments"
-        print __doc__
-        sys.exit()
-    kwargs = {"num_cores": options.num_cores}
-    main(*args, **kwargs)
+    config_file, kwargs = parse_cl_args(sys.argv[1:])
+    main(config_file, **kwargs)
--- scripts/distributed_nextgen_pipeline.py
+++ scripts/distributed_nextgen_pipeline.py
@@ -2,34 +2,34 @@
 """Run an automated analysis pipeline in a distributed cluster architecture.
 
 Usage:
-  distributed_nextgen_pipeline.py <config_file> <fc_dir> [<run_info_yaml>]
+  distributed_nextgen_pipeline.py <config_file> [<fc_dir>] [<run_info_yaml>]
      -n <number of processes to start>
 """
 import sys
-from optparse import OptionParser
 
 import yaml
 
 from bcbio.pipeline.run_info import get_run_info
 from bcbio.distributed.manage import run_and_monitor
 from bcbio.pipeline.config_loader import load_config
+from bcbio.pipeline.main import parse_cl_args
 
-def main(config_file, fc_dir, run_info_yaml=None, num_workers=None):
+def main(config_file, fc_dir=None, run_info_yaml=None, num_cores=None):
     config = load_config(config_file)
     assert config["algorithm"]["num_cores"] == "messaging", \
            "Use this script only with configured 'messaging' parallelization"
-    if num_workers is None:
+    if num_cores is None:
         if config["distributed"].get("num_workers", "") == "all":
             cp = config["distributed"]["cluster_platform"]
             cluster = __import__("bcbio.distributed.{0}".format(cp), fromlist=[cp])
-            num_workers = cluster.available_nodes(config["distributed"]["platform_args"]) - 1
-        if num_workers is None:
-            num_workers = _needed_workers(get_run_info(fc_dir, config, run_info_yaml)[-1])
+            num_cores = cluster.available_nodes(config["distributed"]["platform_args"]) - 1
+        if num_cores is None:
+            num_cores = _needed_workers(get_run_info(fc_dir, config, run_info_yaml)[-1])
     task_module = "bcbio.distributed.tasks"
     args = [config_file, fc_dir]
     if run_info_yaml:
         args.append(run_info_yaml)
-    run_and_monitor(config, config_file, args, num_workers, task_module)
+    run_and_monitor(config, config_file, args, num_cores, task_module)
 
 def _needed_workers(run_info):
     """Determine workers needed to run multiplex flowcells in parallel.
@@ -41,12 +41,5 @@ def _needed_workers(run_info):
     return len(set(names))
 
 if __name__ == "__main__":
-    parser = OptionParser()
-    parser.add_option("-n", "--numworkers", dest="num_workers", default=None)
-    (options, args) = parser.parse_args()
-    if len(args) < 2:
-        print "Incorrect arguments"
-        print __doc__
-        sys.exit()
-    kwargs = {"num_workers": options.num_workers}
-    main(*args, **kwargs)
+    config_file, kwargs = parse_cl_args(sys.argv[1:])
+    main(config_file, **kwargs)
