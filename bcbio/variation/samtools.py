6b845bb84e770b9eee13520bc68fc59baa49579f
chapmanb <chapmanb@50mail.com>
2012-11-23 14:32:11 -0500

    Support for samtools variant calling as part of standard pipeline

--- bcbio/variation/samtools.py
+++ bcbio/variation/samtools.py
@@ -5,6 +5,8 @@ http://samtools.sourceforge.net/mpileup.shtml
 import os
 import subprocess
 
+import sh
+
 from bcbio import broad
 from bcbio.utils import file_exists
 from bcbio.distributed.transaction import file_transaction
@@ -25,9 +27,24 @@ def run_samtools(align_bam, ref_file, config, dbsnp=None, region=None,
             region=region, fname=os.path.basename(align_bam)))
         variant_regions = config["algorithm"].get("variant_regions", None)
         target_regions = subset_variant_regions(variant_regions, region, out_file)
-        if target_regions is not None:
-            pass
-        else:
+        if variant_regions is not None and not os.path.isfile(target_regions):
             write_empty_vcf(out_file)
-        
+        else:
+            with file_transaction(out_file) as tx_out_file:
+                _call_variants_samtools(align_bam, ref_file, config, target_regions,
+                                        tx_out_file)
     return out_file
+
+def _call_variants_samtools(align_bam, ref_file, config, target_regions, out_file):
+    """Call variants with samtools in target_regions.
+    """
+    with open(out_file, "w") as out_handle:
+        mpileup = sh.samtools.mpileup.bake(align_bam,
+                                           f=ref_file, d=1000, L=1000,
+                                           m=3, F=0.0002, D=True, S=True, u=True)
+        if target_regions:
+            mpileup=mpileup.bake(l=target_regions)
+        bcftools = sh.bcftools.view.bake("-", v=True, c=True, g=True,
+                                         _out=out_handle)
+        bcftools(mpileup())
+    
