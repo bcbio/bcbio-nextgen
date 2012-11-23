8e67c64daafc55cb3eebbdd0f32bdaa1bd91bbb3
chapmanb <chapmanb@50mail.com>
2012-11-22 22:27:06 -0500

    Framework for supporting samtools mpileup variation calling

--- bcbio/variation/genotype.py
+++ bcbio/variation/genotype.py
@@ -510,11 +510,12 @@ def parallel_variantcall(sample_info, parallel_fn):
 def variantcall_sample(data, region=None, out_file=None):
     """Parallel entry point for doing genotyping of a region of a sample.
     """
-    from bcbio.variation import freebayes, cortex
+    from bcbio.variation import freebayes, cortex, samtools
     caller_fns = {"gatk": unified_genotyper,
                   "gatk-haplotype": haplotype_caller,
                   "freebayes": freebayes.run_freebayes,
-                  "cortex": cortex.run_cortex}
+                  "cortex": cortex.run_cortex,
+                  "samtools": samtools.run_samtools}
     if data["config"]["algorithm"]["snpcall"]:
         sam_ref = data["sam_ref"]
         config = data["config"]
