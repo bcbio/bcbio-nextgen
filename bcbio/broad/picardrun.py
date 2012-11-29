e561fefd2a2d7873d34cd6b47c3e995a206bdf00
chapmanb <chapmanb@50mail.com>
2012-11-29 06:58:20 -0500

    Remove dependency on samtools, using pysam for header information

--- bcbio/broad/picardrun.py
+++ bcbio/broad/picardrun.py
@@ -1,10 +1,12 @@
 """Convenience functions for running common Picard utilities.
 """
 import os
+from contextlib import closing
+
+import pysam
 
 from bcbio.utils import curdir_tmpdir, file_exists
 from bcbio.distributed.transaction import file_transaction
-import sh
 
 
 def picard_rnaseq_metrics(picard, align_bam, ref, ribo="null", out_file=None):
@@ -207,12 +209,8 @@ def bed2interval(align_file, bed, out_file=None):
     if out_file is None:
         out_file = base + ".interval"
 
-    if ext == ".bam":
-        header = str(sh.samtools("view", "-H", align_file))
-    elif ext == ".sam":
-        header = str(sh.samtools("view", "-H", "-S", align_file))
-    else:
-        raise ValueError("%s is not a BAM or SAM file." % (align_file))
+    with closing(pysam.Samfile(align_file, "r" if ext.endswith(".sam") else "rb")) as in_bam:
+        header = in_bam.text
 
     def reorder_line(line):
         splitline = line.strip().split("\t")
