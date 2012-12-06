975fa54fe231bb5e048e6d1770d3679d06072bc0
chapmanb <chapmanb@50mail.com>
2012-12-06 16:13:47 -0500

    Finalize workflow to prepare combined set of variant calls based on multiple calling approaches

--- bcbio/pipeline/config_loader.py
+++ bcbio/pipeline/config_loader.py
@@ -24,8 +24,8 @@ custom_algorithms:
 galaxy_config, program and analysis supports
 environment variables.
 """
-
 import os
+import glob
 import yaml
 
 def load_config(config_file):
@@ -49,3 +49,51 @@ def expand_path(path):
         return os.path.expandvars(path.replace("~", "$HOME"))
     except AttributeError:
         return path
+
+def get_program(name, config, ptype="cmd"):
+    """Retrieve program information from the configuration.
+
+    This handles back compatible location specification in input
+    YAML. The preferred location for program information is in
+    `resources` but the older `program` tag is also supported.
+    """
+    try:
+        pconfig = config.get("resources", {})[name]
+    except KeyError:
+        pconfig = config.get("program", {}).get("name", None)
+    if ptype == "cmd":
+        return _get_program_cmd(name, pconfig)
+    elif ptype == "dir":
+        return _get_program_dir(name, pconfig)
+    else:
+        raise ValueError("Don't understand program type: %s" % ptype)
+
+def _get_program_cmd(name, config):
+    """Retrieve commandline of a program.
+    """
+    if config.has_key("cmd"):
+        return config["cmd"]
+    elif isinstance(config, basestring):
+        return config
+    else:
+        return name
+
+def _get_program_dir(name, config):
+    """Retrieve directory for a program (local installs/java jars).
+    """
+    if config.has_key("dir"):
+        return config["dir"]
+    elif isinstance(config, basestring):
+        return config
+    else:
+        raise ValueError("Could not find directory in config for %s" % name)
+
+def get_jar(base_name, dname):
+    """Retrieve a jar in the provided directory
+    """
+    jars = glob.glob(os.path.join(dname, "%s*.jar" % base_name))
+    if len(jars) == 1:
+        return jars[0]
+    else:
+        raise ValueError("Could not find java jar %s in %s: %s" % (
+            base_name, dname, jars))
--- bcbio/variation/ensemble.py
+++ bcbio/variation/ensemble.py
@@ -5,11 +5,15 @@ technologies into a single consolidated callset. Uses the bcbio.variation
 toolkit: https://github.com/chapmanb/bcbio.variation
 """
 import os
+import glob
 import copy
+import subprocess
+
 import yaml
 
 from bcbio import utils
 from bcbio.log import logger
+from bcbio.pipeline import config_loader as config_utils
 
 def combine_calls(data):
     """Combine multiple callsets into a final set of merged calls.
@@ -19,9 +23,28 @@ def combine_calls(data):
             ",".join(x["variantcaller"] for x in data["variants"]), data["work_bam"]))
         base_dir = utils.safe_makedir(os.path.join(data["dirs"]["work"], "ensemble"))
         config_file = _write_config_file(data, base_dir)
-        print config_file
-        raise NotImplementedError
+        callinfo = _run_bcbio_variation(config_file, base_dir, data)
+        print callinfo
+        data["variants"] = data["variants"].insert(0, callinfo)
     return [[data]]
+
+def _run_bcbio_variation(config_file, base_dir, data):
+    sample = data["name"][-1]
+    out_vcf_file = os.path.join(base_dir, "{0}-ensembl.vcf".format(sample))
+    out_bed_file = os.path.join(base_dir, "{0}-callregions.bed".format(sample))
+    if not utils.file_exists(out_vcf_file):
+        bv_jar = config_utils.get_jar("bcbio.variation",
+                                      config_utils.get_program("bcbio_variation",
+                                                               data["config"], "dir"))
+        subprocess.check_call(["java", "-jar", bv_jar, "variant-compare", config_file])
+        base_vcf = glob.glob(os.path.join(base_dir, "work", "prep", "*-cfilter.vcf"))[0]
+        base_bed = glob.glob(os.path.join(base_dir, "work", "prep", "*-multicombine.bed"))[0]
+        os.symlink(base_vcf, out_vcf_file)
+        os.symlink(base_bed, out_bed_file)
+
+    return {"variantcaller": "ensemble",
+            "vrn_file": out_vcf_file,
+            "bed_file": out_bed_file}
 
 def _write_config_file(data, base_dir):
     config_dir = utils.safe_makedir(os.path.join(base_dir, "config"))
@@ -47,7 +70,8 @@ def _prep_ensemble_config(sample, variants, align_bam, ref_file, base_dir,
                          "params": {"support": combo_name,
                                     "classifiers": algorithm["ensemble"]["classifiers"],
                                     "xspecific": True,
-                                     "trusted": {"total": algorithm["ensemble"].get("trusted_pct", 0.65)}}}]}
+                                     "trusted":
+                                     {"total": algorithm["ensemble"].get("trusted_pct", 0.65)}}}]}
     if intervals:
         exp["intervals"] = os.path.abspath(intervals)
     for i, v in enumerate(variants):
