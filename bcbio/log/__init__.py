a8807b93010e3ebeebc38d5c570a555c9553d6cf
chapmanb <chapmanb@50mail.com>
2013-01-02 12:41:41 -0500

    Move stray log-dir command from config into generalized resources. Make back compatible with old config approach

--- bcbio/log/__init__.py
+++ bcbio/log/__init__.py
@@ -11,6 +11,11 @@ LOG_NAME = "nextgen_pipeline"
 
 logger = logging.getLogger(LOG_NAME)
 
+def _get_log_dir(config):
+    d = config.get("log_dir",
+                   config.get("resources", {}).get("log", {}).get("dir", "log"))
+    return d
+
 def setup_logging(config):
     logger.setLevel(logging.INFO)
     if not logger.handlers:
@@ -18,7 +23,7 @@ def setup_logging(config):
         handler = logging.StreamHandler()
         handler.setFormatter(formatter)
         logger.addHandler(handler)
-        log_dir = config.get("log_dir", None)
+        log_dir = _get_log_dir(config)
         if log_dir:
             logfile = os.path.join(utils.safe_makedir(log_dir),
                                    "{0}.log".format(LOG_NAME))
@@ -30,17 +35,17 @@ import logbook
 logger2 = logbook.Logger(LOG_NAME)
 
 def create_log_handler(config):
-    log_dir = config.get("log_dir", None)
-    email = config.get("email", None)
+    log_dir = _get_log_dir(config)
+    email = config.get("email", config.get("resources", {}).get("log", {}).get("email"))
 
     if log_dir:
         utils.safe_makedir(log_dir)
         handler = logbook.FileHandler(os.path.join(log_dir, "%s.log" % LOG_NAME))
     else:
         handler = logbook.StreamHandler(sys.stdout)
- 
+
     if email:
-        handler = logbook.MailHandler(email, [email], 
+        handler = logbook.MailHandler(email, [email],
                                       format_string=u'''Subject: [BCBB pipeline] {record.extra[run]} \n\n {record.message}''',
                                       level='INFO', bubble = True)
     return handler
