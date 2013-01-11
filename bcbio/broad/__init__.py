e95cb7c636aac13cd713c9fab0be4609a5be5936
chapmanb <chapmanb@50mail.com>
2013-01-11 08:09:36 -0500

    Provide support for configuring with debian/ubuntu's picard-tools package or a Picard download of jar files. Thanks to @guillermo-carrasco.

--- bcbio/broad/__init__.py
+++ bcbio/broad/__init__.py
@@ -13,11 +13,11 @@ from bcbio.pipeline import config_utils
 class BroadRunner:
     """Simplify running Broad commandline tools.
     """
-    def __init__(self, picard_dir, gatk_dir, resources):
+    def __init__(self, picard_ref, gatk_dir, resources):
         self._jvm_opts = resources.get("jvm_opts", ["-Xmx6g", "-Xms6g"])
         self._resources = resources
-        self._picard_dir = picard_dir
-        self._gatk_dir = gatk_dir or picard_dir
+        self._picard_ref = picard_ref
+        self._gatk_dir = gatk_dir or picard_ref
 
     def run_fn(self, name, *args, **kwds):
         """Run pre-built functionality that used Broad tools by name.
@@ -40,8 +40,7 @@ class BroadRunner:
         """
         options = ["%s=%s" % (x, y) for x, y in options]
         options.append("VALIDATION_STRINGENCY=SILENT")
-        dist_file = self._get_jar(command)
-        cl = ["java"] + self._jvm_opts + ["-jar", dist_file] + options
+        cl = self._get_picard_cmd(command) + options
         if pipe:
             subprocess.Popen(cl)
         elif get_stdout:
@@ -53,8 +52,7 @@ class BroadRunner:
             subprocess.check_call(cl)
 
     def get_picard_version(self, command):
-        dist_file = self._get_jar(command)
-        cl = ["java", "-jar", dist_file, "--version"]
+        cl = self._get_picard_cmd(command) + ["--version"]
         p = subprocess.Popen(cl, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         version = float(p.stdout.read().split("(")[0])
         p.wait()
@@ -95,11 +93,21 @@ class BroadRunner:
                     return True
         return False
 
+    def _get_picard_cmd(self, command):
+        """Retrieve the base Picard command, handling both shell scripts and directory of jars.
+        """
+        if os.path.isdir(self._picard_ref):
+            dist_file = self._get_jar(command)
+            return ["java"] + self._jvm_opts + ["-jar", dist_file]
+        else:
+            # XXX Cannot currently set JVM opts with picard-tools script
+            return [self._picard_ref, command]
+
     def _get_jar(self, command, alts=None):
         """Retrieve the jar for running the specified command.
         """
         dirs = []
-        for bdir in [self._picard_dir, self._gatk_dir]:
+        for bdir in [self._picard_ref, self._gatk_dir]:
             dirs.extend([bdir,
                          os.path.join(bdir, os.pardir, "gatk"),
                          os.path.join(bdir, "dist"),
@@ -112,9 +120,29 @@ class BroadRunner:
                 check_file = os.path.join(dir_check, "%s.jar" % check_cmd)
                 if os.path.exists(check_file):
                     return check_file
-        raise ValueError("Could not find jar %s in %s" % (command, self._picard_dir))
+        raise ValueError("Could not find jar %s in %s:%s" % (command, self._picard_ref, self._gatk_dir))
+
+def _get_picard_ref(config):
+    """Handle retrieval of Picard for running, handling multiple cases:
+
+    - A directory of jar files corresponding to individual commands.
+    - The ubuntu/debian picard-tools commandline, which provides a shell wrapper around
+      a shared jar.
+
+    For a directory, configure resources with:
+      picard:
+        dir: /path/to/jars
+
+    For the debian commandline, configure with:
+      picard:
+        cmd: picard-tools
+    """
+    picard = config_utils.get_program("picard", config, default="notfound")
+    if picard == "notfound" or os.path.isdir(picard):
+        picard = config_utils.get_program("picard", config, "dir")
+    return picard
 
 def runner_from_config(config):
-    return BroadRunner(config_utils.get_program("picard", config, "dir"),
+    return BroadRunner(_get_picard_ref(config),
                        config_utils.get_program("gatk", config, "dir"),
                        config_utils.get_resources("gatk", config))
