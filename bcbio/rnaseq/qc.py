3756973f7c32670c03231c0180b66cfa0629274c
roryk <rory.kirchner@gmail.com>
2013-11-22 15:15:30 -0500

    On whole-file complexity plotting, shuffle the start sites first.

--- bcbio/rnaseq/qc.py
+++ bcbio/rnaseq/qc.py
@@ -132,7 +132,7 @@ def starts_by_depth(bam_file, sample_size=None):
             samples = map(read_parser, filtered)
         else:
             samples = utils.reservoir_sample(filtered, sample_size, read_parser)
-            shuffle(samples)
+        shuffle(samples)
         for read in samples:
             counted += 1
             buffer.append(read)
