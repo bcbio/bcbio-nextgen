8dce20a1c6f515a218cdf9251ba6683b23ef1ebd
chapmanb <chapmanb@50mail.com>
2013-12-03 13:24:11 -0500

    Use standard exist instead of checking file size when transferring directories to final location. Touch final copied directories to avoid multiple transfers with time preserving copytree. Fixes #198

--- bcbio/upload/filesystem.py
+++ bcbio/upload/filesystem.py
@@ -10,7 +10,7 @@ from bcbio.upload import shared
 def copy_finfo(finfo, storage_dir):
     """Copy a file into the output storage directory.
     """
-    if finfo.has_key("sample"):
+    if "sample" in finfo:
         out_file = os.path.join(storage_dir, "%s-%s.%s" % (finfo["sample"], finfo["ext"],
                                                            finfo["type"]))
     else:
@@ -27,9 +27,10 @@ def copy_finfo_directory(finfo, storage_dir):
     out_dir = os.path.abspath(os.path.join(storage_dir, finfo["ext"]))
     if not shared.up_to_date(out_dir, finfo):
         logger.info("Storing directory in local filesystem: %s" % out_dir)
-        if utils.file_exists(out_dir):
+        if os.path.exists(out_dir):
             shutil.rmtree(out_dir)
         shutil.copytree(finfo["path"], out_dir)
+        os.utime(out_dir, None)
     return out_dir
 
 def update_file(finfo, sample_info, config):
@@ -38,9 +39,9 @@ def update_file(finfo, sample_info, config):
     # skip if we have no directory to upload to
     if "dir" not in config:
         return
-    if finfo.has_key("sample"):
+    if "sample" in finfo:
         storage_dir = utils.safe_makedir(os.path.join(config["dir"], finfo["sample"]))
-    elif finfo.has_key("run"):
+    elif "run" in finfo:
         storage_dir = utils.safe_makedir(os.path.join(config["dir"], finfo["run"]))
     else:
         raise ValueError("Unexpected input file information: %s" % finfo)
