955776aece6f514dc90bc7ef7581fb2a064fc7c9
chapmanb <chapmanb@50mail.com>
2013-12-30 06:57:46 -0500

    Provide ability to save configuration locations for standard variables in docker specific scripts, to avoid needing to specify manually

--- scripts/vm/bcbio_nextgen_docker.py
+++ scripts/vm/bcbio_nextgen_docker.py
@@ -15,6 +15,7 @@ import sys
 import time
 
 import requests
+import yaml
 
 # default information about docker container
 DOCKER = {"port": 8085,
@@ -24,13 +25,19 @@ DOCKER = {"port": 8085,
 # ## Running analysis
 
 def run(args):
+    args = add_defaults(args)
     with bcbio_docker(DOCKER, args) as cid:
         print "***", cid
 
 # ## Installation
 
 def install(args):
-    if not args.test:
+    args = add_defaults(args)
+    if not args.datadir:
+        print("Must specify a `--datadir` for installation or save location with `saveconfig`.\n"
+              "bcbio-nextgen not upgraded.")
+        return
+    if args.install_tools:
         pull(DOCKER["image"])
     success = True
     with bcbio_docker(DOCKER, args) as cid:
@@ -54,6 +61,59 @@ def install_data(args, port):
 def pull(image):
     print("Retrieving bcbio-nextgen docker images with code and tools")
     subprocess.check_call(["docker", "pull", image])
+
+# ## Defaults
+
+TOSAVE_DEFAULTS = {"port": 8085, "datadir": None}
+
+def save_defaults(args):
+    """Save user specific defaults to a yaml configuration file.
+    """
+    out = get_defaults()
+    for k in TOSAVE_DEFAULTS:
+        karg = getattr(args, k, None)
+        if karg and karg != TOSAVE_DEFAULTS[k]:
+            out[k] = karg
+    if len(out) > 0:
+        with open(_get_config_file(just_filename=True), "w") as out_handle:
+            yaml.dump(out, out_handle, default_flow_style=False, allow_unicode=False)
+
+def add_defaults(args):
+    """Add user configured defaults to supplied command line arguments.
+    """
+    config_defaults = get_defaults()
+    for k in TOSAVE_DEFAULTS:
+        karg = getattr(args, k, None)
+        if not karg or karg == TOSAVE_DEFAULTS[k]:
+            if k in config_defaults:
+                setattr(args, k, config_defaults[k])
+    return args
+
+def get_defaults():
+    """Retrieve saved default configurations.
+    """
+    config_file = _get_config_file()
+    if config_file:
+        with open(config_file) as in_handle:
+            return yaml.load(in_handle)
+    else:
+        return {}
+
+def _get_config_file(just_filename=False):
+    """Retrieve standard user configuration file.
+    Uses location from appdirs (https://github.com/ActiveState/appdirs). Could
+    pull this in as dependency for more broad platform support.
+    """
+    config_dir = os.path.join(os.getenv('XDG_CONFIG_HOME', os.path.expanduser("~/.config")),
+                              "bcbio-nextgen")
+    if not os.path.exists(config_dir):
+        os.makedirs(config_dir)
+    config_file = os.path.join(config_dir, "bcbio-docker-config.yaml")
+    if just_filename or os.path.exists(config_file):
+        return config_file
+    else:
+        return None
+
 
 # ## Start and stop bcbio-nextgen docker container
 
@@ -126,29 +186,31 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Automatic installation for bcbio-nextgen pipelines, with docker.")
     parser.add_argument("--port", default=8085, help="External port to connect to docker image.")
-    parser.add_argument("--test", default=False, action="store_true",
-                        help="Testing only option, work with pre-running containers.")
+    parser.add_argument("--datadir", help="Directory to install genome data and associated files.",
+                        type=lambda x: (os.path.abspath(os.path.expanduser(x))))
     subparsers = parser.add_subparsers(title="[sub-commands]")
     # installation
     parser_i = subparsers.add_parser("install", help="Install or upgrade bcbio-nextgen docker container and data.")
-    parser_i.add_argument("datadir", help="Directory to install genome data",
-                          type=lambda x: (os.path.abspath(os.path.expanduser(x))))
     parser_i.add_argument("--genomes", help="Genomes to download",
                           action="append", default=["GRCh37"])
     parser_i.add_argument("--aligners", help="Aligner indexes to download",
                           action="append", default=["bwa"])
-    parser_i.add_argument("--nodata", help="Do not install data dependencies",
-                          dest="install_data", action="store_false", default=True)
+    parser_i.add_argument("--data", help="Install or upgrade data dependencies",
+                          dest="install_data", action="store_true", default=False)
+    parser_i.add_argument("--tools", help="Install or upgrade tool dependencies",
+                          dest="install_tools", action="store_true", default=False)
     parser_i.set_defaults(func=install)
     # running
     parser_r = subparsers.add_parser("run", help="Run an automated analysis.")
-    parser_r.add_argument("datadir", help="Directory with installed genome data and configuration.",
-                          type=lambda x: (os.path.abspath(os.path.expanduser(x))))
     parser_r.add_argument("run_config", help="YAML file with details about samples to process.")
     parser_r.add_argument("--fcdir", help="A directory of Illumina output or fastq files to process")
     parser_r.add_argument("--globalconfig", help="Global YAML configuration file specifying details. "
                           "Defaults to installed bcbio_system.yaml.")
     parser_r.set_defaults(func=run)
+    # configuration
+    parser_c = subparsers.add_parser("saveconfig", help="Save standard configuration variables for current user. "
+                                     "Avoids need to specify on the command line in future runs.")
+    parser_c.set_defaults(func=save_defaults)
     if len(sys.argv) == 1:
         parser.print_help()
     else:
