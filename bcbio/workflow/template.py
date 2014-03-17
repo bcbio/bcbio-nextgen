f29bd039d5ff01289a0aa07aad97c7624526b1f2
chapmanb <chapmanb@50mail.com>
2014-03-17 05:33:58 -0400

    Enable specifying genome build in template CSV files

--- bcbio/workflow/template.py
+++ bcbio/workflow/template.py
@@ -244,7 +244,7 @@ def _add_metadata(item, metadata):
     """
     item_md = metadata.get(item["description"],
                            metadata.get(os.path.basename(item["files"][0]), {}))
-    TOP_LEVEL = set(["description"])
+    TOP_LEVEL = set(["description", "genome_build"])
     if len(item_md) > 0:
         if "metadata" not in item:
             item["metadata"] = {}
