11d3bb191703d512381aeb7977a2233b6c410a68
roryk <rory.kirchner@gmail.com>
2014-03-26 14:44:49 -0400

    Revert "Add finished to timing information."
    This reverts commit a98b71e9e7381542e42cadc22b2994b1efd32982.
    
    Accidentally removed _setup_resources. Thanks to @hammer for
    noticing.

--- bcbio/pipeline/main.py
+++ bcbio/pipeline/main.py
@@ -46,6 +46,19 @@ def run_main(workdir, config_file=None, fc_dir=None, run_info_yaml=None,
     else:
         raise ValueError("Unexpected type of parallel run: %s" % parallel["type"])
 
+def _setup_resources():
+    """Attempt to increase resource limits up to hard limits.
+
+    This allows us to avoid out of file handle limits where we can
+    move beyond the soft limit up to the hard limit.
+    """
+    target_procs = 10240
+    cur_proc, max_proc = resource.getrlimit(resource.RLIMIT_NPROC)
+    target_proc = min(max_proc, target_procs) if max_proc > 0 else target_procs
+    resource.setrlimit(resource.RLIMIT_NPROC, (max(cur_proc, target_proc), max_proc))
+    cur_hdls, max_hdls = resource.getrlimit(resource.RLIMIT_NOFILE)
+    target_hdls = min(max_hdls, target_procs) if max_hdls > 0 else target_procs
+    resource.setrlimit(resource.RLIMIT_NOFILE, (max(cur_hdls, target_hdls), max_hdls))
 
 def _run_toplevel(config, config_file, work_dir, parallel,
                   fc_dir=None, run_info_yaml=None):
@@ -405,7 +418,6 @@ class RnaseqPipeline(AbstractPipeline):
                         samples, config, dirs, "persample") as run_parallel:
             with profile.report("quality control", dirs):
                 samples = qcsummary.generate_parallel(samples, run_parallel)
-        logger.info("Timing: finished")
         return samples
 
 class ChipseqPipeline(AbstractPipeline):
