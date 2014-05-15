e2ace2e17d63432039bc930cc388ddf5490fce25
chapmanb <chapmanb@50mail.com>
2014-05-14 22:32:28 -0400

    Avoid too long commandlines by writing regions to intermediate file before passing to samtools commandline #386

--- bcbio/pipeline/shared.py
+++ bcbio/pipeline/shared.py
@@ -88,7 +88,10 @@ def write_noanalysis_reads(in_file, region_file, out_file, config):
             bedtools = config_utils.get_program("bedtools", config)
             samtools = config_utils.get_program("samtools", config)
             region_str = " ".join("%s:%s-%s" % tuple(r) for r in pybedtools.BedTool(region_file))
-            cl = ("{samtools} view -b {in_file} {region_str} | "
+            region_str_file = "%s-region_str.txt" % utils.splitext_plus(tx_out_file)[0]
+            with open(region_str_file, "w") as out_handle:
+                out_handle.write(region_str)
+            cl = ("{samtools} view -b {in_file} `cat {region_str_file}` | "
                   "{bedtools} intersect -abam - -b {region_file} -f 1.0 "
                   "> {tx_out_file}")
             do.run(cl.format(**locals()), "Select unanalyzed reads")
