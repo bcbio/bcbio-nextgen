79738ddc1b3e6abbbb2f0d9f5addeb6410cbd246
roryk <rory.kirchner@gmail.com>
2014-05-30 19:52:47 -0400

    Bump STAR minimum memory requirement to 40G.
    It seems like .z4 uses slightly more memory.

--- bcbio/pipeline/main.py
+++ bcbio/pipeline/main.py
@@ -422,7 +422,7 @@ class RnaseqPipeline(AbstractPipeline):
                 samples = run_parallel("process_lane", samples)
                 samples = run_parallel("trim_lane", samples)
         with prun.start(_wres(parallel, ["aligner"],
-                              ensure_mem={"tophat": 8, "tophat2": 8, "star": 30}),
+                              ensure_mem={"tophat": 8, "tophat2": 8, "star": 40}),
                         samples, config, dirs, "multicore",
                         multiplier=alignprep.parallel_multiplier(samples)) as run_parallel:
             with profile.report("alignment", dirs):
