1b7b48501c4596184400e178d7f99fd255b63fdc
chapmanb <chapmanb@50mail.com>
2014-07-08 15:45:41 -0400

    Ensure paired tumor/normal varscan output does not have non-GATC allele calls. Fixes #491

--- bcbio/variation/varscan.py
+++ bcbio/variation/varscan.py
@@ -160,7 +160,6 @@ def _varscan_paired(align_bams, ref_file, items, target_regions, out_file):
 
         _add_reject_flag(out_file, config)
 
-
 def _fix_varscan_vcf(orig_file, normal_name, tumor_name):
     """Fixes issues with the standard VarScan VCF output.
 
@@ -177,13 +176,11 @@ def _fix_varscan_vcf(orig_file, normal_name, tumor_name):
                 with open(tx_out_file, "w") as out_handle:
 
                     for line in in_handle:
-                        line = _fix_varscan_output(line, normal_name,
-                                                   tumor_name)
+                        line = _clean_varscan_line(_fix_varscan_output(line, normal_name,
+                                                                       tumor_name))
                         if not line:
                             continue
                         out_handle.write(line)
-
-
 
 def _add_reject_flag(in_file, config):
 
