16adf58ad2d8f100e8c011f523fb83cffccb8b6b
John Kern <kern3020@gmail.com>
2014-07-11 07:20:48 -0700

    renamed variable based on code review

--- bcbio/variation/coverage.py
+++ bcbio/variation/coverage.py
@@ -29,20 +29,20 @@ def summary (samples, run_parallel):
         sample_name = tz.get_in(['rgnames','sample'], data[0], None)
         bed_file = tz.get_in(["config", "algorithm", "coverage"], data[0], None)
 
-        outport_dir = os.path.abspath(tz.get_in(['upload', 'dir'], data[0]))
-        if not os.path.exists(outport_dir):
-            safe_makefir(outport_dir)
+        output_dir = os.path.abspath(tz.get_in(['upload', 'dir'], data[0]))
+        if not os.path.exists(output_dir):
+            safe_makefir(output_dir)
 
-        outport = os.path.join(outport_dir, sample_name, '{0}-coverage.bed'.format(sample_name))
-        if not utils.file_exists(outport):
-            with file_transaction(outport) as tx_out_file:
+        output = os.path.join(output_dir, sample_name, '{0}-coverage.bed'.format(sample_name))
+        if not utils.file_exists(output):
+            with file_transaction(output) as tx_out_file:
                 cmd = [cmd_name,'annotate', bam, bed_file, "-o", tx_out_file, '-s', sample_name]
                 do.run(cmd, "Summarizing coverage with {0}".format(cmd_name), samples[0])
     out = []
     for x in samples[0]:
-        outport_dir = os.path.abspath(tz.get_in(['upload', 'dir'], data[0]))
-        outport = os.path.join(outport_dir, sample_name, '{0}-coverage.bed'.format(sample_name))
-        x["coverage"] = {"summary": outport}
+        output_dir = os.path.abspath(tz.get_in(['upload', 'dir'], data[0]))
+        output = os.path.join(output_dir, sample_name, '{0}-coverage.bed'.format(sample_name))
+        x["coverage"] = {"summary": output}
         out.append([x])
     return out
 
