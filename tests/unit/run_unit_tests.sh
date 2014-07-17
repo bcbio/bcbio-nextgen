b5e1ae3b77e4e051c985c80d908e2873a0560b94
roryk <rory.kirchner@gmail.com>
2014-07-17 15:18:37 -0400

    Pull from repository if bcbio-nextgen-test-data already exists.

--- tests/unit/run_unit_tests.sh
+++ tests/unit/run_unit_tests.sh
@@ -4,5 +4,9 @@
 BCBIO_DIR=$(dirname $(readlink -f `which bcbio_nextgen.py`))
 unset PYTHONHOME
 unset PYTHONPATH
-git clone https://github.com/roryk/bcbio-nextgen-test-data.git
+if [ -d "bcbio-nextgen-test-data" ]; then
+    cd bcbio-nextgen-test-data; git pull; cd ../
+else
+    git clone https://github.com/roryk/bcbio-nextgen-test-data.git
+fi
 $BCBIO_DIR/nosetests -v -s -a unit
