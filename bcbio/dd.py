d9e03499b6b247ce6e6eb612af92b1d42cd0cc38
roryk <rory.kirchner@gmail.com>
2014-07-18 15:13:54 -0400

    Add optional checking functions to the data dictionary.

--- bcbio/dd.py
+++ bcbio/dd.py
@@ -3,11 +3,16 @@ functions to access the data dictionary in a clearer way
 """
 
 import toolz as tz
+from bcbio.utils import file_exists
+from bcbio.log import logger
+import sys
 
 def make_access_methods(cls):
     for k, v in cls.LOOKUPS.items():
-        setattr(cls, "get_" + k, getter(cls, v))
-        setattr(cls, "set_" + k, setter(cls, v))
+        keys = v['keys']
+        checker = v.get('checker', None)
+        setattr(cls, "get_" + k, getter(cls, keys))
+        setattr(cls, "set_" + k, setter(cls, keys, checker))
     return cls
 
 def getter(cls, keys):
@@ -15,14 +20,19 @@ def getter(cls, keys):
         return tz.get_in(keys, config, default)
     return lookup
 
-def setter(cls, keys):
+def setter(cls, keys, checker):
+    print checker
     def update(cls, config, value):
+        if checker and not checker(value):
+            logger.error("%s fails check %s." % (value, checker))
+            sys.exit(1)
         return tz.update_in(config, keys, lambda x: value, default=value)
     return update
 
 @make_access_methods
 class ConfigParser(object):
-    LOOKUPS = {"gtf_file": ['genome_resources', 'rnaseq', 'transcripts']}
+    LOOKUPS = {"gtf_file": {"keys": ['genome_resources', 'rnaseq', 'transcripts'],
+                            "checker": file_exists}}
 
     def __init__(self):
         pass
