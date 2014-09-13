d8abf1305e823f9b353743ac41ceeaee1c473434
chapmanb <chapmanb@50mail.com>
2014-09-13 07:52:44 -0400

    Support Psuedomonas genome build

--- scripts/bcbio_nextgen_install.py
+++ scripts/bcbio_nextgen_install.py
@@ -234,7 +234,7 @@ if __name__ == "__main__":
     parser.add_argument("--genomes", help="Genomes to download",
                         action="append", default=[],
                         choices=["GRCh37", "hg19", "mm10", "mm9", "rn5", "canFam3", "dm3", "Zv9", "phix", "sacCer3",
-                                 "xenTro3", "TAIR10", "WBcel235"])
+                                 "xenTro3", "TAIR10", "WBcel235", "pseudomonas_aeruginosa_ucbpp_pa14"])
     parser.add_argument("--aligners", help="Aligner indexes to download",
                         action="append", default=[],
                         choices=["bowtie", "bowtie2", "bwa", "novoalign", "snap", "star", "ucsc"])
