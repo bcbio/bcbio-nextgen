64c678ed51eb21d695aa305c444a9ecbf95e4484
Lorena Pantano <lorena.pantano@gmail.com>
2014-09-22 09:53:51 -0400

    pindel integration as indels caller together with mutect

--- bcbio/structural/pindel.py
+++ bcbio/structural/pindel.py
@@ -5,22 +5,32 @@ http://gmt.genome.wustl.edu/packages/pindel/
 
 from __future__ import print_function
 import os
+import time
+import itertools
+import shutil
 
 try:
     import vcf
 except ImportError:
     vcf = None
 
-from bcbio import bam, utils
-from bcbio.distributed.transaction import file_transaction
+from bcbio import bam, utils, broad
+from bcbio.distributed.transaction import file_transaction, tx_tmpdir
 from bcbio.pipeline import config_utils
 from bcbio.pipeline.shared import subset_variant_regions, remove_lcr_regions
+from bcbio.variation.vcfutils import bgzip_and_index, get_paired_bams
+from bcbio.variation import annotation
 from bcbio.provenance import do
-#from bcbio.variation import annotation
-#from bcbio.variation.vcfutils import get_paired_bams, is_paired_analysis, bgzip_and_index
 
 
-def _pindel_options_from_config(items, config, out_file, region, tmp_path):
+def _pindel_options(items, config, out_file, region, tmp_path):
+    """parse pindel options. Add region to cmd.
+    :param items: (dict) information from yaml
+    :param config: (dict) information from yaml (items[0]['config'])
+    :param region: (str or tupple) region to analyze
+    :param tmp_path: (str) temporal folder
+    :returns: (list) options for pindel
+    """
     variant_regions = utils.get_in(config, ("algorithm", "variant_regions"))
     target = subset_variant_regions(variant_regions, region, out_file, items)
     opts = ""
@@ -36,118 +46,122 @@ def _pindel_options_from_config(items, config, out_file, region, tmp_path):
                 chrom, start, end = region
                 with open(tx_tmp_bed, "w") as out_handle:
                     print("%s\t%s\t%s" % (chrom, start, end), file=out_handle)
-        opts = "-j" + remove_lcr_regions(target_bed, items)
+        opts = "-j " + remove_lcr_regions(target_bed, items)
     return opts
-
-
-def _pindel_regions(bed_file):
-    """get regions from bed file"""
-    regions = set()
-    with open(bed_file) as in_handle:
-        for line in in_handle:
-            cols = line.strip().split("\t")
-            regions.add((cols[0], int(cols[1]), int(cols[2])))
-    return regions
 
 
 def is_installed(config):
     """Check for pindel installation on machine.
+    :param config: (dict) information from yaml(items[0]['config'])
+    :returns: (boolean) if pindel is installed
     """
     try:
         config_utils.get_program("pindel", config)
         return True
     except config_utils.CmdNotFound:
         return False
+    try:
+        config_utils.get_program("pindel2vcf", config)
+        return True
+    except config_utils.CmdNotFound:
+        return False
 
 
-def run(items):
-    """in case go inside structural variation"""
-    if not all(utils.get_in(data, ("config", "algorithm", "aligner")) == "bwa" for data in items):
-        raise ValueError("Require bwa-mem alignment input for lumpy structural variation detection")
-    work_dir = utils.safe_makedir(os.path.join(items[0]["dirs"]["work"], "structural", items[0]["name"][-1],
-                                               "pindel"))
-    work_bams = [x["align_bam"] for x in items]
-    ref_file = items[0]["sam_ref"]
-    out_file = os.path.join(work_dir, items[0]["name"][-1])
-    out_file = _run_pindel_caller(work_bams, items, ref_file, out_file=out_file)
-    return items
-
-
-def run_pindel(align_bams, items, ref_file, assoc_files, region=None,
-               out_file):
-    """Run pindel calling, either paired tumor/normal or germline calling.
+def _run_tumor_pindel_caller(align_bams, items, ref_file, assoc_files,
+                             region=None, out_file=None):
+    """Detect indels with pindel in tumor/[normal] analysis
+    :param align_bam: (list) bam files
+    :param items: (dict) information from yaml
+    :param ref_file: (str) genome in fasta format
+    :param assoc_file: (dict) files for annotation
+    :param region: (str or tupple) region to analyze
+    :param out_file: (str) final vcf file
+    :returns: (str) final vcf file
     """
     config = items[0]["config"]
-    out_file = run_pindel_caller(align_bams, items, ref_file, out_file)
-    return merge_call_file
-
-
-def _run_pindel_caller(align_bams, items, ref_file,
-                       out_file=None):
-    """Detect SV with pindel.
-
-    Single sample mode.
-    """
-    print("parameters{align_bams} {ref_file} {region}".format(**locals()))
-    config = items[0]["config"]
+    paired = get_paired_bams(align_bams, items)
     if out_file is None:
-        out_file = "%s-pindelroot" % os.path.splitext(align_bams[0])[0]
-    if not utils.file_exists(out_file + "_SI"):
-        with file_transaction(config, out_file) as tx_out_file:
+        out_file = "%s-indels.vcf" % os.path.splitext(align_bams[0])[0]
+    paired_bam = [paired.tumor_bam]
+    paired_name = [paired.tumor_name]
+    if paired.normal_bam:
+        paired_bam.append(paired.normal_bam)
+        paired_name.append(paired.normal_name)
+    if not utils.file_exists(out_file):
+        with tx_tmpdir(config) as tmp_path:
             for align_bam in align_bams:
                 bam.index(align_bam, config)
+            root_pindel = os.path.join(tmp_path, "pindelroot")
             pindel = config_utils.get_program("pindel", config)
-            tmp_path = os.path.dirname(tx_out_file)
-            opts = _pindel_options_from_config(items, config, out_file, region, tmp_path)
-            sample_name_str = items[0]["name"][1]
-            tmp_input = _create_tmp_input(align_bams, tmp_path, config)
-            cmd = ("{pindel} -f {ref_file} -i {tmp_input} -o {out_file} {opts} -r false -l false -k false -t false ")
-            print("final command for pindel %s" % cmd.format(**locals()))
+            opts = _pindel_options(items, config, out_file, region, tmp_path)
+            tmp_input = _create_tmp_input(paired_bam, paired_name, tmp_path, config)
+            cmd = ("{pindel} -f {ref_file} -i {tmp_input} -o {root_pindel} " +
+                   "{opts} -r false -l false -k false -t false -I false")
             do.run(cmd.format(**locals()), "Genotyping with pindel", {})
-    out_file = _create_vcf(out_file)
-    return out_file
+            out_file = _create_vcf(root_pindel, out_file, ref_file,
+                                   items, paired)
+    ann_file = annotation.annotate_nongatk_vcf(out_file, align_bams,
+                                               assoc_files.get("dbsnp"),
+                                               ref_file, config)
+    return ann_file
 
 
-def _create_tmp_input(input_bams, tmp_path, config):
-    """Create input file for pindel. tab file: bam file, insert size, name"""
+def _create_tmp_input(input_bams, names, tmp_path, config):
+    """Create input file for pindel. tab file: bam file, insert size, name
+    :param input_bams: (list) bam files
+    :param names: (list) names of samples
+    :param tmp_path: (str) temporal dir
+    :param config: (dict) information from yaml file(itmes[0]['config'])
+    :returns: (str) input file for pindel
+    """
     tmp_input = os.path.join(tmp_path, "pindel.txt")
     with open(tmp_input, 'w') as out_handle:
-        for bam_file in input_bams:
-            name_bam = utils.splitext_plus(os.path.basename(bam_file))[0]
-            print("%s\t%s\t%s\n" % (bam_file, 250, name_bam), file=out_handle)
-            print("%s\t%s\t%s\n" % (bam_file, 250, name_bam))
+        for bam_file, name in itertools.izip(input_bams, names):
+            print("%s\t%s\t%s\n" % (bam_file, 250, name), file=out_handle)
     return tmp_input
 
 
-def _create_vcf(root_file, reference, name_ref, data_ref):
-    """use pindel2vcf to create vcf format"""
-    in_file = root_file + "_SI"
-    out_file = root_file + "-SI-pindel.vcf"
-    if not out_file:
+def _create_vcf(root_file, out_file, reference, items, paired=None):
+    """use pindel2vcf to create vcf file from pindel format
+    :param root_file: (str) prefix for pindel
+    :param out_file: (str) final vcf file
+    :param reference (str) genome in fasta format
+    :param items: (dics) information fro yaml file
+    :param paired: (tupple) bam files and name of tumor/normal samples
+    :returns: (str) final vcf file
+    """
+    config = items[0]["config"]
+    name_ref = items[0]["genome_build"]
+    date = time.strftime("%Y%m%d")
+    if not utils.file_exists(out_file):
         pindel2vcf = config_utils.get_program("pindel2vcf", config)
-        cmd = ("{pindel2vcf} -p {in_file} -r {reference} -R {name_ref} -d {date_ref} -v {out_file}")
-        print("final command for pindel %s" % cmd.format(**locals()))
-        do.run(cmd.format(**locals()), "Converting to vcf", {})
+        vcf_file = out_file.replace(".gz", "")
+        with file_transaction(items[0], vcf_file) as tx_out_file:
+            cmd = ("{pindel2vcf} --gatk_compatible -P {root_file} -r {reference} -R {name_ref} -d {date} -v {tx_out_file} --compact_output_limit 15")
+            do.run(cmd.format(**locals()), "Converting to vcf", {})
+            if paired.normal_name:
+                _filter_paired(paired.tumor_name, paired.normal_name,
+                               tx_out_file, reference, items[0])
+        out_file = bgzip_and_index(vcf_file, config)
     return out_file
 
 
-def _merge_output(prefix, data):
-    """merge outputs files as one single bed file"""
-    exts = ["D", "INV", "LI", "SI", "TD"]
-    out_file = prefix + "-pindel.bed"
-    for ext in exts:
-        single_file = prefix + "_" + ext
-        with file_transaction(data, out_file) as tx_out_file:
-            with open(tx_out_file, 'w') as out_handle:
-                for ext in exts:
-                    single_file = prefix + "_" + ext
-                    with open(single_file) as in_handle:
-                        line = in_handle.readline()
-                        while line:
-                            if line.startswith('#'):
-                                line = in_handle.readline()
-                                cols = line.strip().split("\t")
-                                out_handle.write("%s\t%s\t%s\t%s\n" % (cols[3].split(" ")[1], cols[4].split(" ")[1], cols[5], cols[1]))
-                            line = in_handle.readline()
+def _filter_paired(tumor, normal, out_file, reference, data):
+    """filter paired vcf file with GATK
+    :param    tumor: (str) sample name for tumor
+    :param    normal: (str) sample name for normal
+    :param    out_file: (str) final vcf file
+    :param    reference: (str) genome in fasta format
+    :param    data: (dict) information from yaml file(items[0])
+    :returns: (str) name of final vcf file
+    """
+    in_file = utils.splitext_plus(out_file)[0] + "-tmp.vcf"
+    shutil.move(out_file, in_file)
+    config = data["config"]
+    with file_transaction(data, out_file) as tx_out_file:
+        params = ["-T", "SomaticPindelFilter", "-V", in_file, "-o",
+                  tx_out_file, "-TID", tumor, "-NID", normal, "-R", reference]
+        jvm_opts = broad.get_gatk_framework_opts(config)
+        cmd = [config_utils.get_program("gatk-framework", config)] + jvm_opts + params
+        do.run(cmd, "Filter pindel variants")
     return out_file
-
