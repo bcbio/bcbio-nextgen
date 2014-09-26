bd0b1f9300a034b1bba79734b45193c234907ce5
roryk <rory.kirchner@gmail.com>
2014-09-26 11:09:12 -0400

    Install ia32-libs so we can run 32 bit executables.

--- scripts/vm/bootstrap.sh
+++ scripts/vm/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install -y build-essential zlib1g-dev wget curl python-setuptools git libz-dev
+sudo apt-get install -y build-essential zlib1g-dev wget curl python-setuptools git libz-dev ia32-libs
 mkdir tmp
 cd tmp
 wget https://raw.github.com/chapmanb/bcbio-nextgen/master/scripts/bcbio_nextgen_install.py
