5d098f494cce4204120366f6a1e40e453b559303
roryk <rory.kirchner@gmail.com>
2014-10-31 19:05:24 -0400

    Always make a sequence index when setting up a custom genome.

--- scripts/bcbio_setup_genome.py
+++ scripts/bcbio_setup_genome.py
@@ -128,6 +128,10 @@ if __name__ == "__main__":
     if args.gff3:
         args.gtf = gff3_to_gtf(args.gtf)
 
+    # always make a sequence dictionary
+    if "seq" not in args.indexes:
+        args.indexes.append("seq")
+
     env.system_install = genome_dir
     prepare_tx = os.path.join(cbl["dir"], "utils", "prepare_tx_gff.py")
 
