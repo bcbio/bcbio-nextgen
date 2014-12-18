6202e4c3422a7f027a0b5c24880e7ff5c0bee4af
John Morrissey <jwm@horde.net>
2014-12-18 14:20:13 -0500

    cruft removal

--- bcbio/provenance/profile.py
+++ bcbio/provenance/profile.py
@@ -1,20 +1,11 @@
-"""Profiling of system resources (CPU, memory, disk, filesystem IO) during pipeline runs.
+"""Profiling of system resources (CPU, memory, disk, filesystem IO) during
+pipeline runs.
 """
 import contextlib
-import os
 
-from bcbio import utils
 from bcbio.log import logger
 
 @contextlib.contextmanager
 def report(label, dirs):
-    """Run reporting metrics to prepare reports of resource usage.
-    """
+    """Log timing information for later graphing of resource usage."""
     logger.info("Timing: %s" % label)
-    profile_dir = utils.safe_makedir(os.path.join(dirs["work"], "profile"))
-    # Prepare and start profiling scripts
-    try:
-        yield None
-    finally:
-        # Cleanup and stop profiling
-        pass
