e63a046b6c84a186aa0d508e205bbb83c8a245a3
chapmanb <chapmanb@50mail.com>
2015-01-14 10:19:15 -0500

    cn:mops -- Catch errors on chromosome Y females with low coverage and report no variants. Fixes #604

--- bcbio/structural/cn_mops.py
+++ bcbio/structural/cn_mops.py
@@ -121,7 +121,8 @@ def _run_on_chrom(chrom, work_bams, names, work_dir, items):
 def _allowed_cnmops_errorstates(msg):
     return (msg.find("No CNV regions in result object. Rerun cn.mops with different parameters") >= 0
             or msg.find("Normalization might not be applicable for this small number of segments") >= 0
-            or msg.find("Error in if (is.finite(mv2m)) { : argument is of length zero") >= 0)
+            or msg.find("Error in if (is.finite(mv2m)) { : argument is of length zero") >= 0
+            or msg.find("Some normalization factors are zero") >= 0)
 
 def _prep_load_script(work_bams, names, chrom, items):
     if not chrom: chrom = ""
