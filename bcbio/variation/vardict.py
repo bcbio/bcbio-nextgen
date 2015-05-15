a19f5190bf465258f5e775ed9eec0b342354e5f3
chapmanb <chapmanb@50mail.com>
2015-05-15 05:26:55 -0400

    Generalize bcftools post-filtering without -M flag to add tags to everything that is non-somatic. Thanks to @mjafin

--- bcbio/variation/vardict.py
+++ bcbio/variation/vardict.py
@@ -177,7 +177,7 @@ def _run_vardict_paired(align_bams, items, ref_file, assoc_files,
                    "-N {paired.tumor_name} -b \"{paired.tumor_bam}|{paired.normal_bam}\" {opts} "
                    "| {strandbias} "
                    "| {var2vcf} -N \"{paired.tumor_name}|{paired.normal_name}\" -f {freq} {var2vcf_opts} "
-                   "| bcftools filter -m '+' -s 'REJECT' -e 'STATUS = \"Germline\"' 2> /dev/null "
+                   "| bcftools filter -m '+' -s 'REJECT' -e 'STATUS !~ \".*Somatic\"' 2> /dev/null "
                    "{somatic_filter} | {fix_ambig} | {vcfstreamsort} {compress_cmd} > {tx_out_file}")
             bam.index(paired.tumor_bam, config)
             bam.index(paired.normal_bam, config)
