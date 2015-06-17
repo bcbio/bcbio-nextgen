c864179884914fa83f53d2ae807d69939cca2cca
Alexandru Coman <acoman@cloudbasesolutions.com>
2015-06-17 09:09:36 +0300

    Cosmetic changes for bcbio.rnaseq.rsem module

--- bcbio/rnaseq/rsem.py
+++ bcbio/rnaseq/rsem.py
@@ -1,10 +1,17 @@
 import os
-import subprocess
-from bcbio.distributed.transaction import tx_tmpdir, file_transaction
-from bcbio.utils import chdir, which, safe_makedir, file_exists
+
 from bcbio import bam
+from bcbio import utils
 from bcbio.log import logger
+from bcbio.distributed import transaction
 from bcbio.provenance import do
+
+CALCULATE_EXP = (
+    "rsem-calculate-expression --bam {core_flag} {paired_flag} "
+    "--no-bam-output --forward-prob 0.5 "
+    "--estimate-rspd {bam_file} {rsem_genome_dir}/{build} {samplename}")
+PREPARE_REFERENCE = "rsem-prepare-reference --gtf {gtf} {multifasta} {build}"
+
 
 def prepare_rsem_reference(gtf, multifasta, build):
     """
@@ -12,40 +19,43 @@ def prepare_rsem_reference(gtf, multifasta, build):
     multifasta: path to multifasta file
     build: name of organism build (e.g. hg19)
     """
-    if not which("rsem-prepare-reference"):
-        logger.info("Skipping prepping RSEM reference because rsem-prepare-reference could "
-                    "not be found.")
+    if not utils.which("rsem-prepare-reference"):
+        logger.info("Skipping prepping RSEM reference because "
+                    "rsem-prepare-reference could not be found.")
         return None
 
-    cmd = "rsem-prepare-reference --gtf {gtf} {multifasta} {build}"
-    with tx_tmpdir(remove=False) as rsem_genome_dir:
-        with chdir(rsem_genome_dir):
+    command = PREPARE_REFERENCE.format(gtf=gtf, multifasta=multifasta,
+                                       build=build)
+    with transaction.tx_tmpdir(remove=False) as rsem_genome_dir:
+        with utils.chdir(rsem_genome_dir):
             message = "Preparing rsem reference from %s" % gtf
-            do.run(cmd.format(**locals()), message)
+            do.run(command, message)
     return rsem_genome_dir
 
-def rsem_calculate_expression(bam_file, rsem_genome_dir, samplename, build,
-                              out_dir, cores=1):
+
+def rsem_calculate_expression(bam_file, rsem_genome_dir, samplename,
+                              build, out_dir, cores=1):
     """
     works only in unstranded mode for now (--forward-prob 0.5)
     """
-    if not which("rsem-calculate-expression"):
+    if not utils.which("rsem-calculate-expression"):
         logger.info("Skipping RSEM because rsem-calculate-expression could "
                     "not be found.")
         return None
 
     sentinel_file = os.path.join(out_dir, samplename + "Test.genes.results")
-    if file_exists(sentinel_file):
+    if utils.file_exists(sentinel_file):
         return out_dir
 
     paired_flag = "--paired" if bam.is_paired(bam_file) else ""
     core_flag = "-p {cores}".format(cores=cores)
-    cmd = ("rsem-calculate-expression --bam {core_flag} {paired_flag} --no-bam-output "
-           "--forward-prob 0.5 --estimate-rspd {bam_file} {rsem_genome_dir}/{build} "
-           "{samplename}")
+    command = CALCULATE_EXP.format(
+        core_flag=core_flag, paired_flag=paired_flag, bam_file=bam_file,
+        rsem_genome_dir=rsem_genome_dir, build=build, samplename=samplename)
     message = "Calculating transcript expression of {bam_file} using RSEM."
-    with file_transaction(out_dir) as tx_out_dir:
-        safe_makedir(tx_out_dir)
-        with chdir(tx_out_dir):
-            do.run(cmd.format(**locals()), message.format(**locals()))
+
+    with transaction.file_transaction(out_dir) as tx_out_dir:
+        utils.safe_makedir(tx_out_dir)
+        with utils.chdir(tx_out_dir):
+            do.run(command, message.format(bam_file=bam_file))
     return out_dir
