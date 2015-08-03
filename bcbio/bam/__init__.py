820d84ffeee1afcd5abd90732bf8d5ce08790661
roryk <rory.kirchner@gmail.com>
2015-08-03 23:32:22 +0100

    Look up cores using the wrapper.

--- bcbio/bam/__init__.py
+++ bcbio/bam/__init__.py
@@ -15,6 +15,7 @@ from bcbio.bam import ref
 from bcbio.distributed import objectstore
 from bcbio.distributed.transaction import file_transaction
 from bcbio.pipeline import config_utils
+import bcbio.pipeline.datadict as dd
 from bcbio.provenance import do
 
 def is_paired(bam_file):
@@ -111,7 +112,7 @@ def downsample(in_bam, data, target_counts, read_filter="", always_run=False,
         if not utils.file_exists(out_file):
             with file_transaction(data, out_file) as tx_out_file:
                 sambamba = config_utils.get_program("sambamba", data["config"])
-                num_cores = data["config"]["algorithm"].get("num_cores", 1)
+                num_cores = dd.get_num_cores(data)
                 cmd = ("{sambamba} view -t {num_cores} {read_filter} -f bam -o {tx_out_file} "
                        "--subsample={ds_pct:.3} --subsampling-seed=42 {in_bam}")
                 do.run(cmd.format(**locals()), "Downsample BAM file: %s" % os.path.basename(in_bam))
