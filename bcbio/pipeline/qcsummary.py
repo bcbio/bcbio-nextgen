1a10c561024b1d8ab2fa1095d1ed0a89b9a2cb9e
chapmanb <chapmanb@50mail.com>
2015-08-03 10:25:22 -0400

    Handle split disambiguation stats, from @schelhorn, fixes #948

--- bcbio/pipeline/qcsummary.py
+++ bcbio/pipeline/qcsummary.py
@@ -210,17 +210,17 @@ def _save_fields(sample):
                 saved["summary"]["metrics"]["Disambiguated ambiguous reads"] = disambigStats[2]
     return saved
 
-
 def _parse_disambiguate(disambiguatestatsfilename):
     """Parse disambiguation stats from given file.
     """
-    disambig_stats = [-1, -1, -1]
+    disambig_stats = [0, 0, 0]
     with open(disambiguatestatsfilename, "r") as in_handle:
-        header = in_handle.readline().strip().split("\t")
-        if header == ['sample', 'unique species A pairs', 'unique species B pairs', 'ambiguous pairs']:
-            disambig_stats_tmp = in_handle.readline().strip().split("\t")[1:]
-            if len(disambig_stats_tmp) == 3:
-                disambig_stats = [int(x) for x in disambig_stats_tmp]
+        for i, line in enumerate(in_handle):
+            fields = line.strip().split("\t")
+            if i == 0:
+                assert fields == ['sample', 'unique species A pairs', 'unique species B pairs', 'ambiguous pairs']
+            else:
+                disambig_stats = [x + int(y) for x, y in zip(disambig_stats, fields[1:])]
     return disambig_stats
 
 # ## Generate researcher specific summaries
