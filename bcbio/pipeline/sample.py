fb67267777782f7cbaa570395576a9c179f669a1
chapmanb <chapmanb@50mail.com>
2015-08-11 04:39:04 -0400

    Pre-index BAM file prior to callable calculation
    Avoids race conditions when BAM index is out of date with BAM due
    to filesystem timestampl issues.

--- bcbio/pipeline/sample.py
+++ bcbio/pipeline/sample.py
@@ -159,6 +159,7 @@ def postprocess_alignment(data):
         callable_region_bed, nblock_bed, callable_bed = \
             callable.block_regions(data["work_bam"], ref_file, data)
         highdepth_bed = highdepth.identify(data)
+        bam.index(data["work_bam"], data["config"])
         sample_callable = callable.sample_callable_bed(data["work_bam"], ref_file, data)
         offtarget_stats = callable.calculate_offtarget(data["work_bam"], ref_file, data)
         data["regions"] = {"nblock": nblock_bed, "callable": callable_bed, "highdepth": highdepth_bed,
