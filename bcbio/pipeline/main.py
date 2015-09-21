78e20d2cd193d5ac23c06565e1145c5dfc784f53
roryk <rory.kirchner@gmail.com>
2015-09-20 21:41:46 -0400

    Bump minimum memory for Tophat to account sorting.

--- bcbio/pipeline/main.py
+++ bcbio/pipeline/main.py
@@ -304,7 +304,7 @@ class RnaseqPipeline(AbstractPipeline):
                 samples = run_parallel("prepare_sample", samples)
                 samples = run_parallel("trim_sample", samples)
         with prun.start(_wres(parallel, ["aligner", "picard"],
-                              ensure_mem={"tophat": 8, "tophat2": 8, "star": 2}),
+                              ensure_mem={"tophat": 10, "tophat2": 10, "star": 2}),
                         samples, config, dirs, "alignment",
                         multiplier=alignprep.parallel_multiplier(samples)) as run_parallel:
             with profile.report("alignment", dirs):
