bc0e9a1dd7819fee38cba5f45d4393189b669bfa
Roman Valls Guimera <roman@lifesum.com>
2015-10-06 09:18:58 +0200

    Run CWL tests inside docker

--- tests/test_cwl.py
+++ tests/test_cwl.py
@@ -11,6 +11,7 @@ class CWLTest(unittest.TestCase):
     """ Run a simple CWL workflow.
     """
     @attr(speed=1)
+    @attr(docker=True)
     @attr(cwl=True)
     def test_1_cwl_docker(self):
         cl = ["cwl-runner", "--verbose", "../cwl/bcbio2cwl.cwl", "../cwl/testinput-args.json"]
