2f49151c34a53f4d6f33a82475c79f38a97ad8dc
Roman Valls Guimera <roman@lifesum.com>
2015-10-06 08:40:19 +0200

    Run with docker on TravisCI. Thanks @guillermo-carrasco

--- tests/test_cwl.py
+++ tests/test_cwl.py
@@ -12,7 +12,14 @@ class CWLTest(unittest.TestCase):
     """
     @attr(speed=1)
     @attr(cwl=True)
-    def test_1_cwl_nocontainer(self):
+    def test_1_cwl_docker(self):
+        cl = ["cwl-runner", "--verbose", "../cwl/bcbio2cwl.cwl", "../cwl/testinput-args.json"]
+
+        subprocess.check_call(cl)
+
+    @attr(speed=1)
+    @attr(cwl=True)
+    def test_2_cwl_nocontainer(self):
         cl = ["cwl-runner", "--verbose", "--no-container",
               "../cwl/bcbio2cwl.cwl", "../cwl/testinput-args.json"]
 
