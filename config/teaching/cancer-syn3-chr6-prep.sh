e478e089245ebdd5b32696901fa03ac5bb61755a
chapmanb <chapmanb@50mail.com>
2015-11-16 18:11:37 +0000

    Add configuration files and download script for minimal synthetic 3 teaching example

---
+++ config/teaching/cancer-syn3-chr6-prep.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Setup directory for running a minimal DREAM synthetic 3 dataset
+# The original DREAM data is subset to exomes on chromosome 6
+set -eu -o pipefail
+
+mkdir -p cancer-syn3-chr6
+cd cancer-syn3-chr6
+wget -c https://s3.amazonaws.com/bcbio_nextgen/dream/cancer-syn3-chr6-input.tar.gz
+tar -xczpf cancer-syn3-chr6-input.tar.gz
+rm -f cancer-syn3-chr6-input.tar.gz
+
+mkdir -p config
+cd config
+wget -c https://raw.githubusercontent.com/chapmanb/bcbio-nextgen/master/config/teaching/cancer-syn3-chr6.yaml
+cd ..
+
+mkdir -p work
