348912e331ec43dac1f10b97a1a1efb83a84c35f
Lorena <lorena.pantano@gmail.com>
2015-12-07 16:17:45 -0500

    Fix missing variable for new seqcluster.

--- bcbio/srna/group.py
+++ bcbio/srna/group.py
@@ -176,7 +176,7 @@ def _make_isomir_counts(data):
         miraligner_fn = sample[0]["seqbuster"]
         reads = _read_miraligner(miraligner_fn)
         if reads:
-            out_file, dt = _tab_output(reads, miraligner_fn + ".back", dd.get_sample_name(sample[0]))
+            out_file, dt, dt_pre = _tab_output(reads, miraligner_fn + ".back", dd.get_sample_name(sample[0]))
             out_dts.append(dt)
         else:
             logger.log("WARNING::%s has NOT miRNA annotated. Check if fasta files is small or species value." % dd.get_sample_name(sample[0]))
