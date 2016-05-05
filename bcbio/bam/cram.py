2734827fe7787d56c86d683c58d4ca74dca23f0f
chapmanb <chapmanb@50mail.com>
2016-05-05 09:23:34 -0400

    CRAM: use 8-bin compression with bamUtil
    For CRAM support, use bamUtil squeeze to provide additional output
    compression by removing read names and 8-bin combining. Explicitly
    reference internal samtools and bamUtils to avoid corrupt output.
    Fixes #1372

--- bcbio/bam/cram.py
+++ bcbio/bam/cram.py
@@ -7,23 +7,29 @@ import os
 from bcbio import utils
 from bcbio.provenance import do
 from bcbio.pipeline import datadict as dd
+from bcbio.pipeline import config_utils
 from bcbio.distributed.transaction import file_transaction
 
 def compress(in_bam, data):
     """Compress a BAM file to CRAM, providing indexed CRAM file.
 
-    XXX Does not 8-bin quality scores on output. We switched to samtools
-    CRAM support due to problems with cramtools 3.0, and samtools does not yet
-    support quality binning.
+    Does 8 bin compression of quality score and read name removal
+    using bamUtils squeeze:
+
+    http://genome.sph.umich.edu/wiki/BamUtil:_squeeze
     """
     out_file = "%s.cram" % os.path.splitext(in_bam)[0]
     cores = dd.get_num_cores(data)
     ref_file = dd.get_ref_file(data)
     if not utils.file_exists(out_file):
         with file_transaction(data, out_file) as tx_out_file:
-            cmd = ("samtools view -T {ref_file} -@ {cores} "
+            samtools = config_utils.get_program("samtools", data["config"])
+            bam = config_utils.get_program("bam", data["config"])
+            cmd = ("{bam} squeeze --in {in_bam} --out -.ubam --keepDups --readName "
+                   "--binQualS=2,10,20,25,30,35,70 --binMid | "
+                   "{samtools} view -T {ref_file} -@ {cores} "
                    "-C -x BD -x BI "
-                   "-o {tx_out_file} {in_bam}")
+                   "-o {tx_out_file}")
             do.run(cmd.format(**locals()), "Compress BAM to CRAM")
     index(out_file, data["config"])
     return out_file
