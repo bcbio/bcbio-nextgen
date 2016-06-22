d2f80791541d1908b61aaacc20d053d67dcaf540
vladsaveliev <vladsaveliev@me.com>
2016-06-22 05:58:35 +0300

    Seq2C: calculating coverages using sambamba

--- bcbio/structural/seq2c.py
+++ bcbio/structural/seq2c.py
@@ -8,36 +8,116 @@ calling across all samples.
 """
 import os
 import subprocess
+from collections import defaultdict
 
 from bcbio import utils
 from bcbio.pipeline import datadict as dd
-from bcbio.distributed.multi import run_multicore
 from bcbio.distributed.transaction import file_transaction
-from bcbio.structural import cnvkit
+from bcbio.provenance import do
+from bcbio.log import logger
+from pipeline import config_utils
+
 
 def precall(items):
     """Perform initial pre-calling steps -- coverage calcuation by sample.
 
-    Coverage calculation per gene, using existing CNVkit coverage calculation
-    parallelization in place of seq2cov.pl.
+    Use sambamba to call average region coverage in regions
     """
     items = [utils.to_single_data(x) for x in items]
     assert len(items) == 1, "Expect one item to Seq2C coverage calculation"
     data = items[0]
     assert dd.get_coverage_interval(data) != "genome", "Seq2C only for amplicon and exome sequencing"
+
+    bed_file = dd.get_variant_regions(data)
+    bam_file = dd.get_align_bam(data)
+    sample_name = data['name'][1]
+
     work_dir = _sv_workdir(data)
-    parallel = {"type": "local", "cores": dd.get_cores(data), "progs": ["seq2c"]}
-    split_cnns = run_multicore(cnvkit._cnvkit_coverage,
-                                [(dd.get_align_bam(data), bed, "", work_dir, data)
-                                 for bed in cnvkit._split_bed(dd.get_variant_regions(data), data)],
-                               data["config"], parallel)
-    coverage_cnns = cnvkit._merge_coverage(split_cnns, data)
-    assert len(coverage_cnns) == 1
+    sambamba_depth_file = os.path.join(work_dir, sample_name + '.sambamba_depth.txt')
+    sambamba = config_utils.get_program("sambamba", data["config"])
+    num_cores = dd.get_cores(data)
+    if not utils.file_exists(sambamba_depth_file):
+        with file_transaction(data, sambamba_depth_file) as tx_out_file:
+            cmd = "{sambamba} depth region -t {num_cores} -L {bed_file} -o {tx_out_file} {bam_file}"
+            do.run(cmd.format(**locals()), "Calling sambamba region depth")
+    logger.debug('Saved to ' + sambamba_depth_file)
+
+    logger.debug('Converting sambamba depth output to cov2lr.pl input')
+    out_file = os.path.join(work_dir, os.path.splitext(os.path.basename(bam_file))[0] + '.seq2cov.txt')
+    if not utils.file_exists(out_file):
+        with file_transaction(data, out_file) as tx_out_file:
+            sambabma_depth_to_seq2cov(sambamba_depth_file, tx_out_file, sample_name)
+    logger.debug('Saved to ' + out_file)
+
     if "sv" not in data:
         data["sv"] = []
     data["sv"].append({"variantcaller": "seq2c",
-                       "cnn": coverage_cnns[0]["file"]})
+                       "cov": out_file})
     return [data]
+
+''' sambamba_depth_output_fpath:
+# chrom chromStart  chromEnd  F3       readCount  minDepth  meanCoverage  stdDev   percentage1  percentage5  percentage10  ...  sampleName
+chr20   68345       68413     DEFB125  56         28        32.5          1.66716  100          100          100           ...  chr20_tumor
+chr20   76640       77301     DEFB125  279        24        36.9213       5.74231  100          100          100           ...  chr20_tumor
+'''
+''' seq2cov:
+chr20_tumor_1   DEFB125   chr20   68346   68413   Amplicon    68   28.0
+chr20_tumor_1   DEFB125   chr20   76641   77301   Amplicon    661  24.0
+chr20_tumor_1   DEFB125   chr20   68346   77301   Whole-Gene  729  24.3731138546
+chr20_tumor_1   DEFB126   chr20   123247  123332  Amplicon    86   40.0
+'''
+def sambabma_depth_to_seq2cov(input_fpath, output_fpath, sample_name):
+    with open(input_fpath) as f:
+        ave_depth_col = next(f).split('\t').index('meanCoverage')
+
+    # first round: collecting gene ends
+    gene_end_by_gene = defaultdict(lambda: -1)
+    with open(input_fpath) as f:
+        for l in f:
+            if l.startswith('#'): continue
+            fs = l.replace('\n', '').split('\t')
+            if any(fs[i] == '.' for i in [0, 1, 2, 3, ave_depth_col]): continue
+            end = int(fs[2])
+            gene_name = fs[3]
+            gene_end_by_gene[gene_name] = max(gene_end_by_gene[gene_name], end)
+
+    # second round: calculating coverage
+    total_cov_by_gene = dict()
+    gene_start_by_gene = dict()
+    total_size_by_gene = dict()
+
+    with open(input_fpath) as f, open(output_fpath, 'w') as out:
+        for l in f:
+            if l.startswith('#'): continue
+            fs = l.replace('\n', '').split('\t')
+            if any(fs[i] == '.' for i in [0, 1, 2, 3, ave_depth_col]): continue
+
+            chrom = fs[0]
+            start = int(fs[1])
+            end = int(fs[2])
+            gene_name = fs[3]
+            ave_depth = float(fs[ave_depth_col])
+
+            if gene_name not in gene_start_by_gene:
+                gene_start_by_gene[gene_name] = start
+                total_cov_by_gene[gene_name] = 0
+                total_size_by_gene[gene_name] = 0
+            else:
+                gene_start_by_gene[gene_name] = min(start, gene_start_by_gene[gene_name])
+            total_cov_by_gene[gene_name] += ave_depth * (end - start)
+            total_size_by_gene[gene_name] += end - start
+
+            fs = [sample_name, gene_name, chrom, str(start + 1), str(end), 'Amplicon', str(end - start), str(ave_depth)]
+            out.write('\t'.join(fs) + '\n')
+
+            if end >= gene_end_by_gene[gene_name]:
+                assert end == gene_end_by_gene[gene_name], (end, gene_end_by_gene[gene_name])
+                start = gene_start_by_gene[gene_name]
+                ave_depth = total_cov_by_gene[gene_name] / total_size_by_gene[gene_name]
+                size = total_size_by_gene[gene_name]
+                fs = [sample_name, gene_name, chrom, str(start + 1), str(end), 'Whole-Gene', str(size), str(ave_depth)]
+                out.write('\t'.join(fs) + '\n')
+    return output_fpath
 
 def _sv_workdir(data):
     return utils.safe_makedir(os.path.join(data["dirs"]["work"], "structural",
