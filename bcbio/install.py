f70a9d68666a44cf1e4bcbb9c1051d0511f4818d
chapmanb <chapmanb@50mail.com>
2016-07-07 06:20:47 -0400

    Remove Zv9 genome in favor of GRCz10. Fixes #1461

--- bcbio/install.py
+++ bcbio/install.py
@@ -37,7 +37,7 @@ REMOTES = {
 SUPPORTED_GENOMES = ["GRCh37", "hg19", "hg38", "hg38-noalt", "mm10", "mm9",
                      "rn6", "rn5", "canFam3", "dm3", "galGal4", "phix",
                      "pseudomonas_aeruginosa_ucbpp_pa14", "sacCer3", "TAIR10",
-                     "WBcel235", "xenTro3", "Zv9", "GRCz10"]
+                     "WBcel235", "xenTro3", "GRCz10"]
 SUPPORTED_INDEXES = ["bowtie", "bowtie2", "bwa", "novoalign", "rtg", "snap",
                      "star", "ucsc", "seq", "hisat2"]
 DEFAULT_INDEXES = ["rtg"]
