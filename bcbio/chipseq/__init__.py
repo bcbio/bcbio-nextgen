5417c45a2005cb643a25ac1f082a5233eef3419f
Lorena Pantano <lorena.pantano@gmail.com>
2016-07-19 10:40:48 -0400

    chipseq:Update import in cleaning bam file fn
    thanks @LMannarino

--- bcbio/chipseq/__init__.py
+++ bcbio/chipseq/__init__.py
@@ -1,5 +1,6 @@
 from bcbio.ngsalign.bowtie2 import filter_multimappers
 import bcbio.pipeline.datadict as dd
+from bcbio.log import logger
 
 def clean_chipseq_alignment(data):
     aligner = dd.get_aligner(data)
@@ -9,6 +10,6 @@ def clean_chipseq_alignment(data):
         unique_bam = filter_multimappers(dd.get_work_bam(data), data)
         data["work_bam"] = unique_bam
     else:
-        logger.info("When BAM file is given, bcbio skips multimappers removal.")
+        logger.info("Aligning skipped: When BAM file is given, bcbio skips multimappers removal.")
         logger.info("If BAM is not cleaned for peak calling, can result in downstream errors.")
     return [[data]]
