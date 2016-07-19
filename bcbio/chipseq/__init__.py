5fef3e5a5425ab71abb4c3b8a36a2273c9947b2e
Lorena Pantano <lorena.pantano@gmail.com>
2016-07-19 10:08:42 -0400

    Chipseq: Add warning when aligner is false

--- bcbio/chipseq/__init__.py
+++ bcbio/chipseq/__init__.py
@@ -8,4 +8,7 @@ def clean_chipseq_alignment(data):
         assert aligner == "bowtie2", "ChIP-seq only supported for bowtie2."
         unique_bam = filter_multimappers(dd.get_work_bam(data), data)
         data["work_bam"] = unique_bam
+    else:
+        logger.info("When BAM file is given, bcbio skips multimappers removal.")
+        logger.info("If BAM is not cleaned for peak calling, can result in downstream errors.")
     return [[data]]
