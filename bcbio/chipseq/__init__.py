a0e24d48edeb5bffbaa191570b3634f43254fb5a
Lorena Pantano <lorena.pantano@gmail.com>
2016-07-19 10:00:55 -0400

    Chipseq:Allow BAM file as input
    ref https://github.com/chapmanb/bcbio-nextgen/issues/1481
    
    thanks @LMannarino @lbeltrame

--- bcbio/chipseq/__init__.py
+++ bcbio/chipseq/__init__.py
@@ -3,10 +3,9 @@ import bcbio.pipeline.datadict as dd
 
 def clean_chipseq_alignment(data):
     aligner = dd.get_aligner(data)
-    assert aligner == "bowtie2", "ChIP-seq only supported for bowtie2."
-    if aligner == "bowtie2":
-        data["raw_bam"] = dd.get_work_bam(data)
-        unique_bam = filter_multimappers(dd.get_work_bam(data), data)
-        data["work_bam"] = unique_bam
-        return [[data]]
+    if aligner:
+        assert aligner == "bowtie2", "ChIP-seq only supported for bowtie2."
+    data["raw_bam"] = dd.get_work_bam(data)
+    unique_bam = filter_multimappers(dd.get_work_bam(data), data)
+    data["work_bam"] = unique_bam
     return [[data]]
