a87b6b47b4903acfcecce8f626c67006ddfc75eb
roryk <rory.kirchner@gmail.com>
2016-07-25 20:28:37 -0400

    Remove dead code.

--- bcbio/pipeline/rnaseq.py
+++ bcbio/pipeline/rnaseq.py
@@ -9,10 +9,8 @@ from bcbio.utils import filter_missing, flatten
 from bcbio.log import logger
 
 def fast_rnaseq(samples, run_parallel):
-#    samples = run_parallel("run_rapmap_pseudoalign", samples)
     samples = run_parallel("run_salmon_reads", samples)
     samples = sailfish.combine_sailfish(samples)
-#    samples = run_parallel("run_salmon_bam", samples)
     return samples
 
 def singlecell_rnaseq(samples, run_parallel):
