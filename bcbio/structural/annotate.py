ea73ef38067c1dcfc2d3a7e047a09354bcb54d4a
chapmanb <chapmanb@50mail.com>
2016-09-04 08:11:55 -0400

    Generalize gene annotation to allow external use
    Extracts non-bcbio parts of BED file gene annotation for use from
    external scripts.

--- bcbio/structural/annotate.py
+++ bcbio/structural/annotate.py
@@ -23,29 +23,34 @@ def add_genes(in_file, data, max_distance=10000, work_dir=None):
         if work_dir:
             out_file = os.path.join(work_dir, os.path.basename(out_file))
         if not utils.file_uptodate(out_file, in_file):
-            input_rec = iter(pybedtools.BedTool(in_file)).next()
-            # keep everything after standard chrom/start/end, 1-based
-            extra_fields = range(4, len(input_rec.fields) + 1)
-            # keep the new gene annotation
-            gene_index = len(input_rec.fields) + 4
-            extra_fields.append(gene_index)
-            columns = ",".join([str(x) for x in extra_fields])
-            max_column = max(extra_fields) + 1
-            ops = ",".join(["distinct"] * len(extra_fields))
             fai_file = ref.fasta_idx(dd.get_ref_file(data))
             with file_transaction(data, out_file) as tx_out_file:
-                # swap over gene name to '.' if beyond maximum distance
-                # cut removes the last distance column which can cause issues
-                # with bedtools merge: 'ERROR: illegal character '.' found in integer conversion of string'
-                distance_filter = (r"""awk -F$'\t' -v OFS='\t' '{if ($NF > %s) $%s = "."} {print}'""" %
-                                   (max_distance, gene_index))
-                sort_cmd = bedutils.get_sort_cmd()
-                cmd = ("{sort_cmd} -k1,1 -k2,2n {in_file} | "
-                       "bedtools closest -g <(cut -f1,2 {fai_file} | {sort_cmd} -k1,1 -k2,2n) "
-                       "-d -t all -a - -b <({sort_cmd} -k1,1 -k2,2n {gene_file}) | "
-                       "{distance_filter} | cut -f 1-{max_column} | "
-                       "bedtools merge -i - -c {columns} -o {ops} -delim ',' > {tx_out_file}")
-                do.run(cmd.format(**locals()), "Annotate BED file with gene info")
+                add_genes_to_bed(in_file, gene_file, fai_file, tx_out_file, max_distance)
         return out_file
     else:
         return in_file
+
+def add_genes_to_bed(in_file, gene_file, fai_file, out_file, max_distance=10000):
+    """Re-usable subcomponent that annotates BED file genes from another BED
+    """
+    input_rec = iter(pybedtools.BedTool(in_file)).next()
+    # keep everything after standard chrom/start/end, 1-based
+    extra_fields = range(4, len(input_rec.fields) + 1)
+    # keep the new gene annotation
+    gene_index = len(input_rec.fields) + 4
+    extra_fields.append(gene_index)
+    columns = ",".join([str(x) for x in extra_fields])
+    max_column = max(extra_fields) + 1
+    ops = ",".join(["distinct"] * len(extra_fields))
+    # swap over gene name to '.' if beyond maximum distance
+    # cut removes the last distance column which can cause issues
+    # with bedtools merge: 'ERROR: illegal character '.' found in integer conversion of string'
+    distance_filter = (r"""awk -F$'\t' -v OFS='\t' '{if ($NF > %s) $%s = "."} {print}'""" %
+                       (max_distance, gene_index))
+    sort_cmd = bedutils.get_sort_cmd()
+    cmd = ("{sort_cmd} -k1,1 -k2,2n {in_file} | "
+            "bedtools closest -g <(cut -f1,2 {fai_file} | {sort_cmd} -k1,1 -k2,2n) "
+            "-d -t all -a - -b <({sort_cmd} -k1,1 -k2,2n {gene_file}) | "
+            "{distance_filter} | cut -f 1-{max_column} | "
+            "bedtools merge -i - -c {columns} -o {ops} -delim ',' > {out_file}")
+    do.run(cmd.format(**locals()), "Annotate BED file with gene info")
