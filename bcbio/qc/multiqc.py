8d70322180ce5b9919a40b26642c92a67515f137
Lorena Pantano <lorena.pantano@gmail.com>
2016-09-15 10:24:55 -0400

    QC:ignore bcftool results

--- bcbio/qc/multiqc.py
+++ bcbio/qc/multiqc.py
@@ -79,8 +79,8 @@ def _create_list_file(dirs):
 def _check_multiqc_input(path):
     """Check if dir exists, and return empty if it doesn't"""
     if utils.file_exists(path):
-        return path
-    return None
+        if path.find("bcfstats") == -1:
+            return path
 
 # ## report and coverage
 
