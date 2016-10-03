286cf84b762ac15c54507536bc9e301647fe7349
chapmanb <chapmanb@50mail.com>
2016-10-03 06:33:19 -0400

    WDL generation: support for task output
    Handles translation of tool to task with inputs and commands
    generated. Does not yet handle outputs, need to research best
    ways to retrieve from JSON style output files that CWL uses.

--- scripts/utils/cwltool2wdl.py
+++ scripts/utils/cwltool2wdl.py
@@ -1,11 +1,23 @@
 #!/bin/env python
 """Exploratory code to convert bcbio generated CWL into WDL.
 
-Uses cwltool parser to build objects, calling out to current cwl2wdl
-implementation:
+Uses cwltool parser to parse input CWL then, calls out to cwl2wdl
+for generation of WDL:
 
 https://github.com/adamstruck/cwl2wdl
+
+Targets for supporting in conversion:
+
+- Workflows
+- Tools -> Tasks
+- scatter parallelization
+- Record -> Object
+
+Current status:
+- Basic workflow support at top level
+- Initial Tool output
 """
+import os
 import pprint
 import sys
 
@@ -19,33 +31,79 @@ def main(wf_file, json_file):
     main_wf = cwltool.load_tool.load_tool(wf_file, cwltool.workflow.defaultMakeTool)
     main_wf_dict = _wf_to_dict(main_wf)
     wdl_doc = generators.WdlWorkflowGenerator(main_wf_dict).generate_wdl()
-    print(wdl.parser.parse(wdl_doc))
+    wdl_file = "%s.wdl" % os.path.splitext(wf_file)[0]
+    with open(wdl_file, "w") as out_handle:
+        out_handle.write(wdl_doc)
+    #print(wdl.parser.parse(wdl_doc, os.path.basename(wdl_file)))
 
 def _wf_to_dict(wf):
     """Parse a workflow into cwl2wdl style dictionary.
     """
-    print wf.tool.keys()
-    out = {"name": wf, "inputs": [], "outputs": [], "steps": [], "requirements": []}
+    out = {"name": _id_to_name(wf.tool["id"]), "inputs": [], "outputs": [], "steps": [], "requirements": []}
     for step in wf.steps:
         if isinstance(step.embedded_tool, cwltool.workflow.Workflow):
             task = _wf_to_dict(step.embedded_tool)
         else:
-            task = _tool_to_dict(step.embedded_tool)
+            task_def = _tool_to_dict(step.embedded_tool)
+            out["steps"].append({"task_id": task_def["name"], "task_definition": task_def,
+                                 "import_statement": "", "inputs": [], "outputs": []})
     return cwl2wdl_classes.Workflow(out)
+
+def _to_variable_type(x):
+    """Convert CWL variables to WDL variables, handling nested arrays.
+    """
+    var_mapping = {"string": "String", "File": "File", "null": "String",
+                   "long": "Float", "int": "Int"}
+    if isinstance(x, dict):
+        if x["type"] == "record":
+            return "Object"
+        else:
+            assert x["type"] == "array", x
+            return "Array[%s]" % _to_variable_type(x["items"])
+    elif isinstance(x, (list, tuple)):
+        vars = [v for v in x if v != "null"]
+        assert len(vars) == 1, vars
+        return var_mapping[vars[0]]
+    else:
+        return var_mapping[x]
+
+def _input_to_dict(i):
+    """Convert CWL input into dictionary required for a cwl2wdl Input object.
+    """
+    var_type = _to_variable_type(i["type"])
+    if var_type.startswith("Array") and "inputBinding" in i.get("type", {}):
+        ib = i["type"]["inputBinding"]
+    else:
+        ib = i["inputBinding"]
+    return {"name": _id_to_localname(i["id"]) , "variable_type": var_type,
+            "prefix": ib["prefix"], "separator": ib["itemSeparator"],
+            "position": ib["position"], "is_required": True,
+            "default": i.get("default", ""), "separate": ib.get("separate", True)}
+
+def _id_to_localname(input_id):
+    return os.path.basename(input_id).split("#")[1]
+
+def _id_to_name(input_id):
+    return os.path.splitext(os.path.basename(input_id))[0]
 
 def _tool_to_dict(tool):
     """Parse a tool definition into a cwl2wdl style dictionary.
     """
-    print(tool)
-    print(dir(tool))
-    pprint.pprint(tool.tool)
-    print(tool.requirements)
-    print(tool.hints)
-    out = {"name": tool.split("#")[-1], "baseCommand": "", "arguments": [], "inputs": [],
-           "outputs": [],
-           "requirements": [], "stdin": None, "stdout": None}
-    print(out)
-    return cwl2wdl_classes.Task(out)
+    #print(tool)
+    #print(dir(tool))
+    #pprint.pprint(tool.tool)
+    # print(tool.requirements)
+    # print(tool.hints)
+    out = {"name": _id_to_name(tool.tool["id"]),
+           "baseCommand": " ".join(tool.tool["baseCommand"]),
+           "arguments": [],
+           "inputs": [_input_to_dict(i) for i in tool.tool["inputs"]],
+           "outputs": [{"name": _id_to_localname(o["id"]), "variable_type": _to_variable_type(o["type"]),
+                        "output": "", "is_required": True}
+                       for o in tool.tool["outputs"]],
+           "requirements": [],
+           "stdin": None, "stdout": None}
+    return out
 
 if __name__ == "__main__":
     main(*sys.argv[1:])
