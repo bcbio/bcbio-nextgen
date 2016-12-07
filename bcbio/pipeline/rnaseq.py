bfe9397277956cbddbb1f879249ecc76fc021f4b
Tetiana Khotiaintseva <tetiana.khotiaintseva@ardigen.com>
2016-12-07 16:22:57 +0100

    More tests for rnaseq.detect_fusions

--- bcbio/pipeline/rnaseq.py
+++ bcbio/pipeline/rnaseq.py
@@ -373,5 +373,5 @@ def detect_fusions(samples):
     caller_fn = STANDALONE_CALLERS.get(caller)
 
     if fusion_mode and caller_fn:
-        return [caller_fn(s) for s in samples]
+        return [[caller_fn(s)] for s in dd.sample_data_iterator(samples)]
     return samples
--- tests/unit/pipeline/test_rnaseq.py
+++ tests/unit/pipeline/test_rnaseq.py
@@ -20,9 +20,19 @@ def sample():
     }
 
 
-def test_detect_fusion_callers(ericscript_run, sample):
+def test_detect_fusion_callers_calls_for_each_sample(ericscript_run, sample):
     samples = [[sample], [sample]]
     rnaseq.detect_fusions(samples)
     assert ericscript_run.call_count == len(samples)
 
 
+def test_detect_fusions_returns_updated_samples(ericscript_run, sample):
+    samples = [[sample]]
+    result = rnaseq.detect_fusions(samples)
+    assert result == [[ericscript_run.return_value]]
+
+
+def test_detect_fusions_calls_caller_with_sample_dict(ericscript_run, sample):
+    samples = [[sample]]
+    rnaseq.detect_fusions(samples)
+    ericscript_run.assert_called_once_with(sample)
