9cd9a159e5e30622c11d021e35836c54bda14a76
Tetiana Khotiaintseva <tetiana.khotiaintseva@ardigen.com>
2016-12-07 14:27:57 +0100

    Refactor fusion logic in tophat

--- tests/unit/ngsalign/test_tophat.py
+++ tests/unit/ngsalign/test_tophat.py
@@ -1,47 +1,59 @@
 from bcbio.ngsalign import tophat
 
+import mock
+import pytest
 
-def test_set_fusion_mode_false():
-    config = {
-        'algorithm': {
-            'fusion_mode': False
-        }
-    }
+
+@pytest.yield_fixture
+def mock_should_run(mocker):
+    yield mocker.patch('bcbio.ngsalign.tophat._should_run_fusion')
+
+
+@pytest.mark.parametrize('config', [
+    {'algorithm': {'fusion_mode': False}},
+    {'config': {'algorithm': {'fusion_mode': False}}},
+])
+def test_should_not_run_fusion_when_fusion_mode_false(config):
+    result = tophat._should_run_fusion(config)
+    assert result is False
+
+
+@pytest.mark.parametrize('config', [
+    {'algorithm': {'fusion_mode': True}},
+    {'config': {'algorithm': {'fusion_mode': True}}},
+])
+def test_should_run_fusion_when_fusion_mode_and_default_caller(config):
+    result = tophat._should_run_fusion(config)
+    assert result is True
+
+
+@pytest.mark.parametrize('config', [
+    {'algorithm': {'fusion_mode': True, 'fusion_caller': 'TEST'}},
+    {'config': {'algorithm': {'fusion_mode': True, 'fusion_caller': 'TEST'}}},
+])
+def test_should_not_run_fusion_when_fusion_mode_and_caller_not_tophat(config):
+    result = tophat._should_run_fusion(config)
+    assert result is False
+
+
+@pytest.mark.parametrize('config', [
+    {'algorithm': {'fusion_mode': True, 'fusion_caller': 'tophat'}},
+    {'config': {'algorithm': {'fusion_mode': True, 'fusion_caller': 'tophat'}}},
+])
+def test_should_run_fusion_when_fusion_mode_and_caller_tophat(config):
+    result = tophat._should_run_fusion(config)
+    assert result is True
+
+
+def test_set_fusion_mode_when_should_run_fusion(mock_should_run):
+    mock_should_run.return_value = True
     opts = {}
-    tophat._set_fusion_mode(opts, config)
+    tophat._set_fusion_mode(opts, mock.Mock())
+    assert opts['fusion-search'] is True
+
+
+def test_set_fusion_mode_when_should_not_run_fusion(mock_should_run):
+    mock_should_run.return_value = False
+    opts = {}
+    tophat._set_fusion_mode(opts, mock.Mock())
     assert opts == {}
-
-
-def test_set_fusion_mode_true__default_caller():
-    config = {
-        'algorithm': {
-            'fusion_mode': True
-        }
-    }
-    opts = {}
-    tophat._set_fusion_mode(opts, config)
-    assert opts == {'fusion-search': True}
-
-
-def test_set_fusion_mode_true__wrong_caller():
-    config = {
-        'algorithm': {
-            'fusion_mode': True,
-            'fusion_caller': 'ericscript',
-        }
-    }
-    opts = {}
-    tophat._set_fusion_mode(opts, config)
-    assert opts == {}
-
-
-def test_set_fusion_mode_true__tophat_caller():
-    config = {
-        'algorithm': {
-            'fusion_mode': True,
-            'fusion_caller': 'tophat',
-        }
-    }
-    opts = {}
-    tophat._set_fusion_mode(opts, config)
-    assert opts == {'fusion-search': True}
