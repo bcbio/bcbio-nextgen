f017c4876a1aaf9fe2aacfe91c16e5274a0a0f9b
Tetiana Khotiaintseva <tetiana.khotiaintseva@ardigen.com>
2016-12-20 11:36:51 +0100

    Get EricScript env from config

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -3,14 +3,41 @@ import pytest
 
 from bcbio import utils
 
+TEST_CONDA_ENV = '/path/to/conda/env'
+
 
 @pytest.yield_fixture
-def environ(mocker):
-    environ = mocker.patch('bcbio.utils.os.environ.copy')
+def bcbio_env(mocker):
+    environ = mocker.patch('bcbio.utils.get_bcbio_env')
     environ.return_value = {'PATH': '/usr/bin:/bin'}
+    yield environ
 
 
-def test_get_ericscript_env(environ):
-    env = utils.get_ericscript_env(mock.Mock())
-    expected = '/usr/local/share/bcbio-nextgen/anaconda/envs/ericscript/bin'
-    assert env['PATH'] == '%s:/usr/bin:/bin' % expected
+@pytest.yield_fixture
+def which(mocker):
+    yield mock.patch('bcbio.utils.which')
+
+
+@pytest.fixture
+def config():
+    return {'config': {'resources': {'ericscript': {'env': TEST_CONDA_ENV}}}}
+
+
+def test_get_ericscript_env_if_executable_not_found(which, bcbio_env, config):
+    which.return_value = None
+    env = utils.get_ericscript_env(config)
+    assert env['PATH'].startswith(TEST_CONDA_ENV)
+
+
+def test_ericscript_env_if_exec_found_in_bcbio_env(which, bcbio_env, config):
+    which.return_value = '/some/path'
+    env = utils.get_ericscript_env(config)
+    assert env == bcbio_env.return_value
+
+
+def test_get_ericscript_env_raises_runtime_error_when_install_not_found(
+        which, bcbio_env):
+    which.return_value = None
+    config = {'config': {'resources': {}}}
+    with pytest.raises(RuntimeError):
+        utils.get_ericscript_env(config)
