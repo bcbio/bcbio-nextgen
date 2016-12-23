67202ba4c60417e5f223b03365c45118760aba55
Tetiana Khotiaintseva <tetiana.khotiaintseva@ardigen.com>
2016-12-23 17:14:22 +0100

    Add docstrings

--- bcbio/rnaseq/ericscript.py
+++ bcbio/rnaseq/ericscript.py
@@ -1,3 +1,16 @@
+"""Runs gene fusion caller with EricScript.
+Install EricScript via `bcbio upgrade --toolplus ericscript`,
+or manually add the path to conda environment where it can be found
+to the system config.
+Reference data can be installed via `bcbio upgrade --datatarget ericscript`.
+Alternatively, you can add path to the database into the system config.
+
+EricScript requires bwa index to be built for its reference data.
+
+To run gene fusion detection on disambiguated reads, we convert the .bam file
+which was output by disambiguate to fastq files.
+
+"""
 import os
 
 from bcbio import utils
@@ -51,6 +64,20 @@ def build_bwa_index_if_absent(es_config, sample_config):
 
 
 class EricScriptConfig(object):
+    """This class which encapsulates access to the data
+    related to EricScript in the sample config dictionary.
+
+    Public constants:
+        info_message: text message passed as an argument to do.run
+        EXECUTABLE: name of the EricScipt command
+
+    Private constants:
+        _OUTPUT_DIR_NAME: name of the dir created in working directory for
+    ericscript ouput
+        _REF_INDEX: relative path to BWA index (one of the files) used to
+    detect if the index is present.
+        _REF_FASTA: relative path to the fasta reference file.
+    """
     info_message = 'Detect gene fusions with EricScript'
     EXECUTABLE = 'ericscript.pl'
     _OUTPUT_DIR_NAME = 'ericscript'
@@ -67,6 +94,14 @@ class EricScriptConfig(object):
         self._sample_out_dir = None
 
     def get_run_command(self, tx_output_dir, input_files):
+        """Constructs a command to run EricScript via do.run function.
+
+        :param tx_output_dir: A location where all EricScript output will be
+        written during execution.
+        :param input_files: an iterable with paths to 2 fastq files
+        with input data.
+        :return: list
+        """
         logger.debug("Input data: %s" % ', '.join(input_files))
         return [
             self.EXECUTABLE,
@@ -77,18 +112,28 @@ class EricScriptConfig(object):
 
     @property
     def env(self):
+        """A dictionary with environment variables to pass to do.run command.
+        The path to ericscript conda environment is prepended to the PATH var.
+        """
         if self._env is None:
             self._env = self._get_env()
-        return self._env
+        return self._env.copy()
 
     @property
     def output_dir(self):
+        """Absolute path to permanent location in working directory
+        where EricScript output will be stored.
+        """
         if self._output_dir is None:
             self._output_dir = self._get_output_dir()
         return self._output_dir
 
     @property
     def sample_out_dir(self):
+        """Absolute path to permanent location in working directory
+        where EricScript output for the current sample will be stored.
+        (a subdirectory of `output_dir`)
+        """
         if self._sample_out_dir is None:
             self._sample_out_dir = os.path.join(
                 self.output_dir, self._sample_name
@@ -97,10 +142,12 @@ class EricScriptConfig(object):
 
     @property
     def reference_index(self):
+        """Absolute path to the BWA index for EricScript reference data."""
         return os.path.join(self._db_location, self._REF_INDEX)
 
     @property
     def reference_fasta(self):
+        """Absolute path to the fasta file with EricScript reference data."""
         return os.path.join(self._db_location, self._REF_FASTA)
 
     def _get_env(self):
