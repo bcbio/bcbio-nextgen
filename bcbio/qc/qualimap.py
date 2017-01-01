94b497dbe575b66e55fc6ba3d29345965e1c3c86
chapmanb <chapmanb@50mail.com>
2017-01-01 05:08:14 -0500

    CWL QC: fix Qualimap graph generation in MultiQC
    - Ensure we set genome build correctly for human GC background
    - Correct directory for raw data files from Qualimap

--- bcbio/qc/qualimap.py
+++ bcbio/qc/qualimap.py
@@ -58,9 +58,10 @@ def run(bam_file, data, out_dir):
                    "--skip-duplicated --skip-dup-mode 0 "
                    "-nt {num_cores} --java-mem-size={max_mem} {options}")
             species = None
-            if tz.get_in(("genome_resources", "aliases", "human"), data, ""):
+            if (tz.get_in(("genome_resources", "aliases", "human"), data, "")
+                  or dd.get_genome_build(data).startswith(("hg", "GRCh"))):
                 species = "HUMAN"
-            elif any(tz.get_in("genome_build", data, "").startswith(k) for k in ["mm", "GRCm"]):
+            elif dd.get_genome_build(data).startswith(("mm", "GRCm")):
                 species = "MOUSE"
             if species in ["HUMAN", "MOUSE"]:
                 cmd += " -gd {species}"
@@ -76,7 +77,8 @@ def run(bam_file, data, out_dir):
             tx_results_file = os.path.join(tx_results_dir, "genome_results.txt")
             cmd = "sed -i 's/bam file = .*/bam file = %s.bam/' %s" % (dd.get_sample_name(data), tx_results_file)
             do.run(cmd, "Fix Name Qualimap for {}".format(dd.get_sample_name(data)))
-    return {"base": results_file, "secondary": glob.glob(os.path.join(out_dir, "raw_data_qualimapReport", "*.txt"))}
+    return {"base": results_file,
+            "secondary": glob.glob(os.path.join(results_dir, "raw_data_qualimapReport", "*.txt"))}
 
 def _parse_qualimap_metrics(report_file, data):
     """Extract useful metrics from the qualimap HTML report file.
