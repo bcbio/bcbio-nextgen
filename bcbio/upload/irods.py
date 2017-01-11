e33a0456bb8602c7897bf4cb81c1a0e33beaf4e5
Matthias De Smet <matthias.desmet@ugent.be>
2017-01-11 08:07:38 +0100

    added support for tickets based access, added placeholder functions for downloads

--- bcbio/upload/irods.py
+++ bcbio/upload/irods.py
@@ -1,11 +1,12 @@
 """
-    Handle upload and retrieval of files from iRods. This method requires a preconfigured connection
-    to an iRods repository throught the `iinit` command
+    Handle upload and retrieval of files from iRODS. This method requires a preconfigured connection
+    to an iRODS repository throught the `iinit` command
     config options:
     upload:
       method: irods
-      dir: absolute parent path in iRods repository
-      resource: (optional) iRods resource name, if other than default
+      dir: absolute parent path in iRODS repository
+      resource: (optional) iRODS resource name, if other than default
+      ticket: (optional) iRODS ticket, for ticket based access
       extra: (optional)["list","of","arbitrary","options","that","can","be","passed","to","iput"]
 
 """
@@ -17,8 +18,21 @@ import sys
 from bcbio.provenance import do
 from bcbio.upload import filesystem
 
+def _format_metadata(metadata):
+    """
+    Format metadata to use in iCommands CLI
+    requisite format: "attr1;val1;unit1;attr2;val2;unit2;"
+    """
+    meta_string=""
+    #assuming metadata is a dictionary: {key1:value1,key2:value2}
+    for keyname, value in metadata.iteritems():
+        meta_string += str(keyname;value;;)
+    return meta_string
+
+
 def update_file(finfo, sample_info, config):
-    """Update the file to an iRods repository.
+    """
+    Update the file to an iRODS repository.
     """
     ffinal = filesystem.update_file(finfo, sample_info, config, pass_uptodate=True)
     if os.path.isdir(ffinal):
@@ -51,23 +65,26 @@ def _upload_file_icommands_cli(local_fname, keyname, config=None, metadata=None)
     if config:
         if config.get("resource"):
             args += ["-R", config.get("resource")]
+        if config.get("ticket"):
+            args += ["-t", config.get("ticket")]
         if config.get("extra"):
             args += config.get("extra")
     if metadata:
         args += ["--metadata", metadata]
 
     cmd = ["iput"] + args + [local_fname, irods_fname]
-    do.run(cmd, "Upload to iRods: %s %s" % (config.get("zone"), keyname))
+    do.run(cmd, "Upload to iRODS: %s" % (keyname))
 
-def _format_metadata(metadata):
+def download_file(file):
     """
-    Format metadata to use in icommands CLI
-    requisite format: "attr1;val1;unit1;attr2;val2;unit2;"
+    Download file from an iRODS repository
     """
-    meta_string=""
-    #assuming metadata is a dictionary: {key1:value1,key2:value2}
-    for keyname, value in metadata.iteritems():
-        meta_string += str(keyname;value;;)
-    return meta_string
+    _download_file_icommands_cli(file)
 
-
+def _download_file_icommands_cli(file):
+    """
+    Download file from iRODS using the iCommands CLI
+    example: iget -K $file
+    """
+    cmd = ["iget","-K"] + [file]
+    do.run(cmd, "Download from iRODS: %s" % (file))
