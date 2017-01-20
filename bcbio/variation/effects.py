1f55a97d9c4c95d8b535e870a3113cf278ce7cdd
Matthias De Smet <matthias.desmet@ugent.be>
2017-01-20 10:08:41 +0100

    VEP: added dbscSNV plugin to defaults, only used when dataset is available

--- bcbio/variation/effects.py
+++ bcbio/variation/effects.py
@@ -135,7 +135,7 @@ def run_vep(in_file, data):
                 if is_human:
                     plugin_fns = {"dbnsfp": _get_dbnsfp, "loftee": _get_loftee, "dbscsnv": _get_dbscsnv,
                                   "maxentscan": _get_maxentscan, "genesplicer": _get_genesplicer}
-                    plugins = tz.get_in(("config", "resources", "vep", "plugins"), data, ["dbnsfp", "loftee"])
+                    plugins = tz.get_in(("config", "resources", "vep", "plugins"), data, ["dbnsfp", "loftee", "dbscsnv"])
                     for plugin in plugins:
                         plugin_args, plugin_fields = plugin_fns[plugin](data)
                         config_args += plugin_args
