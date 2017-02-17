a6df96038acbbbc0543cc7ce8ed4698aa0d9fd85
chapmanb <chapmanb@50mail.com>
2017-02-17 15:03:46 -0500

    Damage: annotate INFO field instead of FILTER
    Move DKFZBiasFilter outputs from FILTER field into an INFO annotation,
    DKFZBias. This allows detection of damage artifacts without explicit
    filters.

--- bcbio/qc/damage.py
+++ bcbio/qc/damage.py
@@ -6,12 +6,13 @@ from bcbio import utils
 from bcbio.distributed.transaction import file_transaction
 from bcbio.pipeline import datadict as dd
 from bcbio.provenance import do
+from bcbio.variation import vcfutils
 from bcbio.qc import variant
 
 def run(bam_file, data, out_dir):
     out = {}
     vcinfo = variant.get_active_vcinfo(data)
-    if vcinfo:
+    if vcinfo and vcfutils.vcf_has_variants(vcinfo["vrn_file"]):
         out_file = os.path.join(utils.safe_makedir(out_dir),
                                 "%s-damage.yaml" % (dd.get_sample_name(data)))
         if not utils.file_exists(out_file):
--- bcbio/variation/damage.py
+++ bcbio/variation/damage.py
@@ -5,8 +5,11 @@ INFO level annotations of low frequency variants:
 
 https://github.com/bcbio/bcbio.github.io/blob/master/_posts/2017-01-31-damage-filters.md
 """
+import contextlib
 import os
 import shutil
+
+import cyvcf2
 
 from bcbio import utils
 from bcbio.distributed.transaction import file_transaction
@@ -16,15 +19,17 @@ from bcbio.variation import vcfutils
 
 def run_filter(vrn_file, align_bam, ref_file, data, items):
     """Filter and annotate somatic VCFs with damage/bias artifacts on low frequency variants.
+
+    Moves damage estimation to INFO field, instead of leaving in FILTER.
     """
     if not should_filter(items):
         return data
     else:
-        out_file = "%s-damage.vcf" % utils.splitext_plus(vrn_file)[0]
-        out_plot_files = ["%s%s" % (utils.splitext_plus(out_file)[0], ext)
+        raw_file = "%s-damage.vcf" % utils.splitext_plus(vrn_file)[0]
+        out_plot_files = ["%s%s" % (utils.splitext_plus(raw_file)[0], ext)
                           for ext in ["_seq_bias_simplified.pdf", "_pcr_bias_simplified.pdf"]]
-        if not utils.file_uptodate(out_file, vrn_file) and not utils.file_uptodate(out_file + ".gz", vrn_file):
-            with file_transaction(items[0], out_file) as tx_out_file:
+        if not utils.file_uptodate(raw_file, vrn_file) and not utils.file_uptodate(raw_file + ".gz", vrn_file):
+            with file_transaction(items[0], raw_file) as tx_out_file:
                 # Does not apply --qcSummary plotting due to slow runtimes
                 cmd = ["dkfzbiasfilter.py", "--filterCycles", "1", "--passOnly",
                        "--tempFolder", os.path.dirname(tx_out_file),
@@ -35,11 +40,47 @@ def run_filter(vrn_file, align_bam, ref_file, data, items):
                                                 os.path.basename(out_plot))
                     if utils.file_exists(tx_plot_file):
                         shutil.move(tx_plot_file, out_plot)
-        out_file = vcfutils.bgzip_and_index(out_file, items[0]["config"])
-        data["vrn_file"] = out_file
+        raw_file = vcfutils.bgzip_and_index(raw_file, items[0]["config"])
+        data["vrn_file"] = _filter_to_info(raw_file, items[0])
         out_plot_files = [x for x in out_plot_files if utils.file_exists(x)]
         data["damage_plots"] = out_plot_files
         return data
+
+def _filter_to_info(in_file, data):
+    """Move DKFZ filter information into INFO field.
+    """
+    header = ("""##INFO=<ID=DKFZBias,Number=.,Type=String,"""
+              """Description="Bias estimation based on unequal read support from DKFZBiasFilterVariant Depth">\n""")
+    out_file = "%s-ann.vcf" % utils.splitext_plus(in_file)[0]
+    if not utils.file_uptodate(out_file, in_file) and not utils.file_uptodate(out_file + ".gz", in_file):
+        with file_transaction(data, out_file) as tx_out_file:
+            with utils.open_gzipsafe(in_file) as in_handle:
+                with open(tx_out_file, "w") as out_handle:
+                    for line in in_handle:
+                        if line.startswith("#CHROM"):
+                            out_handle.write(header + line)
+                        elif line.startswith("#"):
+                            out_handle.write(line)
+                        else:
+                            out_handle.write(_rec_filter_to_info(line))
+    return vcfutils.bgzip_and_index(out_file, data["config"])
+
+def _rec_filter_to_info(line):
+    """Move a DKFZBias filter to the INFO field, for a record.
+    """
+    parts = line.rstrip().split("\t")
+    move_filters = {"bSeq": "strand", "bPcr": "damage"}
+    new_filters = []
+    bias_info = []
+    for f in parts[6].split(";"):
+        if f in move_filters:
+            bias_info.append(move_filters[f])
+        elif f not in ".":
+            new_filters.append(f)
+    if bias_info:
+        parts[7] += ";DKFZBias=%s" % ",".join(bias_info)
+    parts[6] = ";".join(new_filters or ["PASS"])
+    return "\t".join(parts) + "\n"
 
 def should_filter(items):
     """Check if we should do damage filtering on somatic calling with low frequency events.
