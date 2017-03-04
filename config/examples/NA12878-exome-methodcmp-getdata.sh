050c61951a6f2408be853a7188360dd8c3a334bb
chapmanb <chapmanb@50mail.com>
2017-03-04 10:50:06 +0000

    Examples: use bcbio installed NGv3 for exome

--- config/examples/NA12878-exome-methodcmp-getdata.sh
+++ config/examples/NA12878-exome-methodcmp-getdata.sh
@@ -17,8 +17,6 @@ mkdir -p input
 cd input
 wget -c -O NA12878-NGv3-LAB1360-A_1.fastq.gz https://s3.amazonaws.com/bcbio_nextgen/NA12878-NGv3-LAB1360-A_1.fastq.gz
 wget -c -O NA12878-NGv3-LAB1360-A_2.fastq.gz https://s3.amazonaws.com/bcbio_nextgen/NA12878-NGv3-LAB1360-A_2.fastq.gz
-wget -c -O NGv3.bed.gz https://s3.amazonaws.com/bcbio_nextgen/NGv3.bed.gz
-gunzip *.bed.gz
 cd ..
 
 mkdir -p work
