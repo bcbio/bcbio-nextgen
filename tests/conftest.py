b15a7b9807aadc396039b2461e697ce1072181ce
chapmanb <chapmanb@50mail.com>
2017-07-25 14:55:41 -0400

    Tests: bump genome tarball to fix space issues
    Trying to get TravisCI working again with less space for tests
    (no STAR index for mm9).

--- tests/conftest.py
+++ tests/conftest.py
@@ -80,7 +80,7 @@ def install_test_files(data_dir):
     DlInfo = collections.namedtuple("DlInfo", "fname dirname version")
     download_data = [
         DlInfo("110106_FC70BUKAAXX.tar.gz", None, None),
-        DlInfo("genomes_automated_test.tar.gz", "genomes", 31),
+        DlInfo("genomes_automated_test.tar.gz", "genomes", 32),
         DlInfo("110907_ERP000591.tar.gz", None, None),
         DlInfo("100326_FC6107FAAXX.tar.gz", None, 11),
         DlInfo("tcga_benchmark.tar.gz", None, 3),
