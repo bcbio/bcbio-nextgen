4d5a24c6494c274137517c8bdc6b1c902c4e6339
vladsaveliev <vladsaveliev@me.com>
2017-08-25 23:22:48 +0300

    MultiQC: support custom options through resources

--- bcbio/qc/multiqc.py
+++ bcbio/qc/multiqc.py
@@ -53,7 +53,9 @@ def summary(*samples):
                     else:
                         export_tmp = ""
                     path_export = utils.local_path_export()
-                    cmd = "{path_export}{export_tmp} {multiqc} -f -l {input_list_file} -o {tx_out}"
+                    other_opts = config_utils.get_resources("multiqc", samples[0]["config"]).get("options", [])
+                    other_opts = " ".join(other_opts)
+                    cmd = "{path_export}{export_tmp} {multiqc} -f -l {input_list_file} {other_opts} -o {tx_out}"
                     do.run(cmd.format(**locals()), "Run multiqc")
                     if utils.file_exists(os.path.join(tx_out, "multiqc_report.html")):
                         shutil.move(os.path.join(tx_out, "multiqc_report.html"), out_file)
