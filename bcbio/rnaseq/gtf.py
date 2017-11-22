8672a6ee94328d404f89f2a3b0088df8dcb92455
Lorena Pantano <lorena.pantano@gmail.com>
2017-11-22 12:16:14 -0500

    QC:accept GTF without exon_number for qualimap

--- bcbio/rnaseq/gtf.py
+++ bcbio/rnaseq/gtf.py
@@ -328,9 +328,8 @@ def is_qualimap_compatible(gtf):
     def qualimap_compatible(feature):
         gene_id = feature.attributes.get('gene_id', [None])[0]
         transcript_id = feature.attributes.get('transcript_id', [None])[0]
-        exon_number = feature.attributes.get('exon_number', [None])[0]
         gene_biotype = feature.attributes.get('gene_biotype', [None])[0]
-        return gene_id and transcript_id and exon_number and gene_biotype
+        return gene_id and transcript_id and gene_biotype
     for feature in db.all_features():
         if qualimap_compatible(feature):
             return True
