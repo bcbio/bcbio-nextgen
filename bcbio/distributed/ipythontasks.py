8f1c24de1fcf5337cb82a8d5e196f913d21a15bd
chapmanb <chapmanb@50mail.com>
2018-06-21 05:48:11 -0400

    Fix typo in bcbiornaseq import/require

--- bcbio/distributed/ipythontasks.py
+++ bcbio/distributed/ipythontasks.py
@@ -541,7 +541,7 @@ def upload_samples_project(*args):
     with _setup_logging(args) as config:
         return ipython.zip_args(apply(upload.project_from_sample, *args))
 
-@require(bcbiornaeq)
+@require(bcbiornaseq)
 def run_bcbiornaseqload(*args):
     args = ipython.unzip_args(args)
     with _setup_logging(args) as config:
