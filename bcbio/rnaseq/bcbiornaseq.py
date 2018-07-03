1cdb1e6cd7a6a725203167f7be649c7824dee6bd
roryk <rory.kirchner@gmail.com>
2018-07-02 20:14:56 -0400

    Automatically render the bcbioRNASeq quality control report.

--- bcbio/rnaseq/bcbiornaseq.py
+++ bcbio/rnaseq/bcbiornaseq.py
@@ -8,6 +8,9 @@ from bcbio.provenance import do
 from bcbio.pipeline import datadict as dd
 
 def make_bcbiornaseq_object(data):
+    """
+    load the initial bcb.rda object using bcbioRNASeq
+    """
     if "bcbiornaseq" not in dd.get_tools_on(data):
         return data
     upload_dir = tz.get_in(("upload", "dir"), data)
@@ -18,13 +21,62 @@ def make_bcbiornaseq_object(data):
     loadstring = create_load_string(upload_dir, groups, organism)
     r_file = os.path.join(report_dir, "load_bcbioRNAseq.R")
     with file_transaction(r_file) as tmp_file:
-        write_load_bcbiornaseq_file(loadstring, tmp_file)
+        memoize_write_file(loadstring, tmp_file)
     rcmd = Rscript_cmd()
     with chdir(report_dir):
         do.run([rcmd, r_file], "Loading bcbioRNASeq object.")
+    make_quality_report(data)
     return data
 
+def make_quality_report(data):
+    """
+    create and render the bcbioRNASeq quality report
+    """
+    if "bcbiornaseq" not in dd.get_tools_on(data):
+        return data
+    upload_dir = tz.get_in(("upload", "dir"), data)
+    report_dir = os.path.join(upload_dir, "bcbioRNASeq")
+    safe_makedir(report_dir)
+    quality_rmd = os.path.join(report_dir, "quality_control.Rmd")
+    quality_html = os.path.join(report_dir, "quality_control.html")
+    quality_rmd = rmarkdown_draft(quality_rmd, "quality_control", "bcbioRNASeq")
+
+def rmarkdown_draft(filename, template, package):
+    """
+    create a draft rmarkdown file from an installed template
+    """
+    draft_template = Template(
+        'rmarkdown::draft("$filename", template="$template", package="$package", edit=FALSE)'
+    )
+    draft_string = draft_template.substitute(
+        filename=filename, template=template, package=package)
+    report_dir = os.path.dirname(filename)
+    rcmd = Rscript_cmd()
+    with chdir(report_dir):
+        do.run([rcmd, "-e", draft_string], "Creating bcbioRNASeq quality control template.")
+        do.run(["sed", "-i", "s/YYYY-MM-DD\///g", filename], "Editing bcbioRNAseq quality control template.")
+    render_rmarkdown_file(filename)
+    return filename
+
+def render_rmarkdown_file(filename):
+    """
+    render a rmarkdown file using the rmarkdown library
+    """
+    render_template = Template(
+        'rmarkdown::render("$filename")'
+    )
+    render_string = render_template.substitute(
+        filename=filename)
+    report_dir = os.path.dirname(filename)
+    rcmd = Rscript_cmd()
+    with chdir(report_dir):
+        do.run([rcmd, "-e", render_string], "Rendering bcbioRNASeq quality control report.")
+    return filename
+
 def create_load_string(upload_dir, groups=None, organism=None):
+    """
+    create the code necessary to load the bcbioRNAseq object
+    """
     libraryline = 'library(bcbioRNASeq)'
     load_template = Template(
         ('bcb <- bcbioRNASeq(uploadDir="$upload_dir",'
@@ -41,15 +93,14 @@ def create_load_string(upload_dir, groups=None, organism=None):
     else:
         groups = _quotestring("sampleName")
     if organism:
-        load_bcbio = load_template.substitute(upload_dir=upload_dir,
-                                              groups=groups,
-                                              organism=organism)
+        load_bcbio = load_template.substitute(
+            upload_dir=upload_dir, groups=groups, organism=organism)
     else:
         load_bcbio = load_noorganism_template.substitute(upload_dir=upload_dir,
                                                 groups=groups)
     return ";\n".join([libraryline, load_bcbio, flatline, saveline])
 
-def write_load_bcbiornaseq_file(string, filename):
+def memoize_write_file(string, filename):
     if file_exists(filename):
         return filename
     with file_transaction(filename) as tx_out_file:
@@ -57,9 +108,12 @@ def write_load_bcbiornaseq_file(string, filename):
             out_handle.write(string)
     return filename
 
-def _quotestring(string):
+def _quotestring(string, double=True):
     """ escape quote a string """
-    return "\"" + string + "\""
+    if double:
+        return "\"" + string + "\""
+    else:
+        return "\'" + string + "\'"
 
 def _list2Rlist(xs):
     """ convert a python list to an R list """
