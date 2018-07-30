f5901220672d9508f712e2d7b5943d29f2988ec0
chapmanb <chapmanb@50mail.com>
2018-07-30 09:58:59 -0400

    RNA-seq variant calling: skip vcfanno if not calling
    Checks for missing and non-configured variant files. Thanks to @ohofmann

--- bcbio/pipeline/rnaseq.py
+++ bcbio/pipeline/rnaseq.py
@@ -53,7 +53,8 @@ def rnaseq_variant_calling(samples, run_parallel):
         for d in joint.square_off(samples, run_parallel):
             out.extend([[to_single_data(xs)] for xs in multi.split_variants_by_sample(to_single_data(d))])
         samples = out
-    samples = run_parallel("run_rnaseq_ann_filter", samples)
+    if variantcaller:
+        samples = run_parallel("run_rnaseq_ann_filter", samples)
     if variantcaller and ("gatk-haplotype" in variantcaller):
         out = []
         for data in (to_single_data(xs) for xs in samples):
@@ -90,12 +91,13 @@ def run_rnaseq_ann_filter(data):
     """Run RNA-seq annotation and filtering.
     """
     data = to_single_data(data)
-    ann_file = vcfanno.run_vcfanno(dd.get_vrn_file(data), ["rnaedit"], data)
-    if ann_file:
-        data = dd.set_vrn_file(data, ann_file)
-    ann_file = population.run_vcfanno(dd.get_vrn_file(data), data)
-    if ann_file:
-        data = dd.set_vrn_file(data, ann_file)
+    if dd.get_vrn_file(data):
+        ann_file = vcfanno.run_vcfanno(dd.get_vrn_file(data), ["rnaedit"], data)
+        if ann_file:
+            data = dd.set_vrn_file(data, ann_file)
+        ann_file = population.run_vcfanno(dd.get_vrn_file(data), data)
+        if ann_file:
+            data = dd.set_vrn_file(data, ann_file)
     variantcaller = dd.get_variantcaller(data)
     if variantcaller and ("gatk-haplotype" in variantcaller):
         filter_file = variation.gatk_filter_rnaseq(dd.get_vrn_file(data), data)
