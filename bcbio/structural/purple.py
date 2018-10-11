44b174f7b3f7aa92e133734cab62d3ee48c6062d
chapmanb <chapmanb@50mail.com>
2018-10-11 06:15:36 -0400

    SV: PURPLE progress -- initial COBALT integration
    PURPLE requires genomes split into 1000bp windows, so currently
    not possible to use CNVkit's flexible target/anti-target binning
    as an input. Will need to use COBALT directly, so added notes
    and skeleton. Still not a functional implementation #2532

--- bcbio/structural/purple.py
+++ bcbio/structural/purple.py
@@ -5,9 +5,12 @@ https://github.com/hartwigmedical/hmftools/tree/master/purity-ploidy-estimator
 import csv
 import os
 
+import toolz as tz
+
 from bcbio import utils
 from bcbio.log import logger
 from bcbio.pipeline import datadict as dd
+from bcbio.distributed.transaction import file_transaction
 from bcbio.variation import vcfutils
 
 def run(items):
@@ -19,7 +22,8 @@ def run(items):
     work_dir = _sv_workdir(paired.tumor_data)
     from bcbio import heterogeneity
     het_file = _amber_het_file(heterogeneity.get_variants(paired.tumor_data), work_dir, paired)
-    print(het_file)
+    depth_file = _run_cobalt(paired, work_dir)
+    print(het_file, depth_file)
     return items
 
 class OutWriter:
@@ -58,6 +62,47 @@ def _amber_het_file(vrn_files, work_dir, paired):
     amber_dir = utils.safe_makedir(os.path.join(work_dir, "amber"))
     out_file = os.path.join(amber_dir, "%s.amber.baf" % dd.get_sample_name(paired.tumor_data))
     utils.symlink_plus(prep_file, out_file)
+    return out_file
+
+def _run_cobalt(paired, work_dir):
+    """Run Cobalt for counting read depth across genomic windows.
+
+    PURPLE requires even 1000bp windows so use integrated counting solution
+    directly rather than converting from CNVkit calculations. If this approach
+    is useful should be moved upstream to be available to other tools as
+    an input comparison.
+
+    https://github.com/hartwigmedical/hmftools/tree/master/count-bam-lines
+    """
+    pass
+
+def _cobalt_ratio_file(paired, work_dir):
+    """Convert CNVkit binning counts into cobalt ratio output.
+
+    This contains read counts plus normalization for GC, from section 7.2
+    "Determine read depth ratios for tumor and reference genomes"
+
+    https://www.biorxiv.org/content/biorxiv/early/2018/09/20/415133.full.pdf
+
+    Since CNVkit cnr files already have GC bias correction, we re-center
+    the existing log2 ratios to be around 1, rather than zero, which matches
+    the cobalt expectations.
+
+    XXX This doesn't appear to be a worthwhile direction since PURPLE requires
+    1000bp even binning. We'll leave this here as a starting point for future
+    work but work on using cobalt directly.
+    """
+    cobalt_dir = utils.safe_makedir(os.path.join(work_dir, "cobalt"))
+    out_file = os.path.join(cobalt_dir, "%s.cobalt" % dd.get_sample_name(paired.tumor_data))
+    if not utils.file_exists(out_file):
+        cnr_file = tz.get_in(["depth", "bins", "normalized"], paired.tumor_data)
+        with file_transaction(paired.tumor_data, out_file) as tx_out_file:
+            with open(tx_out_file, "w") as out_handle:
+                writer = csv.writer(out_handle, delimiter="\t")
+                writer.writerow(["Chromosome", "Position", "ReferenceReadCount", "TumorReadCount",
+                                 "ReferenceGCRatio", "TumorGCRatio", "ReferenceGCDiploidRatio"])
+        print(cnr_file)
+        raise NotImplementedError
     return out_file
 
 def _sv_workdir(data):
