4a8aa1a8d560fad39ed4ffca370aa8b859fd0225
chapmanb <chapmanb@50mail.com>
2018-11-14 09:01:49 -0500

    ChIP-seq: avoid QC errors when peak file missing
    Fixes #2573

--- bcbio/qc/chipseq.py
+++ bcbio/qc/chipseq.py
@@ -6,11 +6,9 @@ import glob
 from bcbio.log import logger
 from bcbio import utils
 from bcbio.bam.readstats import number_of_mapped_reads
-from bcbio.pipeline import config_utils
 from bcbio.pipeline import datadict as dd
 from bcbio.provenance import do
-from bcbio.distributed.transaction import file_transaction, tx_tmpdir
-from bcbio.log import logger
+from bcbio.distributed.transaction import tx_tmpdir
 
 
 supported = ["hg19", "hg38", "mm10", "mm9", "rn4", "ce6", "dm3"]
@@ -18,13 +16,12 @@ supported = ["hg19", "hg38", "mm10", "mm9", "rn4", "ce6", "dm3"]
 def run(bam_file, sample, out_dir):
     """Standard QC metrics for chipseq"""
     out = {}
-    work_dir = dd.get_work_dir(sample)
-    sample_name = dd.get_sample_name(sample)
     # if "rchipqc" in dd.get_tools_on(sample):
     #    out = chipqc(bam_file, sample, out_dir)
 
-    peaks = sample.get("peaks_files", {}).get("main", "NULL")
-    out.update(_reads_in_peaks(bam_file, peaks, sample))
+    peaks = sample.get("peaks_files", {}).get("main")
+    if peaks:
+        out.update(_reads_in_peaks(bam_file, peaks, sample))
     return out
 
 def _reads_in_peaks(bam_file, peaks_file, sample):
@@ -36,40 +33,43 @@ def _reads_in_peaks(bam_file, peaks_file, sample):
 
 def chipqc(bam_file, sample, out_dir):
     """Attempt code to run ChIPQC bioconductor packate in one sample"""
-    work_dir = dd.get_work_dir(sample)
     sample_name = dd.get_sample_name(sample)
     logger.warning("ChIPQC is unstable right now, if it breaks, turn off the tool.")
     if utils.file_exists(out_dir):
         return _get_output(out_dir)
     with tx_tmpdir() as tmp_dir:
         rcode = _sample_template(sample, tmp_dir)
-        # local_sitelib = utils.R_sitelib()
-        rscript = utils.Rscript_cmd()
-        do.run([rscript, "--no-environ", rcode], "ChIPQC in %s" % sample_name, log_error=False)
-        shutil.move(tmp_dir, out_dir)
+        if rcode:
+            # local_sitelib = utils.R_sitelib()
+            rscript = utils.Rscript_cmd()
+            do.run([rscript, "--no-environ", rcode], "ChIPQC in %s" % sample_name, log_error=False)
+            shutil.move(tmp_dir, out_dir)
     return _get_output(out_dir)
 
 def _get_output(out_dir):
-    return {'secondary': glob.glob(out_dir)}
+    files = glob.glob(out_dir)
+    if files:
+        return {'secondary': files}
+    else:
+        return {}
 
 def _sample_template(sample, out_dir):
     """R code to get QC for one sample"""
     bam_fn = dd.get_work_bam(sample)
     genome = dd.get_genome_build(sample)
-    if genome not in supported:
-        genome = "NULL"
-    peaks = sample.get("peaks_files", []).get("main", "NULL")
-    r_code = ("library(ChIPQC);\n"
-              "sample = ChIPQCsample(\"{bam_fn}\","
-              "\"{peaks}\", "
-              "annotation = \"{genome}\","
-              ");\n"
-              "ChIPQCreport(sample);\n")
-    r_code_fn = os.path.join(out_dir, "chipqc.r")
-    with open(r_code_fn, 'w') as inh:
-        print >>inh, r_code.format(**locals())
-
-    return r_code_fn
+    if genome in supported:
+        peaks = sample.get("peaks_files", []).get("main")
+        if peaks:
+            r_code = ("library(ChIPQC);\n"
+                      "sample = ChIPQCsample(\"{bam_fn}\","
+                      "\"{peaks}\", "
+                      "annotation = \"{genome}\","
+                      ");\n"
+                      "ChIPQCreport(sample);\n")
+            r_code_fn = os.path.join(out_dir, "chipqc.r")
+            with open(r_code_fn, 'w') as inh:
+                print >>inh, r_code.format(**locals())
+            return r_code_fn
 
 def _template():
     r_code = ("library(ChIPQC);\n"
