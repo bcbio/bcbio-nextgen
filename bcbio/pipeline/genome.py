0f0e2e3ba7a6d00c2d83f16be5213ac2280b4cb4
chapmanb <chapmanb@50mail.com>
2018-11-21 04:53:41 -0500

    CWL: genome_resources -> aliases -> human for non-human runs
    chapmanb/bcbio-nextgen#2473

--- bcbio/pipeline/genome.py
+++ bcbio/pipeline/genome.py
@@ -49,7 +49,7 @@ def add_required_resources(resources):
                 ["variation", "germline_het_pon"],
                 ["variation", "train_hapmap"], ["variation", "train_indels"],
                 ["variation", "editing"], ["variation", "exac"], ["variation", "esp"],
-                ["variation", "1000g"]]
+                ["variation", "1000g"], ["aliases", "human"]]
     for key in required:
         if not tz.get_in(key, resources):
             resources = tz.update_in(resources, key, lambda x: None)
