1354a92c987e888e95e7e13f92387c6ea25be3b3
chapmanb <chapmanb@50mail.com>
2019-04-05 13:09:23 -0400

    Python 3: flexibly set locale for MultiQC and click based tools
    Looks up available locales in case default C.UTF-8 is not available.
    Fixes #2746

--- bcbio/utils.py
+++ bcbio/utils.py
@@ -850,8 +850,20 @@ def locale_export():
     RuntimeError: Click will abort further execution because Python 3 was
     configured to use ASCII as encoding for the environment.
     Consult https://click.palletsprojects.com/en/7.x/python3/ for mitigation steps.
+
+    Looks up available locales on the system to find an appropriate one to pick,
+    defaulting to C.UTF-8 which is globally available on newer systems.
     """
-    return "export LC_ALL=C.UTF-8 && export LANG=C.UTF-8 && "
+    locale_to_use = "C.UTF-8"
+    try:
+        locales = subprocess.check_output(["locale", "-a"]).decode().split("\n")
+    except subprocess.CalledProcessError:
+        locales = []
+    for locale in locales:
+        if locale.lower().endswith(("utf-8", "utf8")):
+            locale_to_use = locale
+            break
+    return "export LC_ALL=%s && export LANG=%s && " % (locale_to_use, locale_to_use)
 
 def java_freetype_fix():
     """Provide workaround for issues FreeType library symbols.
