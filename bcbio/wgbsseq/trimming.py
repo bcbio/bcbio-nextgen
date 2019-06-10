25dd646915ead84b510091556af6049b5198f495
Ilya Sytchev <isytchev@hsph.harvard.edu>
2019-06-10 11:04:04 -0400

    Code cleanup and enable support for file renaming by trim_galore

--- bcbio/wgbsseq/trimming.py
+++ bcbio/wgbsseq/trimming.py
@@ -1,60 +1,52 @@
 import os
 
-from bcbio.utils import file_exists, append_stem, replace_directory, symlink_plus
+from bcbio.utils import append_stem, replace_directory
 from bcbio.provenance import do
-from bcbio.distributed.transaction import file_transaction, tx_tmpdir
+from bcbio.distributed.transaction import file_transaction
 from bcbio import utils
 from bcbio.pipeline import datadict as dd
 from bcbio.qc import fastqc
 from bcbio.bam import fastq
 from bcbio.pipeline import config_utils
-from bcbio.log import logger
 
 
 def trimming(data):
     """Remove adapter for bisulphite conversion sequencing data"""
     in_files = data["files"]
     names = dd.get_sample_name(data)
-
     work_dir = os.path.join(dd.get_work_dir(data), "trimmed", names)
     out_dir = utils.safe_makedir(work_dir)
-
     out_files = [
-        os.path.join(out_dir, os.path.basename(in_files[0])),
-        os.path.join(out_dir, os.path.basename(in_files[1]))
+        os.path.join(out_dir,
+                     utils.splitext_plus(os.path.basename(in_files[0]))[0]
+                     + '_val_1.fq.gz'),
+        os.path.join(out_dir,
+                     utils.splitext_plus(os.path.basename(in_files[1]))[0]
+                     + '_val_2.fq.gz')
     ]
 
-    # if len(in_file) == 1:
-    #     out_files = [op.join(out_dir, names + "_1.fastq.gz"), None]
-    #     tmp_files = [op.join(out_dir, names + "_tmp_1.fastq.gz"), None]
-    # else:
-    #     out_files = [op.join(out_dir, names + "_1.fastq.gz"),
-    #                  op.join(out_dir, names + "_2.fastq.gz")]
-    #     tmp_files = [op.join(out_dir, names + "_tmp_1.fastq.gz"),
-    #                  op.join(out_dir, names + "_tmp_2.fastq.gz")]
     if utils.file_exists(out_files[0]):
         data["files"] = out_files
         return [[data]]
+
     trim_galore = config_utils.get_program("trim_galore", data["config"])
     cmd = "{trim_galore} --clip_r1 8 --clip_r2 8 --three_prime_clip_r1 8 --three_prime_clip_r2 8 --length 30 --quality 30 --fastqc --paired -o {tx_out_dir} {files}"
     log_file = os.path.join(out_dir, names + "_cutadapt_log.txt")
 
     if not utils.file_exists(out_files[0]):
         with file_transaction(out_dir) as tx_out_dir:
-        # with tx_tmpdir(data, work_dir) as tx_tmp_dir:
             files = "%s %s" % (in_files[0], in_files[1])
             do.run(cmd.format(**locals()), "remove adapters with trimgalore")
 
     data["files"] = out_files
-
     return [[data]]
 
 
 def _run_qc_fastqc(in_files, data, out_dir):
     in_files = fastq.downsample(in_files[0], in_files[1], N=5000000)
     for fastq_file in in_files:
-            if fastq_file:
-                fastqc.run(fastq_file, data, os.path.join(out_dir, utils.splitext_plus(os.path.basename(fastq_file))[0]))
+        if fastq_file:
+            fastqc.run(fastq_file, data, os.path.join(out_dir, utils.splitext_plus(os.path.basename(fastq_file))[0]))
 
 
 def _fix_output(in_file, stem, out_dir):
