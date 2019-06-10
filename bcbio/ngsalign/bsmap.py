d167a207a14d276318f1ff26e60b481cd0b541ae
Ilya Sytchev <isytchev@hsph.harvard.edu>
2019-06-10 11:46:23 -0400

    Cleanup bsmap.py

--- bcbio/ngsalign/bsmap.py
+++ bcbio/ngsalign/bsmap.py
@@ -1,10 +1,4 @@
 import os
-import glob
-import sys
-import shutil
-import subprocess
-import contextlib
-import pysam
 
 from bcbio.pipeline import config_utils
 from bcbio.distributed.transaction import file_transaction, tx_tmpdir
@@ -19,7 +13,7 @@ from bcbio import broad
 
 
 def align(fastq_file, pair_file, ref_file, names, align_dir, data):
-    assert data["analysis"].lower().startswith("wgbs-seq"), "No comparible alignment."
+    assert data["analysis"].lower().startswith("wgbs-seq"), "No comparible alignment"
     config = data["config"]
     sample = dd.get_sample_name(data)
     out_prefix = os.path.join(align_dir, dd.get_lane(data))
@@ -38,21 +32,21 @@ def align(fastq_file, pair_file, ref_file, names, align_dir, data):
     cmd = "{bsmap} {num_cores} -w 100 -v 0.07 -m 10 -x 300 -o {tx_out_bam} -d {ref_file} {fastq_files}"
     if pair_file:
         fastq_files = "-a %s -b %s" % (fastq_file, pair_file)
-    if not final_bam:
-        with file_transaction(final_bam) as tx_out_bam:
+    if not final_out:
+        with file_transaction(final_out) as tx_out_bam:
             run_message = "Running BSMAP aligner on %s and %s" % (fastq_file, ref_file)
             do.run(cmd.format(**locals()), run_message, None)
     data = dd.set_work_bam(data, final_out)
     return data
 
-def _process_bam(bam_file, in_fastq, sample, reference, config):
-        broad_runner = broad.runner_from_config(config)
-        names = {'rg': in_fastq, 'library': 'WGBS_LIB', 'pl': 'Illumina', 'pu': 'R1', 'sm': in_fastq, 'sample': sample}
-        out_fix_bam = broad_runner.run_fn("picard_fix_rgs", bam_file, names)
-        order_bam = utils.append_stem(out_fix_bam, "_order")
-        broad_runner.run_fn("picard_reorder", out_fix_bam, reference, order_bam)
-        bam.index(order_bam, config)
-        # order_bam = _set_quality(order_bam)
-        # bam.index(order_bam, config)
-        return order_bam
 
+def _process_bam(bam_file, in_fastq, sample, reference, config):
+    broad_runner = broad.runner_from_config(config)
+    names = {'rg': in_fastq, 'library': 'WGBS_LIB', 'pl': 'Illumina', 'pu': 'R1', 'sm': in_fastq, 'sample': sample}
+    out_fix_bam = broad_runner.run_fn("picard_fix_rgs", bam_file, names)
+    order_bam = utils.append_stem(out_fix_bam, "_order")
+    broad_runner.run_fn("picard_reorder", out_fix_bam, reference, order_bam)
+    bam.index(order_bam, config)
+    # order_bam = _set_quality(order_bam)
+    # bam.index(order_bam, config)
+    return order_bam
