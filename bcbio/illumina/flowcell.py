ac22e2bd13a35002a91c59d387b397c959c278a3
chapmanb <chapmanb@50mail.com>
2019-08-19 13:17:59 -0400

    Handle new illumina flowcells ending with X3

--- bcbio/illumina/flowcell.py
+++ bcbio/illumina/flowcell.py
@@ -13,7 +13,7 @@ def parse_dirname(fc_dir):
     name = None
     date = None
     for p in parts:
-        if p.endswith(("XX", "xx", "XY", "X2")):
+        if p.endswith(("XX", "xx", "XY", "X2", "X3")):
             name = p
         elif len(p) == 6:
             try:
