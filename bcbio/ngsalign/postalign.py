4bc6d4036b032254c2e1590ebe78ba4af1ec9918
naumenko.sa <evolgenomicslab@gmail.com>
2019-09-18 14:09:05 -0400

    sync

--- bcbio/ngsalign/postalign.py
+++ bcbio/ngsalign/postalign.py
@@ -202,7 +202,7 @@ def correct_umis(data):
     cmd = ("unset JAVA_HOME && "
            "fgbio {jvm_opts} {io_opts} CorrectUmis {group_opts} -t {umi_tag} -s {umi_method} -m 3 -d 1 -x"
            "-U umis.txt"
-           "-i {align_bam} -o {output_bam}")
+           "-i {input_bam} -o {output_bam}")
     do.run(cmd.format(**locals()), "Correcting UMIs")
 
 def umi_consensus(data):
