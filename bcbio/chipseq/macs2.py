3382c0b610ce1088ff5fd78c0b062156ed92e22e
roryk <rory.kirchner@gmail.com>
2019-10-08 16:59:49 -0400

    compres -> compress typo fix.

--- bcbio/chipseq/macs2.py
+++ bcbio/chipseq/macs2.py
@@ -19,7 +19,7 @@ def run(name, chip_bam, input_bam, genome_build, out_dir, method, resources, dat
     out_file = os.path.join(out_dir, name + "_peaks_macs2.xls")
     macs2_file = os.path.join(out_dir, name + "_peaks.xls")
     if utils.file_exists(out_file):
-        _compres_bdg_files(out_dir)
+        _compress_bdg_files(out_dir)
         return _get_output_files(out_dir)
     macs2 = config_utils.get_program("macs2", config)
     options = " ".join(resources.get("macs2", {}).get("options", ""))
@@ -38,7 +38,7 @@ def run(name, chip_bam, input_bam, genome_build, out_dir, method, resources, dat
                                  "You can add specific options for the sample "
                                  "setting resources as explained in docs: "
                                  "https://bcbio-nextgen.readthedocs.org/en/latest/contents/configuration.html#sample-specific-resources")
-    _compres_bdg_files(out_dir)
+    _compress_bdg_files(out_dir)
     return _get_output_files(out_dir)
 
 def _get_output_files(out_dir):
@@ -53,7 +53,7 @@ def _get_output_files(out_dir):
             break
     return {"main": peaks, "macs2": fns}
 
-def _compres_bdg_files(out_dir):
+def _compress_bdg_files(out_dir):
     for fn in glob.glob(os.path.join(out_dir, "*bdg")):
         cmd = "gzip  %s" % fn
         do.run(cmd, "compress bdg file: %s" % fn)
