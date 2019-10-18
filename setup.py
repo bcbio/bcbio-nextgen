1d3ee6c7477fbece473ad642a57e2069336863a2
roryk <rory.kirchner@gmail.com>
2019-10-18 13:23:05 -0400

    Bump version to 1.1.8a.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = "1.1.7"
+version = "1.1.8a"
 
 def write_version_py():
     version_py = os.path.join(os.path.dirname(__file__), 'bcbio', 'pipeline',
