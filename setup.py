fa2d8eb9a187628f2f5320ad1e8d4d2fcd0e30aa
Ilya Sytchev <isytchev@hsph.harvard.edu>
2020-02-21 23:16:26 -0500

    Add setup.cfg

--- setup.py
+++ setup.py
@@ -7,42 +7,22 @@ import subprocess
 
 import setuptools
 
-__version__ = '1.2.0'
+VERSION = '1.2.0'
 
-with open('README.rst', 'r') as readme_file:
-    long_description = readme_file.read()
-
-scripts = ['scripts/bcbio_nextgen.py',
-           'scripts/bcbio_setup_genome.py',
-           'scripts/bcbio_prepare_samples.py',
-           'scripts/bcbio_fastq_umi_prep.py',
-           'scripts/cwltool2wdl.py']
+here = os.path.dirname(os.path.abspath(__file__))
 
 # add bcbio version number and git commit hash of the current revision to version.py
 try:
-    git = subprocess.run(['git', 'rev-parse', '--short', 'HEAD'], stdout=subprocess.PIPE)
-    git.check_returncode()
+    git_run = subprocess.run(['git', 'rev-parse', '--short', 'HEAD'], stdout=subprocess.PIPE)
+    git_run.check_returncode()
 except subprocess.SubprocessError:
     commit_hash = ''
 else:
-    commit_hash = git.stdout.strip().decode()
-version_py = os.path.join(os.path.dirname(__file__), 'bcbio', 'pipeline', 'version.py')
-with open(version_py, 'w') as version_file:
-    version_file.writelines([f'__version__ = "{__version__}"\n',
+    commit_hash = git_run.stdout.strip().decode()
+with open(os.path.join(here, 'bcbio', 'pipeline', 'version.py'), 'w') as version_file:
+    version_file.writelines([f'__version__ = "{VERSION}"\n',
                              f'__git_revision__ = "{commit_hash}"\n'])
 
-setuptools.setup(
-    name='bcbio-nextgen',
-    version=__version__,
-    author='bcbio community',
-    author_email='biovalidation@googlegroups.com',
-    description='Best-practice pipelines for fully automated high throughput sequencing analysis',
-    long_description=long_description,
-    license='MIT',
-    url='https://github.com/bcbio/bcbio-nextgen',
-    packages=setuptools.find_packages(exclude=['tests']),
-    zip_safe=False,
-    scripts=scripts,
-    install_requires=[],  # install dependencies via conda
-    include_package_data=True
-)
+# dependencies are installed via Conda
+# https://github.com/chapmanb/cloudbiolinux/blob/master/contrib/flavor/ngs_pipeline_minimal/packages-conda.yaml
+setuptools.setup(version=VERSION)
