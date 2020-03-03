60fd48eeedd9a5aa7def2769704e48f6b433cef8
Ilya Sytchev <isytchev@hsph.harvard.edu>
2020-03-03 16:39:08 -0500

    Fix doc builds on rtd

--- docs/conf.py
+++ docs/conf.py
@@ -47,6 +47,9 @@ source_suffix = {
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['templates']
 
+# The master toctree document.
+master_doc = 'index'
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
