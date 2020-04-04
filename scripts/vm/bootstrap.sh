e5a6c09c53c639a83baf9f5db8f36637823a5870
Ilya Sytchev <isytchev@hsph.harvard.edu>
2020-04-04 12:18:44 -0400

    Provision Docker in Vagrant (#3159)
    * Provision Docker in Vagrant
    
    * Use absolute paths for all commands in Vagrant bootstrap script

--- scripts/vm/bootstrap.sh
+++ scripts/vm/bootstrap.sh
@@ -3,12 +3,15 @@
 set -e -x
 
 /usr/bin/apt-get -qq update
-/usr/bin/apt-get install -q -y build-essential python-setuptools zlib1g-dev
+/usr/bin/apt-get install -q -y build-essential docker.io python-setuptools zlib1g-dev
 /usr/bin/apt-get -y autoremove
 /usr/bin/apt-get clean
 
-NEW_PATH='${HOME}/local/share/bcbio/anaconda/bin:${HOME}/local/bin:${PATH}'
-grep -qxF "PATH=${NEW_PATH}" ~vagrant/.profile || echo "PATH=${NEW_PATH}" >> ~vagrant/.profile
-grep -qxF 'export BCBIO_TEST_DIR=/tmp/bcbio' ~vagrant/.profile || echo 'export BCBIO_TEST_DIR=/tmp/bcbio' >> ~vagrant/.profile
+/bin/systemctl start docker.service
+/usr/sbin/usermod -aG docker vagrant
 
-wget --no-verbose -P /opt https://raw.githubusercontent.com/bcbio/bcbio-nextgen/master/scripts/bcbio_nextgen_install.py
+NEW_PATH='${HOME}/local/share/bcbio/anaconda/bin:${HOME}/local/bin:${PATH}'
+/bin/grep -qxF "PATH=${NEW_PATH}" ~vagrant/.profile || /bin/echo "PATH=${NEW_PATH}" >> ~vagrant/.profile
+/bin/grep -qxF 'export BCBIO_TEST_DIR=/tmp/bcbio' ~vagrant/.profile || /bin/echo 'export BCBIO_TEST_DIR=/tmp/bcbio' >> ~vagrant/.profile
+
+/usr/bin/wget --no-verbose -P /opt https://raw.githubusercontent.com/bcbio/bcbio-nextgen/master/scripts/bcbio_nextgen_install.py
