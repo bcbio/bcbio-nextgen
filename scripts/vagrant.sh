159a01b5bcc6b6c5b84ec682222e411f6bf8ae89
Ilya Sytchev <isytchev@hsph.harvard.edu>
2020-04-16 17:44:14 -0400

    Remove zlib1g-dev APT package and add comments to Vagrantfile

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# Vagrant shell provisioning script https://www.vagrantup.com/docs/provisioning/shell.html
+
 set -e -x
 
 /usr/bin/apt-get -qq update
-/usr/bin/apt-get install -q -y build-essential docker.io python-setuptools zlib1g-dev
+/usr/bin/apt-get install -q -y build-essential docker.io python-setuptools
 /usr/bin/apt-get -y autoremove
 /usr/bin/apt-get clean
 
@@ -12,4 +14,6 @@ set -e -x
 
 NEW_PATH='${HOME}/local/share/bcbio/anaconda/bin:${HOME}/local/bin:${PATH}'
 /bin/grep -qxF "PATH=${NEW_PATH}" ~vagrant/.profile || /bin/echo "PATH=${NEW_PATH}" >> ~vagrant/.profile
+# configure test directory within the internal VM file system because synced folders do not support certain features required by bcbio
+# for example: mkfifo is required for CWL Cromwell runs, etc
 /bin/grep -qxF 'export BCBIO_TEST_DIR=/tmp/bcbio' ~vagrant/.profile || /bin/echo 'export BCBIO_TEST_DIR=/tmp/bcbio' >> ~vagrant/.profile
