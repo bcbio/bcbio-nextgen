c2daeb3a970c400a57fb3281873dd8467a06d309
Mendler, Kerrin (BISC GLOBAL) <kerrin.mendler@astrazeneca.com>
2020-05-26 17:42:00 -0400

    Get fastq trim settings from the kit specified in config

---
+++ bcbio/wgbsseq/kits.py
@@ -0,0 +1,10 @@
+from collections import namedtuple
+
+Kit = namedtuple('Kit', 'name clip_r1_5 clip_r1_3 clip_r2_5 clip_r2_3')
+
+_KITS = [
+    Kit("truseq", 8, 8, 8, 8),
+]
+
+KITS = {x.name: x for x in _KITS}
+SUPPORTED_KITS = {x.name for x in _KITS}
--- bcbio/wgbsseq/trimming.py
+++ bcbio/wgbsseq/trimming.py
@@ -7,7 +7,9 @@ from bcbio import utils
 from bcbio.pipeline import datadict as dd
 from bcbio.qc import fastqc
 from bcbio.bam import fastq
+from bcbio.log import logger
 from bcbio.pipeline import config_utils
+from bcbio.wgbsseq import kits
 
 
 def trim(data):
@@ -29,8 +31,18 @@ def trim(data):
         data["files"] = out_files
         return [[data]]
 
+    kit = kits.KITS.get(dd.get_kit(data), None)
+    if kit:
+        logger.info(f"{kit.name} specified, using clip settings: R1 5'-{kit.clip_r1_5}nt/--/{kit.clip_r1_3}nt-3', R2 5'-{kit.clip_r2_5}nt/--/{kit.clip_r2_3}nt-3'")
+        clipsettings = _get_clip_settings(kit)
+    else:
+        logger.info(f"No kit specified, using default clip settings")
+        clipsettings = ""
+
     trim_galore = config_utils.get_program("trim_galore", data["config"])
-    cmd = "{trim_galore} --clip_r1 8 --clip_r2 8 --three_prime_clip_r1 8 --three_prime_clip_r2 8 --length 30 --quality 30 --fastqc --paired -o {tx_out_dir} {files}"
+    other_opts = config_utils.get_resources("trim_galore", data["config"]).get("options", [])
+    other_opts = " ".join([str(x) for x in other_opts]).strip()
+    cmd = "{trim_galore} {other_opts} {clipsettings} --length 30 --quality 30 --fastqc --paired -o {tx_out_dir} {files}"
     log_file = os.path.join(out_dir, names + "_cutadapt_log.txt")
 
     if not utils.file_exists(out_files[0]):
@@ -52,3 +64,16 @@ def _run_qc_fastqc(in_files, data, out_dir):
 def _fix_output(in_file, stem, out_dir):
     out_file = utils.splitext_plus(replace_directory(append_stem(in_file, stem), out_dir))
     return  "%s%s" % (out_file[0], out_file[1].replace("fastq", "fq"))
+
+
+def _get_clip_settings(kit):
+    clip_settings = ""
+    if kit.clip_r1_5 > 0:
+        clip_settings = clip_settings + "--clip_r1 " + str(kit.clip_r1_5) + " "
+    if kit.clip_r2_5 > 0:
+        clip_settings = clip_settings + "--clip_r2 " + str(kit.clip_r2_5) + " "
+    if kit.clip_r1_3 > 0:
+        clip_settings = clip_settings + "--three_prime_clip_r1 " + str(kit.clip_r1_3) + " "
+    if kit.clip_r2_3 > 0:
+        clip_settings = clip_settings + "--three_prime_clip_r2 " + str(kit.clip_r2_3) + " "
+    return clip_settings.strip()
