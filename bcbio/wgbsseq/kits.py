41987ae6f45387c67e7fb0f70b417acf49c8ff7a
Mendler, Kerrin (BISC GLOBAL) <kerrin.mendler@astrazeneca.com>
2020-05-26 17:42:53 -0400

    Add support for AccelSeq kit, update docs

--- bcbio/wgbsseq/kits.py
+++ bcbio/wgbsseq/kits.py
@@ -4,6 +4,7 @@ Kit = namedtuple('Kit', 'name clip_r1_5 clip_r1_3 clip_r2_5 clip_r2_3')
 
 _KITS = [
     Kit("truseq", 8, 8, 8, 8),
+    Kit("accelngs", 0, 15, 15, 0)
 ]
 
 KITS = {x.name: x for x in _KITS}
