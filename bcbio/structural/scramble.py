613b84e0343eaf5c5160780cbdcf8291ab1d91ed
Ilya Sytchev <isytchev@hsph.harvard.edu>
2020-08-31 18:18:39 -0400

    Iterate over items datadict when running Scramble

--- bcbio/structural/scramble.py
+++ bcbio/structural/scramble.py
@@ -12,11 +12,14 @@ from bcbio.provenance import do
 
 
 def run(items):
-    data = items[0]
-    input_bam_file_path = dd.get_work_bam(data)
-    work_dir = os.path.join(dd.get_work_dir(data), 'structural', dd.get_sample_name(data))
-    output_clusters_file_path = os.path.join(work_dir, 'clusters.txt')
-    if not utils.file_exists(output_clusters_file_path):
-        with file_transaction(output_clusters_file_path) as temp_output_file_path:
-            do.run(f'cluster_identifier {input_bam_file_path} > {temp_output_file_path}')
+    for index, data in enumerate(items):
+        work_dir = os.path.join(dd.get_work_dir(data), 'structural', dd.get_sample_name(data))
+        output_clusters_file_path = os.path.join(work_dir, 'clusters.txt')
+
+        if not utils.file_exists(output_clusters_file_path):
+            with file_transaction(output_clusters_file_path) as temp_output_file_path:
+                do.run(f'cluster_identifier {dd.get_work_bam(data)} > {temp_output_file_path}')
+
+        items[index].get('sv', []).append({'variantcaller': 'scramble',
+                                           'clusters_file': output_clusters_file_path})
     return items
