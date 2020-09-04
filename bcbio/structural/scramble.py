2142ffea57f8ef0be48df549a57d7b3d6e782249
Ilya Sytchev <isytchev@hsph.harvard.edu>
2020-09-04 13:23:34 -0400

    Add MEI identification step

--- bcbio/structural/scramble.py
+++ bcbio/structural/scramble.py
@@ -14,12 +14,26 @@ from bcbio.provenance import do
 def run(items):
     for index, data in enumerate(items):
         work_dir = os.path.join(dd.get_work_dir(data), 'structural', dd.get_sample_name(data))
-        output_clusters_file_path = os.path.join(work_dir, 'clusters.txt')
 
+        output_clusters_file_path = os.path.join(work_dir, 'clusters.txt')
         if not utils.file_exists(output_clusters_file_path):
             with file_transaction(output_clusters_file_path) as temp_output_file_path:
-                do.run(f'cluster_identifier {dd.get_work_bam(data)} > {temp_output_file_path}')
+                do.run(f'cluster_identifier {dd.get_work_bam(data)} > {temp_output_file_path}',
+                       'Running SCRAMble cluster_identifier')
+
+        output_mei_file_path = os.path.join(work_dir, 'mei.txt')
+        if not utils.file_exists(output_mei_file_path):
+            with file_transaction(output_mei_file_path) as temp_mei_file_path:
+                # temp workaround: remove PREFIX when SCRAMble Conda package becomes available
+                PREFIX = '~/scramble/cluster_analysis'
+                do.run(f'{utils.Rscript_cmd()} --vanilla {PREFIX}/bin/SCRAMble-MEIs.R '
+                       f'--out-name {temp_mei_file_path} '
+                       f'--cluster-file {output_clusters_file_path} '
+                       f'--install-dir {PREFIX}/bin '
+                       f'--mei-refs {PREFIX}/resources/MEI_consensus_seqs.fa',
+                       'Analyzing cluster file for likely MEIs')
 
         items[index].get('sv', []).append({'variantcaller': 'scramble',
-                                           'clusters_file': output_clusters_file_path})
+                                           'clusters_file': output_clusters_file_path,
+                                           'mei_file': output_mei_file_path})
     return items
