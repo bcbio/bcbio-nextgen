de18c514c6fe4200da8b96d93ca244cf035e0630
roryk <rory.kirchner@gmail.com>
2020-12-03 20:03:06 -0500

    Make log less spammy, especially during restarts.
    If we are restarting, we've already aligned here so don't need
    these messages.

--- bcbio/pipeline/sample.py
+++ bcbio/pipeline/sample.py
@@ -136,7 +136,7 @@ def process_alignment(data, alt_input=None):
 #            bam.index(fastq1, dd.get_config(data))
 #            data["work_bam"] = fastq1
         else:
-            logger.info("Aligning lane %s with %s aligner" % (data["rgnames"]["lane"], aligner))
+#            logger.info("Aligning lane %s with %s aligner" % (data["rgnames"]["lane"], aligner))
             data = align_to_sort_bam(fastq1, fastq2, aligner, data)
         if dd.get_correct_umis(data):
             data["work_bam"] = postalign.correct_umis(data)
